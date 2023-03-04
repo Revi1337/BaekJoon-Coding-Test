0de7649690f105f934814470d6d0ad1392d1d5ed
Revi1337 <86167726+Revi1337@users.noreply.github.com>
2023-03-04 19:13:19 +0000

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,26 +1,2 @@
 def solution(array):
-    size = len(array)
-    if size == 1:
-        return array[0]
-    else:
-        array.sort()
-        if size == 3:
-            return array[1]
-        else:
-            return array[size // 2]
-    
-    
-
-# def solution(array):
-#     lenz = len(array)
-#     if lenz == 1:
-#         return array[0]
-#     else:
-#         array.sort()
-#         if lenz == 3:
-#             return array[1]
-#         else:
-#             return array[lenz-1//2]
-    
-    
-    
+    return sorted(array)[len(array) // 2]
