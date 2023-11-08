be517eca4bfc662954cdd3f2636467eea152fa00
Revi1337 <david122123@gmail.com>
2023-11-08 18:18:22 +0900

    Update 과제 안 내신 분．．？.py

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,6 +1,7 @@
-nums = [int(input()) for _ in range(28)]
-pre = list(range(1,31))
-
-answer = sorted(set(pre) - set(nums))
-print(answer[0])
-print(answer[1])
+arr = [0] * 31
+for i in range(28):
+    idx = int(input())
+    arr[idx] = 1
+for i in range(1, 31):
+    if arr[i] == 0:
+        print(i)
