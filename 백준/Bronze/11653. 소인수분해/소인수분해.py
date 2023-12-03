e50d403325f74b783e1b57e5ffac82ca396ada55
Revi1337 <david122123@gmail.com>
2023-12-04 01:27:28 +0900

    Update 소인수분해.py

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -2,9 +2,9 @@ def solution(num):
     if num == 1:
         print('')
         return
-    for val in range(2, num + 1):
-        if num % val == 0:
-            while num % val == 0:
+    for val in range(2, num + 1):   # 2부터 하나씩 나눈다.
+        if num % val == 0:          # 나머지가 0 이면
+            while num % val == 0:   # 해당 숫자로 나눌 수 없을 때까지 나눈다.
                 print(val)
                 num /= val
 
