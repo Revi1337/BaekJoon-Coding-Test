d36091f106e49851643234f087bbe5c10c44f63b
Revi1337 <david122123@gmail.com>
2023-12-04 02:00:04 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -4,11 +4,12 @@ def solution(nums: list):
         if num == 1:
             continue
         cnt = 0
-        for val in range(2, num + 1):
-            if num % val == 0:
+        for i in range(2, num):
+            if num % i == 0:
                 cnt += 1
-        if cnt == 1:
+        if cnt == 0:
             answer += 1
     return answer
+
 _ = input()
 print(solution(list(map(int, input().split()))))
