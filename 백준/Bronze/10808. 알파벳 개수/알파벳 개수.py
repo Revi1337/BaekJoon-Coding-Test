aabc94c758049fd96099a6904efff1e7a926a50f
Revi1337 <david122123@gmail.com>
2024-01-07 17:39:29 +0900

    Update 알파벳 개수.py

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -4,12 +4,11 @@ seed = "abcdefghijklmnopqrstuvwxyz"
 
 def solution(data):
     answer = [0] * len(seed)
-    for idx in range(len(seed)):
-        for val in data:
-            if seed[idx] == val:
-                answer[idx] += 1
-    return " ".join(map(lambda x: str(x), answer))
+    for val in data:
+        answer[ord(val) - 97] += 1      # 현재 단어의 ASII 코드에서 소문자 a 의 ASCII 코드를 뺀 값을 idx 로 사용
+    for char in answer:
+        print(char, end = ' ')
 
 
 data = sys.stdin.readline().rstrip()
-print(solution(data))
+solution(data)
