8803380bd66336205d1eea0638dfada0578d5cd7
Revi1337 <david122123@gmail.com>
2024-01-09 14:07:49 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.95 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def solution(s):
+    table = defaultdict(int)
+    answer = []
+    for idx in range(len(s)):
+        if not s[idx] in table:
+            answer.append(-1)
+            table[s[idx]] = idx
+        else:
+            answer.append(idx - table[s[idx][-1]])
+            table[s[idx]] = idx
+    return answer
