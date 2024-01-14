eac1b357374f89979b2673894808c13f5659d67e
Revi1337 <david122123@gmail.com>
2024-01-14 23:16:11 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -2,15 +2,13 @@ def solution(num):
     answer = 0
     if num == 1:
         return 0
-    number = num
-    while number > 1:
-        if number % 2 == 1:
-            number = (number * 3) + 1
-        elif number % 2 == 0:
-            number = number // 2
-        answer += 1
-        if answer == 500:
+    data = num
+    while data > 0:
+        if answer >= 500:
             return -1
+        if data % 2:
+            data //= 2
+        else:
+            data = data * 3 + 1
+        answer += 1
     return answer
-
-print(solution(626331))
