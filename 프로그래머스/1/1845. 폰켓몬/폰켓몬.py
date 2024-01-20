a11ba7f0ab4b073cb30bf368ad2211019742750e
Revi1337 <david122123@gmail.com>
2024-01-21 03:05:07 +0900

    [level 1] Title: 폰켓몬, Time: 1.60 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -3,6 +3,10 @@ from collections import Counter
 def solution(nums):
     length = len(nums)
     choice = length // 2
-    counter = Counter(nums)
+    counter = {}
+    for integer in nums:
+        counter.setdefault(integer, 0)
+        counter[integer] += 1
+    # counter = Counter(nums)
     kind = sorted(counter.values())
     return min(choice, len(kind))
