6fd6e55c98d83d7461a535182518da65e10a31db
Revi1337 <david122123@gmail.com>
2024-01-24 00:49:21 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,16 @@
+from collections import deque
+
 def solution(cards1, cards2, goal):
-    card1_idx, card2_idx = 0, 0
-    for word in goal:
-        if len(cards1) > card1_idx and word == cards1[card1_idx]:
-            card1_idx += 1
-        elif len(cards2) > card2_idx and word == cards2[card2_idx]:
-            card2_idx += 1
+    answer = 'Yes'
+    queue = deque(goal)
+    cards1_idx = cards2_idx = 0
+    cards1_length, cards2_length = len(cards1), len(cards2)
+    while queue:
+        expect = queue.popleft()
+        if cards1_length > cards1_idx and cards1[cards1_idx] == expect:
+            cards1_idx += 1
+        elif cards2_length > cards2_idx and cards2[cards2_idx] == expect:
+            cards2_idx += 1
         else:
             return 'No'
-    return 'Yes'
+    return answer
