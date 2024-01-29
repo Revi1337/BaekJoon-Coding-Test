c73ece6bead1af87b3962cccb982ddfce45d54af
Revi1337 <david122123@gmail.com>
2024-01-29 14:52:13 +0900

    바이러스 Graph and BFS and DFS.py

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,3 +1,27 @@
+def solution_graph_bfs(n, edges):
+    from collections import deque
+    graph = [[] for _ in range(n + 1)]
+    for v1, v2 in edges:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+    queue = deque()
+    queue.append(1)
+    check = [0] * (n + 1)
+    check[1] = 1
+    answer = 0
+    while queue:
+        node_cnt = len(queue)
+        answer += node_cnt
+        for _ in range(node_cnt):
+            node = queue.popleft()
+            for next_node in graph[node]:
+                if check[next_node] == 0:
+                    check[next_node] = 1
+                    queue.append(next_node)
+    return answer - 1
+
+
 def solution_graph_dfs(n, edges):
     graph = [[] for _ in range(n + 1)]
     for v1, v2 in edges:
@@ -20,5 +44,6 @@ def solution_graph_dfs(n, edges):
 n = int(input())
 edge_count = int(input())
 edges = [list(map(int, input().split())) for _ in range(edge_count)]
-# print(solution_graph_bfs(n, edges))
+print(solution_graph_bfs(n, edges))
 print(solution_graph_dfs(n, edges))
+
