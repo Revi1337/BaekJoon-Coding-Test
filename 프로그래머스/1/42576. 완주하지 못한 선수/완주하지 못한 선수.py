c8a5da7b747d10dbb6063b12f58483a49c65b8eb
Revi1337 <david122123@gmail.com>
2024-02-02 16:05:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.03 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,12 @@
 def solution(participant, completion):
-    table = {}
-    for human in participant:
-        table.setdefault(human, 0)
-        table[human] += 1
-    for com in completion:
-        if com in table:
-            table[com] -= 1
-    for key in table:
-        if table[key] > 0:
-            return key
+    compl_dict = {}
+    for compl in completion:
+        compl_dict[compl] = compl_dict.get(compl, 0) + 1
+    for par in participant:
+        if par not in compl_dict:
+            return par
+
+        compl_dict[par] -= 1
+        
+        if compl_dict[par] < 0:
+            return par
