d4b47d5cddea9f61044da11bb68f0e06e9020818
Revi1337 <david122123@gmail.com>
2024-02-02 21:40:15 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,22 +1,10 @@
 def solution(want, number, discount):
-    table = {}
-    for idx in range(len(want)):
-        table[want[idx]] = number[idx]
-
     answer = 0
-    for idx in range(len(discount) - 9):
-        basket = {}
-        for j in range(idx, idx + 10):
-            basket.setdefault(discount[j], 0)
-            basket[discount[j]] += 1
-
-        boolean = True
-        for key in basket:
-            if basket.get(key) != table.get(key):
-                boolean = boolean and False
-                break
-
-        if boolean:
+    want_dict = dict(zip(want, number))
+    for padding in range(len(discount) - 9):
+        wish = {}
+        for day in range(padding, padding + 10):
+            wish[discount[day]] = wish.get(discount[day], 0) + 1
+        if wish == want_dict:
             answer += 1
-
     return answer
