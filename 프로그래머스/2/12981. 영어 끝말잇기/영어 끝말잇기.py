b0b78f66804f74ec03bff76e2e59d6f0f08a766d
Revi1337 <david122123@gmail.com>
2024-02-18 23:06:34 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,15 +1,9 @@
-def solution(n, words):
-    length = len(words)
-    storage = set()
-    storage.add(words[0])
-    last_char = words[0][-1]
-    for idx in range(1, length):
-        word = words[idx]
-        first_char = word[0]
-        if (first_char != last_char) or (word in storage):
-            pre = (idx // n) * n
-            result = idx - pre
-            return [result + 1, (idx // n) + 1]
-        storage.add(word)
-        last_char = word[-1]
+def solution(k, words):
+    previous_word = words[0]
+    duplicate = {previous_word}
+    for idx, current_word in enumerate(words[1:], start=1):
+        if (current_word[0] != previous_word[-1]) or (current_word in duplicate):
+            return [(idx % k) + 1, (idx // k) + 1]
+        duplicate.add(current_word)
+        previous_word = current_word
     return [0, 0]
