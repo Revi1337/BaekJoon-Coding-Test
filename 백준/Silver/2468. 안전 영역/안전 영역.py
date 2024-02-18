c9c27e489f321a8ee6a3e40374f6f9600df1bd69
Revi1337 <david122123@gmail.com>
2024-02-18 21:53:07 +0900

    [Silver I] Title: 안전 영역, Time: 276 ms, Memory: 116356 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,40 +1,40 @@
-import sys
-
-sys.setrecursionlimit(10 ** 5)
+from collections import deque
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def dfs(row, col):
-    check[row][col] = 1
-    for d in range(4):
-        nrow = row + drow[d]
-        ncol = col + dcol[d]
-        if (0 <= nrow < n) and (0 <= ncol < n) and (blocks[nrow][ncol] > height) and (check[nrow][ncol] == 0):
-            dfs(nrow, ncol)
+def valid_direction():
+    return (0 <= nrow < n) and (0 <= ncol < n) and (blocks[nrow][ncol] > height) and (check[nrow][ncol] == 0)
 
+n = int(input())
 minN = 100_000_000
 maxN = -100_000_000
-n = int(input())
 blocks = []
 for _ in range(n):
     integers = list(map(int, input().split()))
-    for integer in integers:
-        if integer < minN:
-            minN = integer
-        if integer > maxN:
-            maxN = integer
+    minN = min(minN, min(integers))
+    maxN = max(maxN, max(integers))
     blocks.append(integers)
 
-res = -100_000_000
+answer = -100_000_000
 for height in range(0, maxN + 1):
     check = [[0] * n for _ in range(n)]
+    queue = deque()
     cnt = 0
     for i in range(n):
         for j in range(n):
             if check[i][j] == 0 and blocks[i][j] > height:
-                dfs(i, j)
+                queue.append([i, j])
+                check[i][j] = 0
                 cnt += 1
-    res = max(res, cnt)
+                while queue:
+                    row, col = queue.popleft()
+                    for d in range(4):
+                        nrow = row + drow[d]
+                        ncol = col + dcol[d]
+                        if valid_direction():
+                            check[nrow][ncol] = 1
+                            queue.append([nrow, ncol])
+                answer = max(answer, cnt)
 
-print(res)
+print(answer)
