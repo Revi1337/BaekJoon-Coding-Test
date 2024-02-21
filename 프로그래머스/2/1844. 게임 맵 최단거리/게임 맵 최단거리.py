e1b6cf6f2d995a71446a09756d7189f2904483b5
Revi1337 <david122123@gmail.com>
2024-02-22 04:16:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.82 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -7,9 +7,11 @@ def solution(maps):
     row_cnt = len(maps)
     col_cnt = len(maps[0])
     queue = deque()
+    check = set()
     distance = [[0] * col_cnt for _ in range(row_cnt)]
 
     queue.append((0, 0))
+    check.add((0, 0))
     distance[0][0] = 1
 
     while queue:
@@ -19,9 +21,12 @@ def solution(maps):
         for d in range(4):
             nrow = row + drow[d]
             ncol = col + dcol[d]
-            if (0 <= nrow < row_cnt) and (0 <= ncol < col_cnt) and (maps[nrow][ncol]):
-                if distance[nrow][ncol] == 0:
-                    distance[nrow][ncol] = distance[row][col] + 1
-                    queue.append((nrow, ncol))
+            if (0 <= nrow < row_cnt) and (0 <= ncol < col_cnt) and (maps[nrow][ncol]) and ((nrow, ncol) not in check):
+                distance[nrow][ncol] = distance[row][col] + 1
+                check.add((nrow, ncol))
+                queue.append((nrow, ncol))
 
     return -1
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
