4ffcc6494e9e79087fd54d3a926738da37b8a00c
Revi1337 <david122123@gmail.com>
2024-02-22 01:56:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.52 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+drow = [-1, 0, 1, 0]
+dcol = [0, 1, 0, -1]
+
+def solution(maps):
+    row_cnt = len(maps)
+    col_cnt = len(maps[0])
+    queue = deque()
+    distance = [[0] * col_cnt for _ in range(row_cnt)]
+
+    queue.append((0, 0))
+    distance[0][0] = 1
+
+    while queue:
+        row, col = queue.popleft()
+        if distance[row_cnt - 1][col_cnt - 1] != 0:
+            return distance[row_cnt - 1][col_cnt - 1]
+        for d in range(4):
+            nrow = row + drow[d]
+            ncol = col + dcol[d]
+            if (0 <= nrow < row_cnt) and (0 <= ncol < col_cnt) and (maps[nrow][ncol]):
+                if distance[nrow][ncol] == 0:
+                    distance[nrow][ncol] = distance[row][col] + 1
+                    queue.append((nrow, ncol))
+
+    return -1
