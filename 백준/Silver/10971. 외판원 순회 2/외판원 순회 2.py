8ced4b3475b0af9c3b637b837eb8920354a20d5b
Revi1337 <david122123@gmail.com>
2024-02-26 01:07:35 +0900

    [Silver II] Title: 외판원 순회 2, Time: 240 ms, Memory: 111636 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,28 +1,29 @@
 import sys
-input = sys.stdin.readline
- 
+
+def solution(n, graph):
+    vertext_cnt = n
+    answer = float('inf')
+
+    def dfs(c_vertext, next_vertext, cost, check):
+        nonlocal answer
+        if len(check) == vertext_cnt:
+            tmp = graph[next_vertext][c_vertext]
+            if tmp != 0:
+                answer = min(answer, cost + tmp)
+            return
+
+        for n_vertext in range(vertext_cnt):
+            next_cost = graph[next_vertext][n_vertext]
+            if (next_cost) and (n_vertext not in check) and (cost < answer):
+                check.append(n_vertext)
+                dfs(c_vertext, n_vertext, cost + next_cost, check)
+                check.pop()
+
+    for vertext in range(vertext_cnt):
+        dfs(vertext, vertext, 0, [vertext])
+
+    return answer
+
 n = int(input())
-a = list(range(n))
-w = [list(map(int, input().split())) for _ in  range(n)]
- 
-ans = float('inf')
- 
-def bf(start,next, cost, visited):
-    global ans
-    if len(visited)==n:
-        tmp = w[next][start]
-        if tmp !=0:
-            ans = min(ans, cost+tmp)
-        return
-    
-    for i in range(n):
-        tmp = w[next][i]
-        if tmp!=0 and i not in visited and cost<ans:
-            visited.append(i)
-            bf(start, i, cost+tmp, visited)
-            visited.pop()
-        
- 
-for i in range(n):
-    bf(i, i, 0, [i])
-print(ans)
+edges = [list(map(int, input().split())) for _ in range(n)]
+print(solution(n, edges))
