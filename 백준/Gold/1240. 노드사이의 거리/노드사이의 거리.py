ccb8404b32fa50b9511151c6b0b84f13545315e4
Revi1337 <david122123@gmail.com>
2024-03-05 15:28:58 +0900

    [Gold V] Title: 노드사이의 거리, Time: 368 ms, Memory: 113132 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -8,10 +8,10 @@ def solution(n, edges, wish):
         graph[node1].append([node2, cost])
         graph[node2].append([node1, cost])
 
-    def dfs(f):
-        for next_node, cost in graph[f]:
+    def dfs(curr_node):
+        for next_node, cost in graph[curr_node]:
             if check[next_node] == -1:
-                check[next_node] = check[f] + cost
+                check[next_node] = check[curr_node] + cost
                 dfs(next_node)
 
     for f, t in wish:
