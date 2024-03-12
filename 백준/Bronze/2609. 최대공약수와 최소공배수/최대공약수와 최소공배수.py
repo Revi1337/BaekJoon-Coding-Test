24dad521f051c843ea1fda16f6b774486b739a1e
Revi1337 <david122123@gmail.com>
2024-03-13 01:25:33 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -7,18 +7,20 @@ def solution(num1, num2):
         print(num1)
         print(num2)
         return
+
     max_num, min_num = max(num1, num2), min(num1, num2)
     answer = [0] * 2
     for number in range(1, int(max_num * 0.5) + 1):
         if (min_num % number == 0) and (max_num % number == 0):
             answer[0] = number
+
     counter = 1
     while (max_num * counter) % min_num:
         counter += 1
     answer[1] = max_num * counter
 
-    print(answer[0])
-    print(answer[1])
+    for integer in answer:
+        print(integer)
 
 v1, v2 = map(int, input().split())
 solution(v1, v2)
