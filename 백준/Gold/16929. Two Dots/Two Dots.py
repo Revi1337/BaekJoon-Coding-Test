f1d82e86e027a1cb22c41c626a39612c6348e909
Revi1337 <david122123@gmail.com>
2024-03-19 14:34:57 +0900

    [Gold IV] Title: Two Dots, Time: 2700 ms, Memory: 135412 KB -BaekjoonHub

---
+++ 백준/Gold/16929. Two Dots/Two Dots.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+drow = [-1, 0, 1, 0]
+dcol = [0, 1, 0, -1]
+
+def solution(n, m, board):
+
+    cycle = False
+    check = [[0] * m for _ in range(n)]
+
+    def dfs(row, col, s_row, s_col, counter):
+        for d in range(4):
+            nrow = row + drow[d]
+            ncol = col + dcol[d]
+            if (0 <= nrow < n) and (0 <= ncol < m):
+                if (counter >= 4) and ((nrow, ncol) == (s_row, s_col)):
+                    nonlocal cycle
+                    cycle = True
+                    return
+                if (not check[nrow][ncol]) and (board[row][col] == board[nrow][ncol]):
+                    check[nrow][ncol] = 1
+                    dfs(nrow, ncol, s_row, s_col, counter + 1)
+                    check[nrow][ncol] = 0
+
+    for row in range(n):
+        for col in range(m):
+            check[row][col] = 1
+            dfs(row, col, row, col, 1)
+            check[row][col] = 0
+            if cycle:
+                 return 'Yes'
+
+    return 'No'
+
+n, m = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(n)]
+print(solution(n, m, board))
