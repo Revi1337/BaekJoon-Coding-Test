220d1df18319dfbd7c62deace25201561eb4efe2
Revi1337 <david122123@gmail.com>
2024-04-23 14:58:09 +0900

    Update 참외밭.py

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -14,19 +14,11 @@ def solution(K, directions):
 
     sub_size = 1
     for idx in range(6):
-        if idx == 0:
-            if directions[5][0] == directions[1][0]:
-                sub_size *= directions[idx][1]
-        elif idx == 5:
-            if directions[4][0] == directions[0][0]:
-                sub_size *= directions[idx][1]
-        else:
-            if directions[idx - 1][0] == directions[idx + 1][0]:
-                sub_size *= directions[idx][1]
-
+        if directions[(idx - 1) % 6][0] == directions[(idx + 1) % 6][0]:
+            sub_size *= directions[idx][1]
+            
     return (size - sub_size) * K
 
 K = int(input().rstrip())
 directions = [list(map(int, input().split())) for _ in range(6)]
 print(solution(K, directions))
-
