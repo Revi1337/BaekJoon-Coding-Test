10312926b923b549be0d3bfeeb8999135b0a49e2
Revi1337 <david122123@gmail.com>
2024-05-01 12:42:36 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -3,12 +3,12 @@ import sys
 input = sys.stdin.readline
 
 def solution(N):
-    dp = [[0] * 10 for _ in range(N + 1)]
-    dp[0] = [1] * 10
+    dp = [[1] * 10 for _ in range(N + 1)]
     for i in range(1, N + 1):
-        for j in range(10):
-            dp[i][j] = sum(dp[i - 1][j:])
-    return dp[N][0] % 10_007
+        for j in range(1, 10):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    return dp[N][-1] % 10_007
 
 N = int(input().rstrip())
 print(solution(N))
