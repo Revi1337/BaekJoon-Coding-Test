ca5b3e56ee221665aa3886ac9d3ec6218232ec66
Revi1337 <david122123@gmail.com>
2024-05-14 22:47:03 +0900

    [D2] Title: 조교의 성적 매기기, Time: 124 ms, Memory: 45,008 KB -BaekjoonHub

---
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -0,0 +1,19 @@
+def solution(i, N, K, scores):
+    seed = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+    score = [0] * (N + 1)
+    for idx, (s1, s2, s3) in enumerate(scores, start=1):
+        score[idx] += s1 * 0.35
+        score[idx] += s2 * 0.45
+        score[idx] += s3 * 0.2
+
+    target = score[K]
+    sorted_total = sorted(score, reverse=True)
+    offset = N // 10
+    answer = sorted_total.index(target) // offset
+    return f'#{i} {seed[answer]}'
+
+T = int(input())
+for idx in range(T):
+    N, K = map(int, input().split())
+    scores = [list(map(int, input().split())) for _ in range(N)]
+    print(solution(idx + 1, N, K, scores))
