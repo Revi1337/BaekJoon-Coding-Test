fbfc2a550181f60669be2bfc2fc9b30c087aacaf
Revi1337 <david122123@gmail.com>
2024-05-16 18:47:01 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 197 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,30 +1,30 @@
-def dfs(n):
-    global ans
-    if n==N:
-        ans = max(ans, int("".join(map(str,lst))))
-        return
- 
-    # L개에서 2개뽑는 모든 조합(둘을 교환)
-    for i in range(L-1):
-        for j in range(i+1, L):
-            lst[i], lst[j] = lst[j], lst[i]
- 
-            chk = int("".join(map(str,lst)))*10+n
-            if chk not in v:
-                dfs(n+1)
-                v.append(chk)
- 
-            lst[i], lst[j] = lst[j], lst[i] # 반드시 원상복구!!
- 
-T = int(input())
-for test_case in range(1, T + 1):
-    st, t = input().split()
-    N = int(t)
-    lst = []
-    for ch in st:
-        lst.append(int(ch))     # lst => 정수값
-    L = len(lst)
-    ans = 0
-    v = []
+def solution(seq, num, cnt):
+    lst = list(str(num))
+    length = len(lst)
+    answer = 0
+    check = set()
+
+    def dfs(counter):
+        if counter == cnt:
+            nonlocal answer
+            answer = max(answer, int("".join(map(str, lst))))
+            return
+        for i in range(length - 1):             # lst 에서 2개를 뽑는 모든 조합 (둘을 교환)
+            for j in range(i + 1, length):
+                lst[i], lst[j] = lst[j], lst[i]
+
+                tmp = int("".join(map(str, lst)))
+                if (counter, tmp) not in check:
+                    dfs(counter + 1)
+                    check.add((counter, tmp))
+
+                lst[i], lst[j] = lst[j], lst[i] # 반드시 원상복구
+
     dfs(0)
-    print(f'#{test_case} {ans}')
+
+    return f'#{seq} {answer}'
+
+T = int(input())
+for idx in range(T):
+    num, cnt = map(int, input().split())
+    print(solution(idx + 1, num, cnt))
