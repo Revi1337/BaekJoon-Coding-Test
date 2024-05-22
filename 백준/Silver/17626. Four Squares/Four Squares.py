c3c62d157f852687e997dd5d3ec0ea2c6c9d9e9c
Revi1337 <david122123@gmail.com>
2024-05-22 20:16:39 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 110764 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -3,14 +3,15 @@ import sys
 input = sys.stdin.readline
 
 def solution(n):
-    dp = [0, 1]
+    dp = [0] * (n + 1)
+    dp[1] = 1
     for integer in range(2, n + 1):
         min_value = 1e9
         j = 1
         while (j ** 2) <= integer:
             min_value = min(min_value, dp[integer - (j ** 2)])
             j += 1
-        dp.append(min_value + 1)
+        dp[integer] = min_value + 1
     return dp[n]
 
 n = int(input())
