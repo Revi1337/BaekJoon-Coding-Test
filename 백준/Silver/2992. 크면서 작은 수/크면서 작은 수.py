fa453d7903c9a52ed16b508a1c44cac72a69878c
Revi1337 <david122123@gmail.com>
2024-05-25 00:09:06 +0900

    [Silver III] Title: 크면서 작은 수, Time: 120 ms, Memory: 110272 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -1,14 +1,17 @@
 import sys
-from collections import Counter
 
 input = sys.stdin.readline
 
 def solution(X):
-    frequency = Counter(str(X))
+    frequency = [0] * 10
+    for char in str(X):
+        frequency[int(char)] += 1
     sorted_x = sorted(str(X), reverse=True)
     sorted_x = int("".join(sorted_x))
     for number in range(X + 1, sorted_x + 1):
-        counter = Counter(str(number))
+        counter = [0] * 10
+        for char in str(number):
+            counter[int(char)] += 1
         if counter == frequency:
             return number
     return 0
