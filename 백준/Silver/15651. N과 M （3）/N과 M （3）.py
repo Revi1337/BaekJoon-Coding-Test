6c091ae6088c097b6c528b100c55fbda5eebeb53
Revi1337 <david122123@gmail.com>
2024-05-27 18:08:34 +0900

    [Silver III] Title: N과 M (3), Time: 956 ms, Memory: 259280 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution(N, M):
+
+    def permutation(current):
+        if len(current) == M:
+            answer.append(current[:])
+            return
+        for number in range(1, N + 1):
+            current.append(number)
+            permutation(current)
+            current.pop()
+
+    answer = []
+    permutation([])
+    for line in answer:
+        print(*line)
+
+N, M = map(int, input().split())
+solution(N, M)
