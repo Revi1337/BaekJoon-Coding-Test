62bd94e2a8f94087286c3366c6ae8804175260e3
Revi1337 <david122123@gmail.com>
2024-07-10 18:08:17 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,24 +1,23 @@
 import sys
 
 input = sys.stdin.readline
-def solution(num1, num2):
 
-    """ 최대공약수 """
-    def gcd(num1, num2) -> int:
-        min_num, max_num = min(num1, num2), max(num1, num2)
+def solution(a, b):
+
+    def gcd(a, b): # gcd 는 최대공약수를 의미 (유클리드 호제법)
+        max_num, min_num = max(a, b), min(a, b)
         while max_num % min_num > 0:
             max_num, min_num = min_num, max_num % min_num
         return min_num
 
-    """ 최소공배수 """
-    def lcm(num1, num2) -> int:
-        return (num1 * num2) // gcd(num1, num2)
+    def lcm(a, b, gcd): # lcm 은 최소공배수를 의미. a * b 를 gcd 의 결과로 나누면 된다.
+        return (a * b) // gcd
 
-    gcd_num = gcd(num1, num2)
-    lcm_num = lcm(num1, num2)
+    gcd_answer = gcd(a, b)
+    lcm_answer = lcm(a, b, gcd_answer)
 
-    print(gcd_num)
-    print(lcm_num)
+    print(gcd_answer)
+    print(lcm_answer)
 
-v1, v2 = map(int, input().split())
-solution(v1, v2)
+a, b = map(int, input().split())
+solution(a, b)
