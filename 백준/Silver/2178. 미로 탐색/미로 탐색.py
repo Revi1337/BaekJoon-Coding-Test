66df360ec6c0e8693a2ab234ce0a5a1c2a5f1144
Revi1337 <david122123@gmail.com>
2024-07-23 17:38:24 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 114100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,33 +1,25 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(row_cnt, col_cnt, board):
-    end_row, end_col = row_cnt - 1, col_cnt - 1
-
-    queue = deque()
-    distance = [[0] * col_cnt for _ in range(row_cnt)]
-
-    queue.append((0, 0))
-    distance[0][0] = 1
-    board[0][0] = 0
-
+def solution(N, M, board):
+    queue = deque([(0, 0, 1)])
+    board[0][0] = -1
     while queue:
-        row, col = queue.popleft()
-        if row == end_row and col == end_col:
-            break
+        row, col, answer = queue.popleft()
+        if (row, col) == (N - 1, M - 1):
+            return answer
         for d in range(4):
             nrow = row + drow[d]
             ncol = col + dcol[d]
-            if (0 <= nrow < row_cnt) and (0 <= ncol < col_cnt) and (board[nrow][ncol]):
-                if distance[nrow][ncol] == 0:
-                    distance[nrow][ncol] = distance[row][col] + 1
-                    board[nrow][ncol] = 0
-                    queue.append((nrow, ncol))
+            if (0 <= nrow < N) and (0 <= ncol < M) and (board[nrow][ncol] == 1):
+                board[nrow][ncol] = -1
+                queue.append((nrow, ncol, answer + 1))
 
-    return distance[end_row][end_col]
-
-n, m = map(int, input().split())
-board = [list(map(int, input())) for _ in range(n)]
-print(solution(n, m, board))
+N, M = map(int, input().split())
+board = [list(map(int, input().strip())) for _ in range(N)]
+print(solution(N, M, board))
