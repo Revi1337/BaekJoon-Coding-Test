ff157890e348ba356c9678cbab22f63d52392c3a
Revi1337 <david122123@gmail.com>
2024-07-23 14:46:54 +0900

    [Silver IV] Title: 빙고, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution(board, questions):
+    counter = 0
+    for question in questions:
+        for q in question:
+            counter += 1
+            for row in range(5):
+                for col in range(5):
+                    if board[row][col] == q:
+                        board[row][col] = 0
+
+                        answer = 0
+                        for line in board:
+                            if sum(line) == 0:
+                                answer += 1
+                                if answer == 3:
+                                    return counter
+
+                        for col in range(5):
+                            if sum([line[col] for line in board]) == 0:
+                                answer += 1
+                                if answer == 3:
+                                    return counter
+
+                        if sum([board[row][5 - row - 1] for row in range(5)]) == 0:
+                            answer += 1
+                            if answer == 3:
+                                return counter
+
+                        if sum([board[row][row] for row in range(5)]) == 0:
+                            answer += 1
+                            if answer == 3:
+                                return counter
+
+board = [list(map(int, input().split())) for _ in range(5)]
+questions = [list(map(int, input().split())) for _ in range(5)]
+print(solution(board, questions))
