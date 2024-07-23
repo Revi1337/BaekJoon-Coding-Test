9904061e635682b71091cfac56d418ea6f35708a
Revi1337 <david122123@gmail.com>
2024-07-23 17:55:05 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,49 +1,30 @@
-def solution_graph_bfs(n, edges):
-    from collections import deque
-    graph = [[] for _ in range(n + 1)]
+import sys
+
+input = sys.stdin.readline
+
+drow = [-1, 0, 1, 0]
+dcol = [0, 1, 0, -1]
+
+def solution(N, M, edges):
+    graphs = [[] for _ in range(N + 1)]
     for v1, v2 in edges:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
+        graphs[v1].append(v2)
+        graphs[v2].append(v1)
 
-    queue = deque()
-    queue.append(1)
-    check = [0] * (n + 1)
-    check[1] = 1
-    answer = -1
-    while queue:
-        node_cnt = len(queue)
-        answer += node_cnt
-        for _ in range(node_cnt):
-            node = queue.popleft()
-            for next_node in graph[node]:
-                if check[next_node] == 0:
-                    check[next_node] = 1
-                    queue.append(next_node)
-    return answer
-
-
-def solution_graph_dfs(n, edges):
-    graph = [[] for _ in range(n + 1)]
-    for v1, v2 in edges:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-
-    answer = -1
-    check = [0] * (n + 1)
-    def DFS(v):
+    def dfs(entry):
         nonlocal answer
-        check[v] = 1
+        check[entry] = 1
         answer += 1
-        for vertext in graph[v]:
-            if check[vertext] == 0:
-                DFS(vertext)
+        for nv in graphs[entry]:
+            if not check[nv]:
+                dfs(nv)
 
-    DFS(1)
-    return answer
+    check = [0] * (N + 1)
+    answer = 0
+    dfs(1)
+    return answer - 1
 
-n = int(input())
-edge_count = int(input())
-edges = [list(map(int, input().split())) for _ in range(edge_count)]
-print(solution_graph_bfs(n, edges))
-print(solution_graph_dfs(n, edges))
-
+N = int(input())
+M = int(input())
+edges = [list(map(int, input().split())) for _ in range(M)]
+print(solution(N, M, edges))
