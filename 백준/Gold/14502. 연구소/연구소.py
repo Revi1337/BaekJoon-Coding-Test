e60cc77bdbdf7226d3c8a9f428f9f59572565480
Revi1337 <david122123@gmail.com>
2024-07-26 02:27:07 +0900

    [Gold IV] Title: 연구소, Time: 1584 ms, Memory: 116408 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,5 +1,4 @@
 import sys
-from copy import deepcopy
 from collections import deque
 
 input = sys.stdin.readline
@@ -7,54 +6,52 @@ input = sys.stdin.readline
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(n, m, board):
-
-    answer = 0
-    virus_positions = []
-
-    def bfs():
-        new_board = deepcopy(board)
-        queue = deque()
-        for row, col in virus_positions:
-            queue.append([row, col])
-        while queue:
-            row, col = queue.popleft()
-            for d in range(4):
-                nrow = row + drow[d]
-                ncol = col + dcol[d]
-                if (0 <= nrow < n) and (0 <= ncol < m):
-                    if new_board[nrow][ncol] == 0:
-                        new_board[nrow][ncol] = 2
-                        queue.append([nrow, ncol])
-
-        counter = 0
-        for row in range(n):
-            for col in range(m):
-                if new_board[row][col] == 0:
-                    counter += 1
-        nonlocal answer
-        answer = max(answer, counter)
+def solution(N, M, board):
+    virus = []
+    answer = -1e9
+    for row in range(N):
+        for col in range(M):
+            if board[row][col] == 2:
+                virus.append((row, col))
 
     def dfs(count):
         if count == 3:
             bfs()
             return
-        for row in range(n):
-            for col in range(m):
+        for row in range(N):
+            for col in range(M):
                 if board[row][col] == 0:
                     board[row][col] = 1
                     dfs(count + 1)
                     board[row][col] = 0
 
-    for row in range(n):
-        for col in range(m):
-            if board[row][col] == 2:
-                virus_positions.append([row, col])
+    def bfs():
+        queue = deque(virus)
+        check = [[0] * M for _ in range(N)]
+        for row in range(N):
+            for col in range(M):
+                check[row][col] = board[row][col]
+
+        while queue:
+            row, col = queue.popleft()
+            for d in range(4):
+                nrow, ncol = row + drow[d], col + dcol[d]
+                if (0 <= nrow < N) and (0 <= ncol < M) and (not check[nrow][ncol]):
+                    check[nrow][ncol] = 2
+                    queue.append((nrow, ncol))
+
+        ans = 0
+        for row in range(N):
+            for col in range(M):
+                if check[row][col] == 0:
+                    ans += 1
+        nonlocal answer
+        answer = max(answer, ans)
 
     dfs(0)
 
     return answer
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
-print(solution(n, m, board))
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, M, board))
