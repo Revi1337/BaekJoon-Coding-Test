95a251d2f77ef225b9390dfb8fdfc8715de72750
Revi1337 <david122123@gmail.com>
2024-07-26 21:21:42 +0900

    [Silver I] Title: 안전 영역, Time: 248 ms, Memory: 114740 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,87 +1,50 @@
-########################## DFS Version ##########################
 import sys
-
-sys.setrecursionlimit(10 ** 5)
-
-drow = [-1, 0, 1, 0]
-dcol = [0, 1, 0, -1]
-
-def dfs(row, col):
-    check[row][col] = 1
-    for d in range(4):
-        nrow = row + drow[d]
-        ncol = col + dcol[d]
-        if (0 <= nrow < n) and (0 <= ncol < n) and (blocks[nrow][ncol] > height) and (check[nrow][ncol] == 0):
-            dfs(nrow, ncol)
-
-minN = 100_000_000
-maxN = -100_000_000
-n = int(input())
-blocks = []
-for _ in range(n):
-    integers = list(map(int, input().split()))
-    for integer in integers:
-        if integer < minN:
-            minN = integer
-        if integer > maxN:
-            maxN = integer
-    blocks.append(integers)
-
-res = -100_000_000
-for height in range(0, maxN + 1):
-    check = [[0] * n for _ in range(n)]
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if check[i][j] == 0 and blocks[i][j] > height:
-                dfs(i, j)
-                cnt += 1
-    res = max(res, cnt)
-
-print(res)
-
-
-########################## BFS Version ##########################
 from collections import deque
 
+input = sys.stdin.readline
+
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def valid_direction():
-    return (0 <= nrow < n) and (0 <= ncol < n) and (blocks[nrow][ncol] > height) and (check[nrow][ncol] == 0)
+def solution(N, board):
+    answer = -1e9
+    nboard = [[0] * N for _ in range(N)]
+    maxN = -1e9
+    for line in board:
+        maxN = max(maxN, max(line))
 
-n = int(input())
-minN = 100_000_000
-maxN = -100_000_000
-blocks = []
-for _ in range(n):
-    integers = list(map(int, input().split()))
-    minN = min(minN, min(integers))
-    maxN = max(maxN, max(integers))
-    blocks.append(integers)
+    def bfs(r, c):
+        queue = deque([(r, c)])
+        nboard[r][c] = -1
+        while queue:
+            row, col = queue.popleft()
+            for d in range(4):
+                nrow = row + drow[d]
+                ncol = col + dcol[d]
+                if (0 <= nrow < N) and (0 <= ncol < N) and (nboard[nrow][ncol] != -1):
+                    nboard[nrow][ncol] = -1
+                    queue.append((nrow, ncol))
 
-answer = -100_000_000
-for height in range(0, maxN + 1):
-    check = [[0] * n for _ in range(n)]
-    queue = deque()
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if check[i][j] == 0 and blocks[i][j] > height:
-                queue.append([i, j])
-                check[i][j] = 0
-                cnt += 1
-                while queue:
-                    row, col = queue.popleft()
-                    for d in range(4):
-                        nrow = row + drow[d]
-                        ncol = col + dcol[d]
-                        if valid_direction():
-                            check[nrow][ncol] = 1
-                            queue.append([nrow, ncol])
-                answer = max(answer, cnt)
+    pre = 0
+    for depth in range(maxN):
+        for row in range(N):
+            for col in range(N):
+                if board[row][col] <= depth:
+                    nboard[row][col] = -1
+                else:
+                    nboard[row][col] = board[row][col]
+        for row in range(N):
+            for col in range(N):
+                if nboard[row][col] != -1:
+                    pre += 1
+                    bfs(row, col)
 
-print(answer)
+        answer = max(answer, pre)
+        pre = 0
 
-########################## BFS Version ##########################
+    return answer
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, board))
 
