a7bc2baa07df7f509c451f94fdcd462ec060605f
Revi1337 <david122123@gmail.com>
2024-07-26 23:58:44 +0900

    [Silver II] Title: 섬의 개수, Time: 176 ms, Memory: 114256 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,39 +1,38 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
 drow = [-1, -1, 0, 1, 1, 1, 0, -1]
 dcol = [0, 1, 1, 1, 0, -1, -1, -1]
 
-def solution(w, h, datas):
-
-    def DFS(row, col):
-        datas[row][col] = 0
-        for d in range(8):
-            nrow = row + drow[d]
-            ncol = col + dcol[d]
-            if (
-                    (0 <= nrow < h)
-                    and
-                    (0 <= ncol < w)
-                    and
-                    (datas[nrow][ncol] != 0)
-            ):
-                DFS(nrow, ncol)
+def solution(w, h, board):
 
     answer = 0
+
+    def bfs(r, c):
+        board[r][c] = -1
+        queue = deque([(r, c)])
+        while queue:
+            row, col = queue.popleft()
+            for d in range(8):
+                nrow = row + drow[d]
+                ncol = col + dcol[d]
+                if (0 <= nrow < h) and (0 <= ncol < w) and (board[nrow][ncol] == 1):
+                    board[nrow][ncol] = -1
+                    queue.append((nrow, ncol))
+
     for row in range(h):
         for col in range(w):
-            if datas[row][col] == 1:
+            if board[row][col] == 1:
                 answer += 1
-                DFS(row, col)
+                bfs(row, col)
 
     return answer
 
-input = sys.stdin.readline
 while True:
-    w, h = map(int, input().rstrip().split())
-    if [w, h] == [0,0]:
+    w, h = map(int, input().split())
+    if  (w, h) == (0, 0):
         break
-    datas = [list(map(int, input().rstrip().split())) for _ in range(h)]
-    print(solution(w, h, datas))
+    board = [list(map(int, input().split())) for _  in range(h)]
+    print(solution(w, h, board))
