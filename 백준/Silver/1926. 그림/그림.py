627119a51179d91ec8635204cbe89a3044f352f7
Revi1337 <david122123@gmail.com>
2024-07-28 21:11:09 +0900

    [Silver I] Title: 그림, Time: 580 ms, Memory: 99216 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,34 +1,36 @@
 import sys
-from collections import deque
 
-input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(N, M, board):
-    answer = 0
-    ans = 0
-    for row in range(N):
-        for col in range(M):
-            if board[row][col]:
-                answer += 1
-                board[row][col] = 0
-                queue = deque([(row, col)])
-                pre = 1
-                while queue:
-                    r, c = queue.popleft()
-                    for d in range(4):
-                        nr = r + drow[d]
-                        nc = c + dcol[d]
-                        if (0 <= nr < N) and (0 <= nc < M) and (board[nr][nc] == 1):
-                            pre += 1
-                            board[nr][nc] = 0
-                            queue.append((nr, nc))
-                ans = max(ans, pre)
-    print(answer)
-    print(ans)
+def solution(row_length, col_length, board):
+    def dfs(row, col):
+        nonlocal breath
+        breath += 1
+        board[row][col] = 0
+        for d in range(4):
+            nrow = row + drow[d]
+            ncol = col + dcol[d]
+            if (0 <= nrow < row_length) and (0 <= ncol < col_length) and (board[nrow][ncol]):
+                dfs(nrow, ncol)
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-solution(N, M, board)
+    counter = 0
+    answer = 0
+    breath = 0
+    for row in range(row_length):
+        for col in range(col_length):
+            if board[row][col] == 1:
+                dfs(row, col)
+                counter += 1
+                if breath > answer:
+                    answer = breath
+                breath = 0
+
+    print(counter)
+    print(answer)
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+solution(n, m, board)
