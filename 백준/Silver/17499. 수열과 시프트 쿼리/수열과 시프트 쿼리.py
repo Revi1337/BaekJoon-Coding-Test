2716f4a4d7f260603fba8a5b51302555eb056e0e
Revi1337 <david122123@gmail.com>
2024-07-28 16:38:05 +0900

    [Silver III] Title: 수열과 시프트 쿼리, Time: 312 ms, Memory: 145468 KB -BaekjoonHub

---
+++ 백준/Silver/17499. 수열과 시프트 쿼리/수열과 시프트 쿼리.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution(N, Q, integers, operations):
+    left = 0
+    for operation in operations:
+        if operation[0] == 1:
+            target = (left + (operation[1] - 1)) % N
+            integers[target] += operation[2]
+        elif operation[0] == 2:
+            left = (left - operation[1]) % N
+        elif operation[0] == 3:
+            left = (left + operation[1]) % N
+
+    for idx in range(left, N):
+        print(integers[idx], end = ' ')
+    for idx in range(left):
+        print(integers[idx], end = ' ')
+    print()
+
+N, Q = map(int, input().split())
+integers = list(map(int, input().split()))
+operations = [list(map(int, input().split())) for _ in range(Q)]
+solution(N, Q, integers, operations)
