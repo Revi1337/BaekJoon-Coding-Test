90170461b289a09daae6784e2b2b927f473eb328
Revi1337 <david122123@gmail.com>
2024-08-06 14:01:46 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 120 ms, Memory: 121172 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -2,27 +2,28 @@ import sys
 
 input = sys.stdin.readline
 
-"""
-가로등의 위치 X 에서 idx 1 의 위치는 따로 검사한다.
-나머지는 이분탐색으로 풀면 될 것 같다.
-"""
-def solution(N, M, X):
-    left, right, mid = 1, N, 0
-    answer = float('inf')
-    flag = False
+'''
+어두운 굴다리 (https://www.acmicpc.net/problem/17266)
+'''
+
+def solution(N, M, x):
+    left, right = 1, N
+    answer = 1e9
     while left <= right:
         mid = (left + right) // 2
-        flag = True
-        if X[0] - mid > 0:
-            flag = False
-        elif X[M - 1] + mid < N:
-            flag = False
-        else:
-            for idx in range(1, M - 1):
-                if X[idx] + mid < X[idx + 1] - mid:
-                    flag = False
-                    break
-        if flag:
+        predicate = True
+        for idx in range(M):
+            if x[0] - mid > 0:
+                predicate = False
+                break
+            if x[-1] + mid < N:
+                predicate = False
+                break
+            if x[idx - 1] + mid < x[idx] - mid:
+                predicate = False
+                break
+
+        if predicate:
             answer = min(mid, answer)
             right = mid - 1
         else:
@@ -30,7 +31,7 @@ def solution(N, M, X):
 
     return answer
 
-N = int(input().rstrip())
-M = int(input().rstrip())
-X = list(map(int, input().split()))
-print(solution(N, M, X))
+N = int(input())
+M = int(input())
+x = list(map(int, input().split()))
+print(solution(N, M, x))
