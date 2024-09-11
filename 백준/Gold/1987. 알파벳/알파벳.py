99906c03f90c61f3bd022075b4c551d1d9f4d60e
Revi1337 <david122123@gmail.com>
2024-09-11 14:34:58 +0900

    [Gold IV] Title: 알파벳, Time: 6260 ms, Memory: 174192 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -10,24 +10,26 @@ input = sys.stdin.readline
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(R, C, boards):
+def solution2(R, C, boards):
 
-    def dfs(row, col):
+    def dfs(row, col, counter):
         nonlocal answer
-        answer = max(answer, len(cache))
+        answer = max(answer, counter)
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
-            if 0 <= nrow < R and 0 <= ncol < C and boards[nrow][ncol] not in cache:
-                cache.add(boards[nrow][ncol])
-                dfs(nrow, ncol)
-                cache.discard(boards[nrow][ncol])
+            if 0 <= nrow < R and 0 <= ncol < C:
+                if cache[(65 - ord(boards[nrow][ncol])) % 26] == 0:
+                    cache[(65 - ord(boards[nrow][ncol])) % 26] = 1
+                    dfs(nrow, ncol, counter + 1)
+                    cache[(65 - ord(boards[nrow][ncol])) % 26] = 0
 
     answer = 1
-    cache = set(boards[0][0])
-    dfs(0, 0)
+    cache = [0] * 26
+    cache[(65 - ord(boards[0][0])) % 26] = 1
+    dfs(0, 0, 1)
 
     return answer
 
 R, C = map(int, input().split())
 boards = [list(input().rstrip()) for _ in range(R)]
-print(solution(R, C, boards))
+print(solution2(R, C, boards))
