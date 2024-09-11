2812bc5b66a1b6160117ad80b0ee35ccd963dad4
Revi1337 <david122123@gmail.com>
2024-09-11 14:26:37 +0900

    [Gold IV] Title: 알파벳, Time: 7480 ms, Memory: 170256 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,28 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+"""
+알파벳 (https://www.acmicpc.net/problem/1987)
+2024-09-11
+"""
+
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(r, c, board):
-    answer = 0
-    duplicate = [0] * 26
+def solution(R, C, boards):
 
-    def dfs(row, col, counter):
+    def dfs(row, col):
         nonlocal answer
-        answer = max(answer, counter)
+        answer = max(answer, len(cache))
         for d in range(4):
-            nrow = row + drow[d]
-            ncol = col + dcol[d]
-            if (0 <= nrow < r) and (0 <= ncol < c):
-                if not duplicate[ord(board[nrow][ncol]) % 26]:
-                    duplicate[ord(board[nrow][ncol]) % 26] = 1
-                    dfs(nrow, ncol, counter + 1)
-                    duplicate[ord(board[nrow][ncol]) % 26] = 0
+            nrow, ncol = row + drow[d], col + dcol[d]
+            if 0 <= nrow < R and 0 <= ncol < C and boards[nrow][ncol] not in cache:
+                cache.add(boards[nrow][ncol])
+                dfs(nrow, ncol)
+                cache.discard(boards[nrow][ncol])
 
-    duplicate[ord(board[0][0]) % 26] = 1
-    dfs(0, 0, 1)
-    duplicate[ord(board[0][0]) % 26] = 0
+    answer = 1
+    cache = set(boards[0][0])
+    dfs(0, 0)
 
     return answer
 
-r, c = map(int, input().split())
-board = [list(input()) for _ in range(r)]
-print(solution(r, c, board))
+R, C = map(int, input().split())
+boards = [list(input().rstrip()) for _ in range(R)]
+print(solution(R, C, boards))
