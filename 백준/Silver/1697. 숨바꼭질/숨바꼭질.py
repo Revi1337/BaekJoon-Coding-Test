c3601969fe07d304b4650c2be3c7abde9c38b012
Revi1337 <david122123@gmail.com>
2024-09-11 13:12:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 115388 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,16 +1,25 @@
+import sys
 from collections import deque
 
-def solution(n, k):
-    distance = [0] * 100_001
-    queue = deque([n])
-    while queue:
-        curr_distance = queue.popleft()
-        if curr_distance == k:
-            return distance[k]
-        for next_distance in [curr_distance - 1, curr_distance + 1, curr_distance * 2]:
-            if 0 <= next_distance <= 100_000 and not distance[next_distance]:
-                distance[next_distance] = distance[curr_distance] + 1
-                queue.append(next_distance)
+input = sys.stdin.readline
 
-n, k = map(int, input().split())
-print(solution(n, k))
+"""
+숨바꼭질 (https://www.acmicpc.net/problem/1697)
+2024-09-11
+"""
+
+def solution(N, K):
+    queue = deque([N])
+    distance = [-1] * 100_001
+    distance[N] = 0
+    while queue:
+        d = queue.popleft()
+        if d == K:
+            return distance[d]
+        for nd in [d * 2, d + 1, d - 1]:
+            if 0 <= nd <= 100_000 and distance[nd] == -1:
+                distance[nd] = distance[d] + 1
+                queue.append(nd)
+
+N, K = map(int, input().split())
+print(solution(N, K))
