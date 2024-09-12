d9617886d1b41fff0ba4686d98fdd670276b06dc
Revi1337 <david122123@gmail.com>
2024-09-12 12:08:40 +0900

    [Silver I] Title: 스타트링크, Time: 208 ms, Memory: 134852 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+"""
+스타트링크 (https://www.acmicpc.net/problem/5014)
+2024-09-11
+"""
+
+def solution(F, S, G, U, D):
+    distance = [-1] * 2_000_001
+    distance[S] = 0
+    queue = deque([S])
+    while queue:
+        current = queue.popleft()
+        if current == G:
+            return distance[current]
+        for ndir in [current + U, current - D]:
+            if (1 <= ndir <= F) and (distance[ndir] == -1):
+                queue.append(ndir)
+                distance[ndir] = distance[current] + 1
+
+    return 'use the stairs'
+
+F, S, G, U, D = map(int, input().split())
+print(solution(F, S, G, U, D))
