e736197449a42e25dcf600d833df2e20177a5fd2
Revi1337 <david122123@gmail.com>
2024-09-16 15:43:55 +0900

    [Silver II] Title: 부분수열의 합, Time: 148 ms, Memory: 112020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -9,29 +9,21 @@ input = sys.stdin.readline
 
 def solution(N, S, nums):
 
-    def recursive(depth, max_depth, index, lst):
-        if depth == max_depth:
-            answer.append(lst)
+    def recursive(index, sm, cnt):
+        if index == N:
+            if cnt and sm == S:
+                nonlocal answer
+                answer += 1
             return
-        for idx in range(N):
-            if not check[idx] and idx >= index:
-                check[idx] = 1
-                recursive(depth + 1, max_depth, idx, lst + [nums[idx]])
-                check[idx] = 0
+        recursive(index + 1, sm + nums[index], cnt + 1)
+        recursive(index + 1, sm, cnt)
 
-    answer = []
-    check = [0] * N
-    for depth in range(1, N + 1):
-        recursive(0, depth, 0, [])
-
-    ans = 0
-    for line in answer:
-        if sum(line) == S:
-            ans += 1
-    print(ans)
-
+    answer = 0
+    recursive(0, 0, 0)
+    print(answer)
 
 N, S = map(int, input().split())
 nums = list(map(int, input().split()))
 solution(N, S, nums)
+
 
