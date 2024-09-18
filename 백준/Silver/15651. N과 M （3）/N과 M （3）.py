587e4b3dcf4a081dd06ddc2a2b442b7362e2f6b2
Revi1337 <david122123@gmail.com>
2024-09-18 13:15:26 +0900

    [Silver III] Title: N과 M (3), Time: 872 ms, Memory: 271696 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -2,21 +2,25 @@ import sys
 
 input = sys.stdin.readline
 
+"""
+N 과 M (3) (https://www.acmicpc.net/problem/15651)
+2024-09-18
+"""
+
 def solution(N, M):
 
-    def permutation(current):
-        if len(current) == M:
-            answer.append(current[:])
+    def recursive(depth, lst):
+        if depth == M:
+            answer.append(lst)
             return
         for number in range(1, N + 1):
-            current.append(number)
-            permutation(current)
-            current.pop()
+            recursive(depth + 1, lst + [number])
 
     answer = []
-    permutation([])
+    recursive(0, [])
     for line in answer:
         print(*line)
+
 
 N, M = map(int, input().split())
 solution(N, M)
