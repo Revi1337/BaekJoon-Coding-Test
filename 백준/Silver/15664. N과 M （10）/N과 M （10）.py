7e141e808d35ea3dd389c7bc5c80eb7ff763ffdb
Revi1337 <david122123@gmail.com>
2024-09-25 22:00:45 +0900

    [Silver II] Title: N과 M (10), Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+"""
+N 과 M (9) (https://www.acmicpc.net/problem/15664)
+2024-09-25
+"""
+
+def solution(N, M, numbers):
+    numbers.sort()
+
+    def recursive(depth, idx, lst):
+        if depth == M:
+            answer.append(lst)
+            return
+        prev = 0
+        for idx in range(idx, N):
+            if not check[idx] and prev != numbers[idx]:
+                prev = numbers[idx]
+                check[idx] = 1
+                recursive(depth + 1, idx, lst + [numbers[idx]])
+                check[idx] = 0
+
+    answer = []
+    check = [0] * N
+    recursive(0, 0, [])
+    for ans in answer:
+        print(*ans)
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+solution(N, M, numbers)
