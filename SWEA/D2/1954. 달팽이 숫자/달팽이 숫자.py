7b9cc01af90ff1913d58dc14d677b266b9627d76
Revi1337 <david122123@gmail.com>
2024-11-13 14:00:12 +0900

    [D2] Title: 달팽이 숫자, Time: 105 ms, Memory: 44,776 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,21 +1,26 @@
 drow = [0, 1, 0, -1]
 dcol = [1, 0, -1, 0]
+
 def solution(idx, N):
-    check = [[0] * N for _ in range(N)]
-    row, col, cnt, d = 0, 0, 1, 0
-    check[row][col] = cnt
-    cnt += 1
-    while cnt <= N ** 2:
-        nrow = row + drow[d]
-        ncol = col + dcol[d]
-        if (0 <= nrow < N) and (0 <= ncol < N) and (not check[nrow][ncol]):
-            row, col = nrow, ncol
-            check[nrow][ncol] = cnt
-            cnt += 1
+    d = 0
+    answer = [[0] * N for _ in range(N)]
+    init = 1
+
+    direction = [0, 0]
+    answer[0][0] = init
+    init += 1
+
+    while init <= N ** 2:
+        nrow, ncol = direction[0] + drow[d], direction[1] + dcol[d]
+        if (0 <= nrow < N) and (0 <= ncol < N) and (not answer[nrow][ncol]):
+            answer[nrow][ncol] = init
+            direction = [nrow, ncol]
+            init += 1
         else:
             d = (d + 1) % 4
+
     print(f'#{idx}')
-    for line in check:
+    for line in answer:
         print(*line)
 
 T = int(input())
