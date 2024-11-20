c40c44b5eeddaa1a0c686613ba331b3794c69849
Revi1337 <david122123@gmail.com>
2024-11-20 21:53:05 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 169 ms, Memory: 49,752 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,25 +1,16 @@
-def solution(seq, board):
+def solution(board):
     answer = 0
-    # 행
-    for row in board:
-        answer = max(answer, sum(row))
-    # 열
+    for line in board:
+        answer = max(answer, sum(line))
     for col in range(100):
-        answer = max(answer, sum([row[col] for row in board]))
-    # 좌 대각선
-    pre = 0
-    for offset in range(100):
-        pre += board[offset][offset]
-    answer = max(answer, pre)
-    # 우 대각선
-    pre = 0
-    for offset in range(100):
-        pre += board[offset][100 - offset - 1]
-    answer = max(answer, pre)
-    return f'#{seq} {answer}'
+        answer = max(answer, sum([line[col] for line in board]))
+    answer = max(answer, sum([board[idx][idx] for idx in range(100)]))
+    answer = max(answer, sum([board[idx][99 - idx] for idx in range(100)]))
+
+    return answer
 
 T = 10
 for _ in range(T):
-    s = int(input())
+    seq = int(input())
     board = [list(map(int, input().split())) for _ in range(100)]
-    print(solution(s, board))
+    print(f'#{seq} {solution(board)}')
