3cc23e0693e29464dfb2216049a84088132825b2
Revi1337 <david122123@gmail.com>
2024-11-21 01:28:24 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 156 ms, Memory: 49,272 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -0,0 +1,41 @@
+drow = [0, 1, 0]
+dcol = [-1, 0, 1]
+
+def solution(board):
+    for line in board:
+        line.insert(0, 0)
+        line.append(0)
+
+    end = None
+    for row in range(100):
+        for col in range(102):
+            if board[row][col] == 2:
+                end = [row, col]
+                break
+
+    possible = []
+    for col in range(102):
+        if board[0][col] == 1:
+            possible.append([0, col])
+
+    for poss in possible:
+        d = 1
+        curr = [poss[0], poss[1]]
+
+        while curr[0] < 100:
+            if curr[1] > 0 and board[curr[0]][curr[1] - 1] == 1:  # 왼쪽 이동 가능?
+                while curr[1] > 0 and board[curr[0]][curr[1] - 1] == 1:
+                    curr[1] -= 1
+            elif curr[1] < 101 and board[curr[0]][curr[1] + 1] == 1:  # 오른쪽 이동 가능?
+                while curr[1] < 101 and board[curr[0]][curr[1] + 1] == 1:
+                    curr[1] += 1
+            curr[0] += 1 # 아래로 이동
+
+        if curr[1] == end[1]:
+            return poss[1] - 1
+
+T = 10
+for _ in range(T):
+    seq = int(input())
+    board = [list(map(int, input().split())) for _ in range(100)]
+    print(f'#{seq} {solution(board)}')
