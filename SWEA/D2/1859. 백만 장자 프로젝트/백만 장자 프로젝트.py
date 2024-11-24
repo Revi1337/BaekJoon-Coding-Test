eee2c6a4bb09077db3107c082640692a0520f5dd
Revi1337 <david122123@gmail.com>
2024-11-24 16:29:59 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,268 ms, Memory: 208,656 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,12 +1,10 @@
 def solution(N, prices):
-    max_price = prices[-1]
-    answer = 0
-    for price in prices[-2::-1]:
+    max_price = answer = 0
+    for price in prices[::-1]:
         if price > max_price:
             max_price = price
-            continue
-
-        answer += max_price - price
+        else:
+            answer += max_price - price
 
     return answer
 
