e80c44bf98909ead674ddba9a2c2676c40433308
Revi1337 <david122123@gmail.com>
2024-11-25 23:49:08 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,13 @@
 def solution(N, M, cards):
-    possible = []
-    for i in range(N):
-        for j in range(i + 1, N):
+    answer = 0
+    for i in range(N - 2):
+        for j in range(i + 1, N - 1):
             for k in range(j + 1, N):
                 cost = cards[i] + cards[j] + cards[k]
-                if cost <= M:
-                    possible.append(cost)
+                if answer < cost <= M:
+                    answer = cost
 
-    return max(possible)
-
+    return answer
 
 N, M = map(int, input().split())
 cards = list(map(int, input().split()))
