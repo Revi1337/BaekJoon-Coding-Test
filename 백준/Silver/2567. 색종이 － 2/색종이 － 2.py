d760ccb0cd75ab37f582af7b2d5c81c8e4d7f370
Revi1337 <david122123@gmail.com>
2024-11-27 04:27:27 +0900

    [Silver IV] Title: 색종이 - 2, Time: 96 ms, Memory: 110712 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,25 +1,27 @@
-drow = [-1, 0, 1, 0]
-dcol = [0, 1, 0, -1]
-
 def solution(N, pos):
     paper = [[0] * 102 for _ in range(102)]
+
     for x, y in pos:
         for row in range(y + 1, y + 11):
             for col in range(x + 1, x + 11):
                 paper[row][col] = 1
 
+    rev_paper = list(zip(*paper))
+
     answer = 0
-    for row in range(102):
-        for col in range(102):
-            if paper[row][col]:
-                for d in range(4):
-                    nrow, ncol = row + drow[d], col + dcol[d]
-                    if paper[nrow][ncol] == 0:
-                        answer += 1
+    for line in paper:
+        for idx in range(1, len(line)):
+            if line[idx] != line[idx - 1]:
+                answer += 1
+
+    for line in rev_paper:
+        for idx in range(1, len(line)):
+            if line[idx] != line[idx - 1]:
+                answer += 1
 
     return answer
+
 
 N = int(input())
 pos = [list(map(int, input().split())) for _ in range(N)]
 print(solution(N, pos))
-
