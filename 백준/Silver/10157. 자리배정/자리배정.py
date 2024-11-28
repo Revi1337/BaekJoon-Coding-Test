3f6d10cdeb6c2bbe48fe4455ae510695ee1c64cd
Revi1337 <david122123@gmail.com>
2024-11-28 19:51:18 +0900

    [Silver III] Title: 자리배정, Time: 112 ms, Memory: 116156 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.py
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -1,32 +1,30 @@
-import sys
-
-input = sys.stdin.readline
-
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+drow = [-1, 0 ,1, 0]
+dcol = [0, 1, 0, -1]
 
 def solution(C, R, K):
-    d = 0
-    board = [[0] * C for _ in range(R)]
-    pos = [R - 1, 0]
-    start = 1
-    board[pos[0]][pos[1]] = start
-    start += 1
-    while start <= C * R:
-        nrow, ncol = pos[0] + direction[d][0], pos[1] + direction[d][1]
-        if (not ((0 <= nrow < R) and (0 <= ncol < C))) or board[nrow][ncol]:
+    board = [[0] * R for _ in range(C)]
+
+    d = init = 1
+    board[0][0] = init
+    init += 1
+    curr = [0, 0]
+
+    while init <= R * C:
+        nrow, ncol = curr[0] + drow[d], curr[1] + dcol[d]
+        if not (0 <= nrow < C and 0 <= ncol < R) or board[nrow][ncol]:
             d = (d + 1) % 4
         else:
-            pos[0], pos[1] = nrow, ncol
-            board[pos[0]][pos[1]] = start
-            start += 1
+            board[nrow][ncol] = init
+            curr = [nrow, ncol]
+            init += 1
 
-    if K > C * R:
-        print(0)
-    else:
-        for row in range(R):
-            for col in range(C):
-                if board[row][col] == K:
-                    print(f'{col + 1} {R - row} ')
+    for row in range(C):
+        for col in range(R):
+            if board[row][col] == K:
+                print(row + 1, col + 1, sep = ' ')
+                return
+
+    print(0)
 
 C, R = map(int, input().split())
 K = int(input())
