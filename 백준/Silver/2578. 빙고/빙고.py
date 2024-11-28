d7c0d50d3f5589287296a69787fddddb0decb564
Revi1337 <david122123@gmail.com>
2024-11-29 01:11:25 +0900

    [Silver IV] Title: 빙고, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,42 +1,30 @@
-import sys
+def solution(board, numbers):
+    numbers = [num for line in numbers for num in line]
+    lru = [[0] * 5 for _ in range(5)]
 
-input = sys.stdin.readline
-
-def solution(board, questions):
-    counter = 0
-    for question in questions:
-        for q in question:
-            for row in range(5):
-                for col in range(5):
-                    if board[row][col] == q:
-                        counter += 1
-                        board[row][col] = 0
-
-            answer = 0
-            for line in board:
-                if sum(line) == 0:
-                    answer += 1
-                    if answer == 3:
-                        return counter
-
+    for idx in range(25):
+        num = numbers[idx]
+        for row in range(5):
             for col in range(5):
-                if sum([line[col] for line in board]) == 0:
-                    answer += 1
-                    if answer == 3:
-                        return counter
+                if board[row][col] == num:
+                    lru[row][col] = 1
 
-            if sum([board[row][5 - row - 1] for row in range(5)]) == 0:
-                answer += 1
-                if answer == 3:
-                    return counter
+                    bing_cnt = 0
+                    if sum([lru[c][c] for c in range(5)]) == 5:
+                        bing_cnt += 1
+                    if sum([lru[c][5 - c - 1] for c in range(5)]) == 5:
+                        bing_cnt += 1
 
-            if sum([board[row][row] for row in range(5)]) == 0:
-                answer += 1
-                if answer == 3:
-                    return counter
+                    for c in range(5):
+                        r = c
+                        if sum(lru[r]) == 5:
+                            bing_cnt += 1
+                        if sum([cache[c] for cache in lru]) == 5:
+                            bing_cnt += 1
+
+                        if bing_cnt >= 3:
+                            return idx + 1
 
 board = [list(map(int, input().split())) for _ in range(5)]
-questions = [list(map(int, input().split())) for _ in range(5)]
-print(solution(board, questions))
-
-
+numbers = [list(map(int, input().split())) for _ in range(5)]
+print(solution(board, numbers))
