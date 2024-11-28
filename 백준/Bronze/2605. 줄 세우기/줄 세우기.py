664ac2f9b74fc6634213d4f95ad720dfde4b9257
Revi1337 <david122123@gmail.com>
2024-11-28 16:12:55 +0900

    [Bronze II] Title: 줄 세우기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -1,17 +1,12 @@
 def solution(N, students):
-    answer = [0] * N
+    answer = []
     for idx in range(N):
         answer = answer[:idx - students[idx]] + [idx] + answer[idx - students[idx]:]
 
     for idx in range(N):
         answer[idx] += 1
 
-    for ans in answer:
-        if ans:
-            print(ans, end = ' ')
-        else:
-            break
-    print()
+    print(*answer)
 
 N = int(input())
 students = list(map(int, input().split()))
