c262605bb50525c4d80394c1a2b238fa01c41bf8
Revi1337 <david122123@gmail.com>
2024-12-01 17:23:41 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 148 ms, Memory: 47,992 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -3,26 +3,19 @@ from collections import deque
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-"""
-bfs
-"""
 def solution(board):
-
-    queue = deque()
-
-    st = []
-    end = []
-    check = [[0] * 16 for _ in range(16)]
-
+    st = end = None
     for row in range(16):
         for col in range(16):
             if board[row][col] == 2:
                 st = [row, col]
             if board[row][col] == 3:
                 end = [row, col]
+            if board[row][col] == 1:
+                board[row][col] = -1
 
-    queue.append((st[0], st[1]))
-    check[st[0]][st[1]] = 1
+    board[st[0]][st[1]] = -1
+    queue = deque([(st[0], st[1])])
 
     while queue:
         row, col = queue.popleft()
@@ -30,15 +23,15 @@ def solution(board):
             return 1
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
-            if (0 <= nrow < 16) and (0 <= ncol < 16):
-                if (board[nrow][ncol] != 1) and not check[nrow][ncol]:
+            if 0 <= nrow < 16 and 0 <= ncol < 16:
+                if board[nrow][ncol] != -1:
+                    board[nrow][ncol] = -1
                     queue.append((nrow, ncol))
-                    check[nrow][ncol] = 1
 
     return 0
 
 T = 10
 for _ in range(T):
-    index = int(input())
-    board = [list(map(int, list(input().rstrip()))) for _ in range(16)]
-    print(f'#{index} {solution(board)}')
+    seq = int(input())
+    board = [list(map(int, input().rstrip())) for _ in range(16)]
+    print(f'#{seq} {solution(board)}')
