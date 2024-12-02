fd3e2ae47a06a19eec35afc5ef452ff62d3c976e
Revi1337 <david122123@gmail.com>
2024-12-02 18:55:48 +0900

    [D4] Title: 정사각형 방, Time: 208 ms, Memory: 51,184 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -4,42 +4,40 @@ drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
 def solution(N, board):
+    ans_val = 1
+    ans_counter = 1
+    check = [[0] * N for _ in range(N)]
 
-    def bfs(r, c):
-        queue = deque([(r, c, 1)])
-        check = [[0] * N for _ in range(N)]
-        check[r][c] = 1
-        distance = 1
-
-        while queue:
-            row, col, dis = queue.popleft()
-            distance = max(distance, dis)
-            for d in range(4):
-                nrow, ncol = row + drow[d], col + dcol[d]
-                if 0 <= nrow < N and 0 <= ncol < N:
-                    if board[nrow][ncol] - board[row][col] == 1 and not check[nrow][ncol]:
-                        check[nrow][ncol] = check[row][col] + 1
-                        queue.append((nrow, ncol, dis + 1))
-
-        return distance
-
-    collect = []
     for row in range(N):
         for col in range(N):
-            collect.append((board[row][col], bfs(row, col)))
+            if check[row][col]:
+                continue
 
-    max_answer = max(collect, key=lambda x: x[1])
-    answers = []
-    for ans in collect:
-        if ans[1] == max_answer[1]:
-            answers.append(ans)
+            check[row][col] = 1
+            queue = deque([(row, col)])
+            node_val, counter = board[row][col], 1
+            while queue:
+                r, c = queue.popleft()
+                for d in range(4):
+                    nr, nc = r + drow[d], c + dcol[d]
+                    if 0 <= nr < N and 0 <= nc < N:
+                        if abs(board[r][c] - board[nr][nc]) == 1 and not check[nr][nc]:
+                            check[nr][nc] = 1
+                            queue.append((nr, nc))
+                            node_val = min(node_val, board[nr][nc])
+                            counter += 1
 
-    answer = min(answers, key=lambda x: x[0])
+            if counter > ans_counter:
+                ans_val, ans_counter = node_val, counter
+            elif counter == ans_counter:
+                ans_val = min(ans_val, node_val)
 
-    return f'{answer[0]} {answer[1]}'
+    return f'{ans_val} {ans_counter}'
+
 
 T = int(input())
-for index in range(T):
+for seq in range(T):
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{index + 1} {solution(N, board)}')
+    print(f'#{seq + 1} {solution(N, board)}')
+
