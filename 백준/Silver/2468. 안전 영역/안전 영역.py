1347872d16c637efb56ac42276e071bf699f1aca
Revi1337 <david122123@gmail.com>
2024-12-07 15:11:46 +0900

    [Silver I] Title: 안전 영역, Time: 212 ms, Memory: 116580 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,47 +1,40 @@
-import sys
 from collections import deque
-
-input = sys.stdin.readline
-
-"""
-안전 영역 (https://www.acmicpc.net/problem/2468)
-2024-09-12
-"""
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
 def solution(N, board):
-    maxH = 1
-    for line in board:
-        maxH = max(maxH, max(line))
+    max_height = max([max(heights) for heights in board])
+
+    def bfs(row, col):
+        new_board[row][col] = 0
+        queue = deque([(row, col)])
+        while queue:
+            r, c = queue.popleft()
+            for d in range(4):
+                nr, nc = r + drow[d], c + dcol[d]
+                if 0 <= nr < N and 0 <= nc < N and new_board[nr][nc] == 1:
+                    new_board[nr][nc] = 0
+                    queue.append((nr, nc))
 
     answer = 0
-    for h in range(maxH + 1):
-        new = [line[:] for line in board]
+    for height in range(max_height + 1):
+        new_board = [[1] * N for _ in range(N)]
         for row in range(N):
             for col in range(N):
-                if new[row][col] <= h:
-                    new[row][col] = -1
+                if board[row][col] <= height:
+                    new_board[row][col] = 0
 
         counter = 0
         for row in range(N):
             for col in range(N):
-                if new[row][col] not in [-1, -2]:
+                if new_board[row][col]:
                     counter += 1
-                    new[row][col] = -2
-                    queue = deque([(row, col)])
-                    while queue:
-                        r, c = queue.popleft()
-                        for d in range(4):
-                            nr, nc = r + drow[d], c + dcol[d]
-                            if (0 <= nr < N) and (0 <= nc < N):
-                                if new[nr][nc] not in [-1, -2]:
-                                    new[nr][nc] = -2
-                                    queue.append((nr, nc))
+                    bfs(row, col)
         answer = max(answer, counter)
 
     return answer
+
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
