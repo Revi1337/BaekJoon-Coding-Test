babae0d6b9168e0c1316f1a4dccc80a0c11cb41f
Revi1337 <david122123@gmail.com>
2024-12-10 13:41:04 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 208 ms, Memory: 51,868 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,21 @@
+def solution(N, B, S):
+
+    def backtracking(n, sm):
+        if n == N:
+            if sm >= B:
+                nonlocal answer
+                answer = min(answer, sm)
+            return
+        backtracking(n + 1, sm + S[n])
+        backtracking(n + 1, sm)
+
+    answer = 1e9
+    backtracking(0, 0)
+
+    return answer - B
+
+T = int(input())
+for seq in range(T):
+    N, B = map(int, input().split())
+    S = list(map(int, input().split()))
+    print(f'#{seq + 1} {solution(N, B, S)}')
