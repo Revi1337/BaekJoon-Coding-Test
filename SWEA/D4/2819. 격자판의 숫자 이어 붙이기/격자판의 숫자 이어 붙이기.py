3920146d1730564c65373b87a5692651bf81bf1c
Revi1337 <david122123@gmail.com>
2024-12-11 22:43:28 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 330 ms, Memory: 62,612 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -3,32 +3,23 @@ dcol = [0, 1, 0, -1]
 
 def solution(board):
 
-    def backtracking(row, col):
-        if len(lst) == 7:
-            val = "".join(lst)
-            if val not in check:
-                check.add(val)
-                nonlocal answer
-                answer += 1
+    def backtracking(n, num, row, col):
+        if n == 7:
+            answer.add(num)
             return
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
             if 0 <= nrow < 4 and 0 <= ncol < 4:
-                lst.append(board[nrow][ncol])
-                backtracking(nrow, ncol)
-                lst.pop()
+                backtracking(n + 1, num * 10 + board[nrow][ncol], nrow, ncol)
 
-    check = set()
-    answer = 0
-    lst = []
-
+    answer = set()
     for row in range(4):
         for col in range(4):
-            backtracking(row, col)
+            backtracking(0, 0, row, col)
 
-    return answer
+    return len(answer)
 
 T = int(input())
 for seq in range(T):
-    board = [input().rstrip().split() for _ in range(4)]
+    board = [list(map(int, input().split())) for _ in range(4)]
     print(f'#{seq + 1} {solution(board)}')
