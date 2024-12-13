d8c2f04daae3ccabea3983e1c12013f17f452d19
Revi1337 <david122123@gmail.com>
2024-12-13 13:35:43 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 2,600 ms, Memory: 86,524 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -3,24 +3,24 @@ dcol = [-1, 1, 1, -1]
 
 def solution(N, board):
 
-    def backtracking(d, r, c, check):
+    def backtracking(d, r, c, lst):
         nonlocal answer
         if d > 3:
             return
         if d == 3 and (r, c) == (row, col):
-            answer = max(answer, len(check))
+            answer = max(answer, len(lst))
             return
         for nd in range(d, min(d + 2, 4)):
             nr, nc = r + drow[nd], c + dcol[nd]
-            if 0 <= nr < N and 0 <= nc < N and board[nr][nc] not in check:
-                check.add(board[nr][nc])
-                backtracking(nd, nr, nc, check)
-                check.discard(board[nr][nc])
+            if 0 <= nr < N and 0 <= nc < N and board[nr][nc] not in lst:
+                lst.append(board[nr][nc])
+                backtracking(nd, nr, nc, lst)
+                lst.pop()
 
     answer = -1
     for row in range(N - 2):
         for col in range(N - 1):
-            backtracking(0, row, col, set())
+            backtracking(0, row, col, [])
 
     return answer
 
