68ba1d88134f900b49db2fcd2c2c423771f1bb2d
Revi1337 <david122123@gmail.com>
2024-12-13 13:17:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 2,045 ms, Memory: 82,820 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 디저트 카페 - 2105 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu) 
+
+### 성능 요약
+
+메모리: 82,820 KB, 시간: 2,045 ms, 코드길이: 864 Bytes
+
+### 제출 일자
+
+2024-12-13 13:17
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -0,0 +1,31 @@
+drow = [1, 1, -1, -1, 1]
+dcol = [-1, 1, 1, -1, -1]
+
+def solution(N, board):
+
+    def backtracking(d, r, c, lst):
+        if d > 3:
+            return
+        if d == 3 and (r, c) == (row, col):
+            nonlocal answer
+            answer = max(answer, len(lst))
+            return
+        for nd in range(d, d + 2):
+            nr, nc = r + drow[nd], c + dcol[nd]
+            if 0 <= nr < N and 0 <= nc < N and board[nr][nc] not in lst:
+                lst.append(board[nr][nc])
+                backtracking(nd, nr, nc, lst)
+                lst.pop()
+
+    answer = -1
+    for row in range(N - 2):
+        for col in range(N - 1):
+            backtracking(0, row, col, [])
+
+    return answer
+
+T = int(input())
+for seq in range(T):
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    print(f'#{seq + 1} {solution(N, board)}')
