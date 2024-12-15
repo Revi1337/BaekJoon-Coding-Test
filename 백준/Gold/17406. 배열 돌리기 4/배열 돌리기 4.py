1c1228ce18668647121edac1f36759a7ad0f4264
Revi1337 <david122123@gmail.com>
2024-12-15 12:02:14 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 180 ms, Memory: 111528 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -4,30 +4,32 @@ from itertools import permutations
 drow = [0, 1, 0, -1]
 dcol = [1, 0, -1, 0]
 
-def rotate(n_board, rr, rc, t):
-    to, fro = [rr - t - 1, rc - t - 1], [rr + t - 1, rc + t - 1]
-    offset = 0
-    while offset < t:
-        row, col, d = to[0], to[1] + 1, 0
-        prev = n_board[to[0]][to[1]]
-        while [row, col] != to:
-            nrow, ncol = row + drow[d], col + dcol[d]
-            if not (to[0] <= nrow <= fro[0] and to[1] <= ncol <= fro[1]):
-                d = (d + 1) % 4
-                continue
-            n_board[row][col], prev = prev, n_board[row][col]
-            row, col = nrow, ncol
-        n_board[row][col] = prev
-        offset += 1
-        to = [to[0] + 1, to[1] + 1]
-        fro = [fro[0] - 1, fro[1] - 1]
-
 def solution(N, M, K, board, rots):
-    answer = float('inf')
+    perm = []
+    for i in range(K):
+        for j in range(K):
+            if i != j:
+                perm.append([rots[i], rots[j]])
+
+    answer = 1e9
     for rotators in permutations(rots, K):
         n_board = [[*line] for line in board]
         for rr, rc, t in rotators:
-            rotate(n_board, rr, rc, t)
+            to, fro = [rr - t - 1, rc - t - 1], [rr + t - 1, rc + t - 1]
+            offset = 0
+            while offset < t:
+                row, col, d = to[0], to[1] + 1, 0
+                prev = n_board[to[0]][to[1]]
+                while [row, col] != to:
+                    nrow, ncol = row + drow[d], col + dcol[d]
+                    if not (to[0] <= nrow <= fro[0] and to[1] <= ncol <= fro[1]):
+                        d = (d + 1) % 4
+                        continue
+                    n_board[row][col], prev = prev, n_board[row][col]
+                    row, col = nrow, ncol
+                n_board[row][col] = prev
+                offset, to, fro = offset + 1, [to[0] + 1, to[1] + 1], [fro[0] - 1, fro[1] - 1]
+
         minN = min([sum(line) for line in n_board])
         answer = min(answer, minN)
 
