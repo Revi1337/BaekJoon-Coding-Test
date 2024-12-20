ce3927314c052b6fb8601462c31a9e8899f2a5d9
Revi1337 <david122123@gmail.com>
2024-12-20 10:38:33 +0900

    [Gold IV] Title: 알파벳, Time: 904 ms, Memory: 273904 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,25 @@
+from collections import deque
+
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
 def solution(R, C, board):
+    check = [[set() for _ in range(C)] for _ in range(R)]
+    check[0][0].add(board[0][0])
+    queue = deque([(0, 0, board[0][0])])
 
-    def dfs(row, col, curr, count):
-        nonlocal answer
-        answer = max(answer, count)
+    ans = 1
+    while queue:
+        row, col, alpha = queue.popleft()
+        ans = max(ans, len(alpha))
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
-            if 0 <= nrow < R and 0 <= ncol < C and not cache[ord(board[nrow][ncol]) - 65]:
-                cache[ord(board[nrow][ncol]) - 65] = 1
-                count += 1
-                dfs(nrow, ncol, curr, count)
-                count -= 1
-                cache[ord(board[nrow][ncol]) - 65] = 0
+            if 0 <= nrow < R and 0 <= ncol < C:
+                if board[nrow][ncol] not in alpha and alpha + board[nrow][ncol] not in check[nrow][ncol]:
+                    check[nrow][ncol].add(alpha + board[nrow][ncol])
+                    queue.append((nrow, ncol, alpha + board[nrow][ncol]))
 
-    cache = [0] * 26
-    cache[ord(board[0][0]) - 65] = 1
-    answer = 1
-    dfs(0, 0, cache, 1)
-
-    return answer
+    return ans
 
 R, C = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(R)]
