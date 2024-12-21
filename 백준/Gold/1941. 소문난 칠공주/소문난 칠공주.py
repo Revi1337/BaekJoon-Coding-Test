1520e265e4e573f49d1aa267701274810c86e533
Revi1337 <david122123@gmail.com>
2024-12-21 13:03:15 +0900

    [Gold III] Title: 소문난 칠공주, Time: 704 ms, Memory: 122656 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,57 +1,52 @@
-import sys
 from collections import deque
-
-input = sys.stdin.readline
-
-"""
-소문난 칠공주 (https://www.acmicpc.net/problem/1941)
-2024-10-01
-"""
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
 def solution(board):
 
-    def visted():
-        def bfs(r, c):
-            queue = deque([(r, c)])
-            v = [[0] * 5 for _ in range(5)]
-            v[r][c] = 1
-            cnt = 1
-            while queue:
-                row, col = queue.popleft()
-                for d in range(4):
-                    nrow, ncol = row + drow[d], col + dcol[d]
-                    if (0 <= nrow < 5) and (0 <= ncol < 5) and (not v[nrow][ncol]) and (check[nrow][ncol]):
-                        queue.append((nrow, ncol))
-                        v[nrow][ncol] = 1
-                        cnt += 1
-            return cnt == 7
+    def bfs(row, col):
+        vv = [[0] * 5 for _ in range(5)]
+        vv[row][col] = 1
+        queue = deque([(row, col)])
+        cnt = 1
+        while queue:
+            r, c = queue.popleft()
+            for d in range(4):
+                nr, nc = r + drow[d], c + dcol[d]
+                if 0 <= nr < 5 and 0 <= nc < 5 and not vv[nr][nc] and v[nr][nc]:
+                    queue.append((nr, nc))
+                    vv[nr][nc] = 1
+                    cnt += 1
+        return cnt == 7
 
+    def check():
         for row in range(5):
             for col in range(5):
-                if check[row][col] == 1:
+                if v[row][col]:
                     return bfs(row, col)
 
-    def backtrack(n, cnt, scnt):
+    def backtracking(n, cnt, scnt):
         if cnt > 7:
             return
+
         if n == 25:
             if cnt == 7 and scnt >= 4:
-                if visted():
+                if check():
                     nonlocal answer
                     answer += 1
             return
-        check[n // 5][n % 5] = 1
-        backtrack(n + 1, cnt + 1, scnt + int(board[n // 5][n % 5] == 'S'))
-        check[n // 5][n % 5] = 0
-        backtrack(n + 1, cnt, scnt)
 
+        v[n // 5][n % 5] = 1
+        backtracking(n + 1, cnt + 1, scnt + int(board[n // 5][n % 5] == 'S'))
+        v[n // 5][n % 5] = 0
+        backtracking(n + 1, cnt, scnt)
+
+    v = [[0] * 5 for _ in range(5)]
     answer = 0
-    check = [[0] * 5 for _ in range(5)]
-    backtrack(0, 0, 0)
-    print(answer)
+    backtracking(0, 0, 0)
 
-board = [list(input().strip()) for _ in range(5)]
-solution(board)
+    return answer
+
+board = [list(input().rstrip()) for _ in range(5)]
+print(solution(board))
