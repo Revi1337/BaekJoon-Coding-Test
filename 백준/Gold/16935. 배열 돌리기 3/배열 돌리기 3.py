bcb612c01c1b113b0bee2e7bb9ceb131d4e19b83
Revi1337 <david122123@gmail.com>
2025-01-07 01:13:01 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 260 ms, Memory: 114772 KB -BaekjoonHub

---
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.py
@@ -0,0 +1,58 @@
+def solution(N, M, R, array, opers):
+
+    def first(arr):
+        return arr[::-1]
+
+    def second(arr):
+        return [row[::-1] for row in arr]
+
+    def third(arr):
+        return [list(row) for row in zip(*arr[::-1])]
+
+    def four(arr):
+        return [list(row) for row in zip(*arr)][::-1]
+
+    def five(arr):
+        N, M = len(arr), len(arr[0])
+        carr = [[0] * M for _ in range(N)]
+        hn, hm = N // 2, M // 2
+        for row in range(N):
+            for col in range(M):
+                if row < hn and col < hm:
+                    carr[row][col + hm] = arr[row][col]
+                elif row < hn and col >= hm:
+                    carr[row + hn][col] = arr[row][col]
+                elif row >= hn and col < hm:
+                    carr[(row + hn) % N][col] = arr[row][col]
+                elif row >= hn and col >= hm:
+                    carr[row][(col + hm) % M] = arr[row][col]
+        return carr
+
+    def six(arr):
+        N, M = len(arr), len(arr[0])
+        carr = [[0] * M for _ in range(N)]
+        hn, hm = N // 2, M // 2
+        for row in range(N):
+            for col in range(M):
+                if row < hn and col < hm:
+                    carr[(row + hn) % N][col] = arr[row][col]
+                elif row < hn and col >= hm:
+                    carr[row][(col + hm) % M] = arr[row][col]
+                elif row >= hn and col < hm:
+                    carr[row][col + hm] = arr[row][col]
+                elif row >= hn and col >= hm:
+                    carr[(row + hn) % N][col] = arr[row][col]
+        return carr
+
+    callables = [0, first, second, third, four, five, six]
+    for oper in opers:
+        callable = callables[oper]
+        array = callable(array)
+
+    for line in array:
+        print(*line)
+
+N, M, R = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+opers = list(map(int, input().split()))
+solution(N, M, R, arr, opers)
