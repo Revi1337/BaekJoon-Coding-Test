7aa93f87f43a0f704602f2b6c8aab3f377406659
Revi1337 <david122123@gmail.com>
2025-01-07 14:08:12 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 720 ms, Memory: 112072 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.py
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.py
@@ -3,29 +3,25 @@ dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
 def solution(N, M, R, arr):
-
-    def rotate(padding, rotations):
+    def rotate_layer(padding):
         d, sr, sc = 0, padding, padding
         prev = arr[sr][sc]
         er, ec = sr, sc
-        for _ in range(rotations):
-            sr, sc = er, ec
-            prev = arr[sr][sc]
-            while True:
-                nr, nc = sr + dr[d], sc + dc[d]
-                if not (padding <= nr < N - padding and padding <= nc < M - padding):
-                    d = (d + 1) % 4
-                    continue
-                arr[nr][nc], prev = prev, arr[nr][nc]
-                sr, sc = nr, nc
-                if (sr, sc) == (er, ec):
-                    break
+        while True:
+            nr, nc = sr + dr[d], sc + dc[d]
+            if not (padding <= nr < N - padding and padding <= nc < M - padding):
+                d = (d + 1) % 4
+                continue
+            arr[nr][nc], prev = prev, arr[nr][nc]
+            sr, sc = nr, nc
+            if (sr, sc) == (er, ec):
+                break
 
     for padding in range(min(N, M) // 2):
-        layer_length = 2 * ((N - 2 * padding) + (M - 2 * padding) - 2)
-        rotations = R % layer_length
-        for _ in range(rotations):
-            rotate(padding, 1)
+        layer_size = 2 * ((N - 2 * padding) + (M - 2 * padding) - 2)
+        actual_rotations = R % layer_size
+        for _ in range(actual_rotations):
+            rotate_layer(padding)
 
     for line in arr:
         print(*line)
