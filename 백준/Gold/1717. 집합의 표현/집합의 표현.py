bd2fd90eb03c2b2b93118499b41b65355adfbb09
Revi1337 <david122123@gmail.com>
2025-01-12 14:52:36 +0900

    [Gold V] Title: 집합의 표현, Time: 340 ms, Memory: 266888 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -14,19 +14,23 @@ def solution(N, M, opers):
         tree[n] = find(tree[n])
         return tree[n]
 
+    def union(n1, n2):
+        r1, r2 = find(n1), find(n2)
+        if rank[r1] < rank[r2]:
+            tree[r1] = r2
+        elif rank[r1] > rank[r2]:
+            tree[r2] = r1
+        else:
+            tree[r2] = r1
+            rank[r1] += 1
+
     rank = [0] * (N + 1)
     tree = [num for num in range(N + 1)]
     for oper, n1, n2 in opers:
-        r1, r2 = find(n1), find(n2)
         if not oper:
-            if rank[r1] < rank[r2]:
-                tree[r1] = r2
-            elif rank[r1] > rank[r2]:
-                tree[r2] = r1
-            else:
-                tree[r2] = r1
-                rank[r1] += 1
+            union(n1, n2)
         else:
+            r1, r2 = find(n1), find(n2)
             print('YES' if r1 == r2 else 'NO')
 
 N, M = map(int, input().split())
