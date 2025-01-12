163af6926f268c078634d7105494655bf9a2609c
Revi1337 <david122123@gmail.com>
2025-01-12 21:18:05 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 568 ms, Memory: 151040 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -0,0 +1,32 @@
+def solution(N, edges):
+
+    def find(n):
+        if island[n] == n:
+            return n
+
+        island[n] = find(island[n])
+        return island[n]
+
+    def union(i1, i2):
+        r1, r2 = find(i1), find(i2)
+        if ranks[r1] < ranks[r2]:
+            island[r1] = r2
+        elif ranks[r1] > ranks[r2]:
+            island[r2] = r1
+        else:
+            island[r2] = r1
+            ranks[r1] += 1
+
+    island = list(range(0, N + 1))
+    ranks = [0] * (N + 1)
+    for i1, i2 in edges:
+        union(i1, i2)
+
+    for idx in range(2, N+ 1):
+        if find(1) != find(idx):
+            print(1, idx)
+            return
+
+N = int(input())
+edges = [list(map(int, input().split())) for _ in range(N - 2)]
+solution(N, edges)
