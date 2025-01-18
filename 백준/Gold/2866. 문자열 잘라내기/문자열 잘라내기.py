e199ae4ffd46d073cfb29c06441a4ff4dc69298b
Revi1337 <david122123@gmail.com>
2025-01-18 20:10:47 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 240 ms, Memory: 120252 KB -BaekjoonHub

---
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.py
@@ -0,0 +1,23 @@
+def solution(R, C, words):
+    def is_valid(mid):
+        seen = set()
+        for col in range(C):
+            string = "".join(words[row][col] for row in range(mid, R))
+            seen.add(string)
+        return len(seen) == C
+
+    answer = 0
+    left, right = 0, R - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if is_valid(mid):
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return answer
+
+R, C = map(int, input().split())
+words = [input().rstrip() for _ in range(R)]
+print(solution(R, C, words))
