9579a4f635c64168125eeaecfe11b3c4c9ef36e1
Revi1337 <david122123@gmail.com>
2025-02-12 21:45:35 +0900

    Update 의상.py

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -8,3 +8,26 @@ def solution(clothes):
         answer *= cnt + 1
         
     return answer - 1
+
+
+# def solution(clothes):
+
+#     def backtracking(n, cnt):
+#         if n == length:
+#             nonlocal answer
+#             if cnt:
+#                 answer += 1
+#             return
+
+#         cloth, kind = clothes[n]
+#         backtracking(n + 1, cnt)
+
+#         if kind not in cache:
+#             cache.add(kind)
+#             backtracking(n + 1, cnt + 1)
+#             cache.discard(kind)
+
+#     answer, length, cache = 0, len(clothes), set()
+#     backtracking(0, 0)
+
+#     return answer
