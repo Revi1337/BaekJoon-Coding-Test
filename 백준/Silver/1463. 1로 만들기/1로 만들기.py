578a0e3c1dc2c5456a31ffd143c1ee92a9bee106
Revi1337 <david122123@gmail.com>
2025-02-16 17:49:04 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 126392 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,13 @@
-import sys
-
-input = sys.stdin.readline
-
 def solution(N):
-    dp = [0] * (N + 1)
-    for num in range(2, N + 1):
-        dp[num] = dp[num - 1] + 1
-        if num % 3 == 0:
-            dp[num] = min(dp[num], dp[num // 3] + 1)
-        if num % 2 == 0:
-            dp[num] = min(dp[num], dp[num // 2] + 1)
-
+    dp = [1e9] * (N + 1)
+    dp[0] = dp[1] = 0
+    for idx in range(2, N + 1):
+        dp[idx] = min(dp[idx], dp[idx - 1] + 1)
+        if not idx % 2:
+            dp[idx] = min(dp[idx], dp[idx // 2] + 1)
+        if not idx % 3:
+            dp[idx] = min(dp[idx], dp[idx // 3] + 1)
     return dp[N]
 
-N = int(input().rstrip())
+N = int(input())
 print(solution(N))
