313bddfecfa71cd82e9292ba68aabac0c306a55b
Revi1337 <david122123@gmail.com>
2025-02-16 22:37:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,15 +1,11 @@
-import sys
-
-input = sys.stdin.readline
-
 def solution(N):
     dp = [[0] * 12 for _ in range(N + 1)]
-    dp[1][2 : 11] = [1] * 9
-    for i in range(2, N + 1):
-        for j in range(1, 11):
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+    dp[1][2:11] = [1] * 9
+    for idx in range(2, N + 1):
+        for jdx in range(1, 11):
+            dp[idx][jdx] = dp[idx - 1][jdx - 1] + dp[idx - 1][jdx + 1]
 
     return sum(dp[N]) % 1_000_000_000
-
+    
 N = int(input().rstrip())
 print(solution(N))
