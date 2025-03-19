23704bc54db9bbb716c8040a37b345d693abe037
Revi1337 <david122123@gmail.com>
2025-03-19 15:17:54 +0900

    [level 3] Title: 입국심사, Time: 643.39 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -12,12 +12,11 @@ def solution(n, times):
     left, right = 1, times[-1] * n
     answer = 1e11
     
-    while left <= right:
+    while left < right:
         mid = (left+right) // 2
-                
-        if valid(mid):
-            answer = mid
-            right = mid - 1
-        else :
+        if not valid(mid):
             left = mid + 1
+        else:
+            answer = mid
+            right = mid
     return answer
