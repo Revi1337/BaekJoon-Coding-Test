d6eaeb66fbf5c30c9127c2d7cfd1c1005b2d2708
Revi1337 <david122123@gmail.com>
2025-03-21 12:49:49 +0900

    [Gold III] Title: 소수의 연속합, Time: 184 ms, Memory: 161832 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,41 +1,28 @@
-import sys
-
-input = sys.stdin.readline
-
-def solution(n):
-    # 에라토스테네스체 알고리즘으로 (소수배열을 구한 후)
-    if n == 1:
-        return 0
-    arr = [1] * (n + 1)
-    numbers = []
-    for number in range(2, int(n ** 0.5) + 1):
-        if arr[number]:
+def solution(N):
+    cache = [1] * (N + 1)
+    for num in range(2, int(N ** 0.5) + 1):
+        if cache[num]:
             offset = 2
-            while number * offset <= n:
-                arr[number * offset] = 0
+            while num * offset <= N:
+                cache[num * offset] = 0
                 offset += 1
-    for number in range(2, n + 1):
-        if arr[number]:
-            numbers.append(number)
+    so = [num for num in range(2, N + 1) if cache[num]]
 
-
-    # Two Pointer 알고리즘으로 소수배열의 구간합을 구하여, n 이 만들어지나 판단한다.
-    counter = 0
-    numbers_length = len(numbers)
-    left, right = numbers_length - 1, numbers_length - 1
-    cur = numbers[-1]
-    while 0 <= left <= right and 0 <= right < numbers_length:
-        if cur > n:
-            cur -= numbers[right]
-            right -= 1
+    length = len(so)
+    sm = so[0] if so else 0
+    answer = right = left = 0
+    while left <= right < length:
+        if sm >= N:
+            if sm == N:
+                answer += 1
+            sm -= so[left]
+            left += 1
         else:
-            if cur == n:
-                counter += 1
-            left -= 1
-            if left < 0:
-                break
-            cur += numbers[left]
-    return counter
+            right += 1
+            if right < length:
+                sm += so[right]
 
-n = int(input().rstrip())
-print(solution(n))
+    return answer
+
+N = int(input())
+print(solution(N))
