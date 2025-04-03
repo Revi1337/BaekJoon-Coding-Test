f3a7d9877036c8262dabf54417e614f680d5907f
Revi1337 <david122123@gmail.com>
2025-04-03 16:28:53 +0900

    [Gold III] Title: 게임 개발, Time: 132 ms, Memory: 114476 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -3,10 +3,8 @@ from collections import deque
 def solution(N, edges):
     priority = [0] * (N + 1)
     graph = [[] for _ in range(N + 1)]
-    time = [0] * (N + 1)
     answer = [0] * (N + 1)
     for curr, edge in enumerate(edges, start = 1):
-        time[curr] = edge[0]
         for need in edge[1:]:
             if need == -1:
                 break
@@ -17,13 +15,13 @@ def solution(N, edges):
     for curr in range(1, N + 1):
         if priority[curr] == 0:
             queue.append(curr)
-            answer[curr] = time[curr]
+            answer[curr] = edges[curr - 1][0]
 
     while queue:
         curr = queue.popleft()
         for next in graph[curr]:
             priority[next] -= 1
-            answer[next] = max(answer[next], answer[curr] + time[next])
+            answer[next] = max(answer[next], answer[curr] + edges[next - 1][0])
             if priority[next] == 0:
                 queue.append(next)
 
