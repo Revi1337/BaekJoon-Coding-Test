236294aafa5bf803ddb3935f4bca9cb992d492a2
Revi1337 <david122123@gmail.com>
2025-04-05 15:30:59 +0900

    [Gold III] Title: 중량제한, Time: 296 ms, Memory: 138040 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,29 +1,34 @@
-import heapq
+from collections import deque
 
-"""
-다익스트라를 최대힙으로
-"""
 def solution(N, M, bridges, start, end):
     graph = [[] for _ in range(N + 1)]
     for a, b, c in bridges:
         graph[a].append((b, c))
         graph[b].append((a, c))
 
-    pq = [(-1_000_000_000, start)]
-    visited = [False] * (N + 1)
-    while pq:
-        weight, node = heapq.heappop(pq)
-        weight = -weight
-        if node == end:
-            return weight
-        if visited[node]:
-            continue
-        visited[node] = True
-        for next_node, limit in graph[node]:
-            if not visited[next_node]:
-                heapq.heappush(pq, (-min(weight, limit), next_node))
+    def bfs(weight):
+        visited = [False] * (N + 1)
+        queue = deque([start])
+        visited[start] = True
+        while queue:
+            cur = queue.popleft()
+            for nxt, limit in graph[cur]:
+                if not visited[nxt] and limit >= weight:
+                    visited[nxt] = True
+                    queue.append(nxt)
+        return visited[end]
 
-    return 0
+    left, right = 1, 1_000_000_000
+    answer = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if bfs(mid):
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return answer
 
 N, M = map(int, input().split())
 bridges = [list(map(int, input().split())) for _ in range(M)]
