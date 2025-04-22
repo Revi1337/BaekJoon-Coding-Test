d442c860991b7c61a22b842e56bfd5e47ae6b98f
Revi1337 <david122123@gmail.com>
2025-04-22 22:06:57 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 144 ms, Memory: 113996 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -1,37 +1,25 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
-
 def solution(N, edges, A, B):
-
-    def dfs(n):
-        for nxt in graph[n]:
-            if not check[nxt]:
-                check[nxt] = 1
-                t.append(nxt)
-                dfs(nxt)
 
     graph = [[] for _ in range(N + 1)]
     for v1, v2 in edges:
         graph[v2].append(v1)
 
-    answer = []
-
     check = [0] * (N + 1)
-    check[A] = 1
-    t = [A]
-    dfs(A)
-    answer.append(t)
-
-    check = [0] * (N + 1)
-    check[B] = 1
-    t = [B]
-    dfs(B)
-    answer.append(t)
-
-    for node in answer[0]:
-        if check[node]:
+    for node in A, B:
+        if not check[node]:
+            check[node] = 1
+        else:
             return node
+
+        while True:
+            if graph[node]:
+                node = graph[node][0]
+                if not check[node]:
+                    check[node] = 1
+                else:
+                    return node
+            else:
+                break
 
 T = int(input())
 for _ in range(T):
