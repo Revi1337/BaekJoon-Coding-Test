821de79ee2e2b5f08eb61e5b9a07657537fa5461
Revi1337 <david122123@gmail.com>
2025-06-12 13:30:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 208 ms, Memory: 65,920 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
@@ -1,34 +1,30 @@
 def solution(N, M, C, board):
 
-    def backtracking(n, cnt, sm, ci, cj):
+    def backtrack(n, amount, sm, sr, sc):
         nonlocal mx
-        if cnt > C:
+        if amount > C:
             return
         if n == M:
             mx = max(mx, sm)
             return
 
-        backtracking(n + 1, cnt + board[ci][cj + n], sm + board[ci][cj + n] ** 2, ci, cj)
-        backtracking(n + 1, cnt, sm, ci, cj)
+        backtrack(n + 1, amount + board[sr][sc + n], sm + board[sr][sc + n] ** 2, sr, sc)
+        backtrack(n + 1, amount, sm, sr, sc)
 
-    ans = mx = sm1 = 0
-    memo = [[0] * N for _ in range(N)]
-    # [0] 각 위치의 가능한 최대값을 한번 저장.
-    for i in range(N):
-        for j in range(N - M + 1):
+    answer = 0
+    for row1 in range(N):
+        for col1 in range(N - M + 1):
             mx = 0
-            backtracking(0, 0, 0, i, j)
-            memo[i][j] = mx
+            backtrack(0, 0, 0, row1, col1)
+            first = mx
+            for row2 in range(row1, N):
+                c2 = col1 + M if row1 == row2 else 0
+                for col2 in range(c2, N - M + 1):
+                    mx = 0
+                    backtrack(0, 0, 0, row2, col2)
+                    answer = max(answer, first + mx)
 
-    # [1] 가능한 모든 시작위치 (일꾼 1, 2)
-    for i1 in range(N): # 첫번쨰 일꾼
-        for j1 in range(N - M + 1):
-            for i2 in range(i1, N): # 두번쨰 일꾼 (가능한 모든 2명의 일꾼 위치)
-                sj = j1 + M if i1 == i2 else 0
-                for j2 in range(sj, N - M + 1):
-                    ans = max(ans, memo[i1][j1] + memo[i2][j2])
-
-    return ans
+    return answer
 
 T = int(input())
 for seq in range(T):
