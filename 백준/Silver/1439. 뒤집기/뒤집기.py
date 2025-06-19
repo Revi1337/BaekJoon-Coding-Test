7669fe644615c82eba81b036ef9e86d7e4e4631f
Revi1337 <david122123@gmail.com>
2025-06-20 02:17:57 +0900

    [Silver V] Title: 뒤집기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,17 +1,14 @@
 def solution(S):
     counter = [0, 0]
-    flag0 = flag1 = False
-    for ch in S:
-        if ch == '0':
-            flag1 = False
-            if not flag0:
-                counter[0] += 1
-                flag0 = True
-        else:
-            flag0 = False
-            if not flag1:
-                counter[1] += 1
-                flag1 = True
+    for i in range(1, len(S)):
+        if S[i] == '0' and S[i - 1] == '1':
+            counter[0] += 1
+        if S[i] == '1' and S[i - 1] == '0':
+            counter[1] += 1
+    if S[0] == '0':
+        counter[0] += 1
+    if S[0] == '1':
+        counter[1] += 1
 
     return min(counter)
 
