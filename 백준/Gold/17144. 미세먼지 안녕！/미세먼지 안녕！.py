4eaf968911011d95517dbd6cffab762a1b6fc730
Revi1337 <david122123@gmail.com>
2025-06-21 16:20:32 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 4760 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,74 +1,55 @@
-# 상 우 하 좌
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-
-# 우 상 좌 하
-urow = [0, -1, 0, 1]
-ucol = [1, 0, -1, 0]
-
-# 우 하 좌 상
-drow = [0, 1, 0, -1]
-dcol = [1, 0, -1, 0]
+DROW = [-1, 0, 1, 0]
+DCOL = [0, 1, 0, -1]
 
 def solution(R, C, T, board):
-    conditioner = []
-    for row in range(R):
-        if board[row][0] == -1:
-            conditioner.append((row, 0))
 
-    uur, uuc = conditioner[0]
-    ddr, ddc = conditioner[1]
+    inside = lambda row, col: 0 <= row < R and 0 <= col < C
+    condi = [row for row in range(R) if board[row][0] == -1]
+    urow, ucol = condi[0], 0
+    drow, dcol = condi[1], 0
 
-    while T:
-        new_board = [[0] * C for _ in range(R)]
+    for _ in range(T):
+        # spread
+        poss = dict()
         for row in range(R):
             for col in range(C):
-                if board[row][col] > 0:
-                    curr, count = board[row][col] // 5, 0
+                if board[row][col] and board[row][col] != -1:
+                    cnt = 0
+                    scost = board[row][col] // 5
                     for d in range(4):
-                        nrow, ncol = row + dr[d], col + dc[d]
-                        if 0 <= nrow < R and 0 <= ncol < C and board[nrow][ncol] != -1:
-                            count += 1
-                            new_board[nrow][ncol] += curr
-                    new_board[row][col] += board[row][col] - (curr * count)
+                        nrow, ncol = row + DROW[d], col + DCOL[d]
+                        if not inside(nrow, ncol) or board[nrow][ncol] == -1:
+                            continue
+                        cnt += 1
+                        poss[(nrow, ncol)] = poss.get((nrow, ncol), 0) + scost
+                    poss[(row, col)] = poss.get((row, col), 0) + board[row][col] - scost * cnt
+        for (row, col), cnt in poss.items():
+            board[row][col] = cnt
 
-        d = 0
-        prev = 0
-        row, col = ddr, 1
-        start = (ddr, 0)
-        while (row, col) != start:
-            nrow, ncol = row + drow[d], col + dcol[d]
-            if not (ddr <= nrow < R and 0 <= ncol < C):
-                d = (d + 1) % 4
+        # rotate
+        upos = (urow, ucol)
+        srow, scol, dir, prev = urow, 1, 1, 0
+        while (srow, scol) != upos:
+            nrow, ncol = srow + DROW[dir], scol + DCOL[dir]
+            if not inside(nrow, ncol):
+                dir = (dir - 1) % 4
                 continue
-            new_board[row][col], prev = prev, new_board[row][col]
-            row, col = nrow, ncol
+            board[srow][scol], prev = prev, board[srow][scol]
+            srow, scol = nrow, ncol
 
-        d = 0
-        prev = 0
-        row, col = uur, 1
-        start = (uur, 0)
-        while (row, col) != start:
-            nrow, ncol = row + urow[d], col + ucol[d]
-            if not (0 <= nrow <= uur and 0 <= ncol < C):
-                d = (d + 1) % 4
+        dpos = (drow, dcol)
+        srow, scol, dir, prev = drow, 1, 1, 0
+        while (srow, scol) != dpos:
+            nrow, ncol = srow + DROW[dir], scol + DCOL[dir]
+            if not inside(nrow, ncol):
+                dir = (dir + 1) % 4
                 continue
-            new_board[row][col], prev = prev, new_board[row][col]
-            row, col = nrow, ncol
+            board[srow][scol], prev = prev, board[srow][scol]
+            srow, scol = nrow, ncol
 
-        for row, col in conditioner:
-            new_board[row][col] = -1
+    board[urow][0] = board[drow][0] = 0
+    return sum(sum(line) for line in board)
 
-        board = new_board
-        T -= 1
-
-    answer = 0
-    for row in range(R):
-        for col in range(C):
-            if board[row][col] != -1:
-                answer += board[row][col]
-
-    return answer
 
 R, C, T = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(R)]
