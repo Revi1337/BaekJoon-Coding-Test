1ed4281d03c32db8d585a10e2a3649799fffd0b7
Revi1337 <david122123@gmail.com>
2025-06-22 01:27:29 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 24992 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -7,21 +7,24 @@ import java.util.*;
 public class Main {
 
     public static void solution(String[] words) {
-        Set<String> set = new HashSet<>(Arrays.asList(words));
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            set.add(words[i]);
+        }
+
         String[] uniqueWords = set.toArray(new String[0]);
         Arrays.sort(uniqueWords, (s1, s2) -> {
-            int s1Len = s1.length();
-            int s2Len = s2.length();
-
-            if (s1Len != s2Len) {
+            if (s1.length() != s2.length()) {
                 return Integer.compare(s1.length(), s2.length());
             }
             return s1.compareTo(s2);
         });
 
-        for (String word : uniqueWords) {
-            System.out.println(word);
+        StringBuilder sb = new StringBuilder();
+        for(String s : uniqueWords){
+            sb.append(s).append('\n');
         }
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws IOException {
