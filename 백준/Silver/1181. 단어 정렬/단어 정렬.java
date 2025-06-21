c53e3a302b05ca801feee39c16644025e3e1239e
Revi1337 <david122123@gmail.com>
2025-06-22 01:45:20 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 27432 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -6,23 +6,20 @@ import java.util.*;
  */
 public class Main {
 
-    public static void solution(String[] words) {
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < words.length; i++) {
-            set.add(words[i]);
-        }
-
-        String[] uniqueWords = set.toArray(new String[0]);
-        Arrays.sort(uniqueWords, (s1, s2) -> {
+    public static void solution(int N, String[] words) {
+        Arrays.sort(words, (s1, s2) -> {
             if (s1.length() != s2.length()) {
                 return Integer.compare(s1.length(), s2.length());
             }
             return s1.compareTo(s2);
         });
-
+        
         StringBuilder sb = new StringBuilder();
-        for(String s : uniqueWords){
-            sb.append(s).append('\n');
+        sb.append(words[0]).append('\n');
+        for(int i = 1; i < N; i ++) {
+            if(!words[i].equals(words[i - 1])) {
+                sb.append(words[i]).append('\n');
+            }
         }
         System.out.println(sb);
     }
@@ -33,6 +30,6 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] words = br.lines().limit(N).toArray(String[]::new);
-        solution(words);
+        solution(N, words);
     }
 }
