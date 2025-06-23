d8603fa619abfffd25dfab0e280513aca8c581ca
Revi1337 <david122123@gmail.com>
2025-06-23 14:25:29 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 280 ms, Memory: 114172 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,49 +1,49 @@
-sand_ratios = [
-    [(-1, 1, 0.01), (1, 1, 0.01), (-2, 0, 0.02), (2, 0, 0.02), (-1, 0, 0.07), (1, 0, 0.07), (-1, -1, 0.1), (1, -1, 0.1), (0, -2, 0.05)],
-    [(-1, -1, 0.01), (-1, 1, 0.01), (0, -2, 0.02), (0, 2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (1, -1, 0.1), (1, 1, 0.1), (2, 0, 0.05)],
-    [(-1, -1, 0.01), (1, -1, 0.01), (-2, 0, 0.02), (2, 0, 0.02), (-1, 0, 0.07), (1, 0, 0.07), (-1, 1, 0.1), (1, 1, 0.1), (0, 2, 0.05)],
-    [(1, -1, 0.01), (1, 1, 0.01), (0, -2, 0.02), (0, 2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (-1, -1, 0.1), (-1, 1, 0.1), (-2, 0, 0.05)]
-]
-
 drow = [0, 1, 0, -1]
 dcol = [-1, 0, 1, 0]
-
-def spread_sand(N, board, row, col, d):
-    outside_sand = 0
-    total_sand = board[row][col]
-    board[row][col] = 0
-
-    for dr, dc, ratio in sand_ratios[d]:
-        nr, nc = row + dr, col + dc
-        moved_sand = int(total_sand * ratio)
-        if 0 <= nr < N and 0 <= nc < N:
-            board[nr][nc] += moved_sand
-        else:
-            outside_sand += moved_sand
-
-    nr, nc = row + drow[d], col + dcol[d]
-    remaining_sand = total_sand - sum(int(total_sand * r[2]) for r in sand_ratios[d])
-    if 0 <= nr < N and 0 <= nc < N:
-        board[nr][nc] += remaining_sand
-    else:
-        outside_sand += remaining_sand
-
-    return outside_sand
+rates = [
+    [(-1, -1, 0.1), (-1, 0, 0.07), (-1, 1, 0.01), (1, 1, 0.01), (1, 0, 0.07), (1, -1, 0.1), (0, -2, 0.05), (-2, 0, 0.02), (2, 0, 0.02)],
+    [(-1, -1, 0.01), (-1, 1, 0.01), (0, 1, 0.07), (0, 2, 0.02), (1, 1, 0.1), (2, 0, 0.05), (1, -1, 0.1), (0, -1, 0.07), (0, -2, 0.02)],
+    [(-1, -1, 0.01), (-2, 0, 0.02), (-1, 0, 0.07), (-1, 1, 0.1), (0, 2, 0.05), (1, 1, 0.1), (1, 0, 0.07), (2, 0, 0.02), (1, -1, 0.01)],
+    [(-1, -1, 0.1), (-2, 0, 0.05), (-1, 1, 0.1), (0, 1, 0.07), (0, 2, 0.02), (1, 1, 0.01), (1, -1, 0.01), (0, -1, 0.07), (0, -2, 0.02)]
+]
 
 def solution(N, board):
-    row = col = N // 2
-    d = outside_sand = 0
-    step = 1
+
+    inside = lambda row, col: 0 <= row < N and 0 <= col < N
+
+    def spread(srow, scol, d):
+        out = 0
+        curr = board[srow][scol]
+
+        for dr, dc, ratio in rates[d]:
+            nrow, ncol = srow + dr, scol + dc
+            mv = int(curr * ratio)
+            if inside(nrow, ncol):
+                board[nrow][ncol] += mv
+            else:
+                out += mv
+
+        nrow, ncol = srow + drow[d], scol + dcol[d]
+        remain = curr - sum(int(curr * r[2]) for r in rates[d])
+        if inside(nrow, ncol):
+            board[nrow][ncol] += remain
+        else:
+            out += remain
+
+        return out
+
+    srow, scol = N // 2, N // 2
+    answer, dir, step = 0, 0, 1
 
     while True:
         for _ in range(2):
             for _ in range(step):
-                row += drow[d]
-                col += dcol[d]
-                outside_sand += spread_sand(N, board, row, col, d)
-                if row == 0 and col == 0:
-                    return outside_sand
-            d = (d + 1) % 4
+                nrow, ncol = srow + drow[dir], scol + dcol[dir]
+                answer += spread(nrow, ncol, dir)
+                srow, scol = nrow, ncol
+                if (srow, scol) == (0, 0):
+                    return answer
+            dir = (dir + 1) % 4
         step += 1
 
 N = int(input())
