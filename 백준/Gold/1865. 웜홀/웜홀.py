e4c2083fbbddaa7222401ece78e8928ebb2ebd6d
Revi1337 <david122123@gmail.com>
2025-06-27 16:58:55 +0900

    [Gold III] Title: 웜홀, Time: 208 ms, Memory: 112980 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,24 +1,21 @@
 def solution(N, M, K, roads, holes):
-    graph = [[] for _ in range(N + 1)]
-    for v1, v2, cost in roads:
-        graph[v1].append([v2, cost])
-        graph[v2].append([v1, cost])
-    for v1, v2, cost in holes:
-        graph[v1].append([v2, -cost])
+    edges = []
+    for v, nv, c in roads:
+        edges.append([v, nv, c])
+        edges.append([nv, v, c])
+    for v, nv, c in holes:
+        edges.append([v, nv, -c])
 
-    def has_negative_cycle():
-        dist = [1e9] * (N + 1)
-        dist[0] = 0
-        for _ in range(N):
-            for cur in range(1, N + 1):
-                for nxt, cost in graph[cur]:
-                    if dist[nxt] > dist[cur] + cost:
-                        dist[nxt] = dist[cur] + cost
-                        if _ == N - 1:
-                            return True
-        return False
+    dist = [1e9] * (N + 1)
+    dist[0] = 0
+    for _ in range(N):
+        for v, nv, cost in edges:
+            if dist[nv] > dist[v] + cost:
+                dist[nv] = dist[v] + cost
+                if _ == N - 1:
+                    return 'YES'
 
-    return 'YES' if has_negative_cycle() else 'NO'
+    return 'NO'
 
 T = int(input())
 for _ in range(T):
