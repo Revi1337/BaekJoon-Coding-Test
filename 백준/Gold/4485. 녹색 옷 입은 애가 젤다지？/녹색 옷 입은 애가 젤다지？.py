81a1b47d457ab4e956c33eeff2d2b933fa1cd2ea
Revi1337 <david122123@gmail.com>
2025-07-08 20:03:06 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 180 ms, Memory: 116152 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,34 +1,37 @@
-import sys
-import heapq
-
-input = sys.stdin.readline
-
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(idx, n, field):
-    check = [[0] * n for _ in range(n)]
-    check[0][0] = 1
-    queue = []
-    heapq.heappush(queue, (field[0][0], 0, 0))
+from collections import deque
+
+def solution(N, board):
+
+    inside = lambda row, col: 0 <= row < N and 0 <= col < N
+
+    check = [[-1] * N for _ in range(N)]
+    check[0][0] = board[0][0]
+    queue = deque([(0, 0)])
     while queue:
-        cost, row, col = heapq.heappop(queue)
-        if (row, col) == (n - 1, n - 1):
-            return f'Problem {idx}: {cost}'
+        row, col = queue.popleft()
         for d in range(4):
-            nrow = row + drow[d]
-            ncol = col + dcol[d]
-            if (0 <= nrow < n) and (0 <= ncol < n) and (not check[nrow][ncol]):
-                check[nrow][ncol] = 1
-                heapq.heappush(queue, (cost + field[nrow][ncol], nrow, ncol))
+            nrow, ncol = row + drow[d], col + dcol[d]
+            if not inside(nrow, ncol):
+                continue
+            if check[nrow][ncol] >= 0:
+                if check[row][col] + board[nrow][ncol] < check[nrow][ncol]:
+                    check[nrow][ncol] = check[row][col] + board[nrow][ncol]
+                    queue.append((nrow, ncol))
+            else:
+                check[nrow][ncol] = check[row][col] + board[nrow][ncol]
+                queue.append((nrow, ncol))
 
-idx = 1
+    return check[N - 1][N - 1]
+
+seq = 1
 while True:
-    n = int(input())
-    if n == 0:
+    N = int(input())
+    if N == 0:
         break
-    field = [list(map(int, input().split())) for _ in range(n)]
-    print(solution(idx, n, field))
-    idx += 1
-
+    board = [list(map(int, input().split())) for _ in range(N)]
+    print(f'Problem {seq}: {solution(N, board)}')
+    seq += 1
 
