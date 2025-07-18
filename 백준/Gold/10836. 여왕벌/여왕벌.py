de5f100319a686930c55577fc48b1265c40cb0a3
Revi1337 <david122123@gmail.com>
2025-07-18 16:34:09 +0900

    [Gold III] Title: 여왕벌, Time: 3740 ms, Memory: 226776 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -2,28 +2,28 @@ drow = [0, -1, -1]
 dcol = [-1, -1, 0]
 
 def solution(M, N, grows):
+    acc = [0] * (2 * M - 1)
+
+    for z, o, t in grows:
+        idx = 0
+        for _ in range(z): idx += 1
+        for _ in range(o): acc[idx] += 1; idx += 1
+        for _ in range(t): acc[idx] += 2; idx += 1
+
     I = [*range(M * (M - 1), -1, -M), *range(1, M)]
-    G = []
-    for grow in grows:
-        gg = []
-        for g, v in enumerate(grow):
-            gg.extend([g] * v)
-        G.append(gg)
-
     B = [[1] * M for _ in range(M)]
-    for gg in G:
-        for idx in range(len(gg)):
-            val = gg[idx]
-            ridx, cidx = I[idx] // M, I[idx] % M
-            B[ridx][cidx] += val
 
+    for i in range(2 * M - 1):
+        r, c = I[i] // M, I[i] % M
+        B[r][c] += acc[i]
+
+    for col in range(1, M):
+        B[1][col] += max(B[1 + drow[d]][col + dcol[d]] - 1 for d in range(3))
+
+    print(*B[0], sep = ' ')
     for row in range(1, M):
-        for col in range(1, M):
-            B[row][col] += max(B[row + drow[d]][col + dcol[d]] - 1 for d in range(3))
-
-    for line in B:
-        print(*line, sep = ' ')
-
+        print(B[row][0], end = ' ')
+        print(*B[1][1:], sep = ' ')
 
 M, N = map(int, input().split())
 grows = [list(map(int, input().split())) for _ in range(N)]
