b0ba3bc7de6c7b0cd687fe962d7859594cda2591
Revi1337 <david122123@gmail.com>
2025-07-22 20:16:18 +0900

    [Silver III] Title: 두 개의 배열, Time: 576 ms, Memory: 200396 KB -BaekjoonHub

--- 백준/Silver/17124. 두 개의 배열/두 개의 배열.py
+++ 백준/Silver/17124. 두 개의 배열/두 개의 배열.py
@@ -2,21 +2,22 @@ def solution(N, A, M, B):
     B.sort()
     total = 0
 
-    for idx in range(N):
-        curr = A[idx]
+    for num in A:
         left, right = 0, M - 1
-        clo = B[0]
-
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            if abs(B[mid] - curr) < abs(clo - curr) or (abs(B[mid] - curr) == abs(clo - curr) and B[mid] < clo):
-                clo = B[mid]
-            if B[mid] < curr:
+            if B[mid] <= num:
                 left = mid + 1
             else:
-                right = mid - 1
+                right = mid
 
-        total += clo
+        if left == 0:
+            total += B[left]
+        else:
+            if num - B[left - 1] > B[left] - num:
+                total += B[left]
+            else:
+                total += B[left - 1]
 
     return total
 
