986336f294be75eca9f524d44814f7481475d237
Revi1337 <david122123@gmail.com>
2025-07-24 21:47:35 +0900

    [Silver II] Title: Three Dots, Time: 424 ms, Memory: 111264 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.py
+++ 백준/Silver/13423. Three Dots/Three Dots.py
@@ -1,18 +1,13 @@
 def solution(T, N, arr):
     arr.sort()
+    cache = set(arr)
     ans = 0
-    for idx in range(N):
-        for jdx in range(idx + 1, N):
-            diff = arr[jdx] - arr[idx]
-            left, right = jdx + 1, N - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if arr[mid] - arr[jdx] == diff:
-                    ans += 1
-                if arr[mid] - arr[jdx] < diff:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+    for left in range(N):
+        for mid in range(left + 1, N):
+            right = arr[mid] + (arr[mid] - arr[left])
+            if right in cache:
+                ans += 1
+
     return ans
 
 T = int(input())
