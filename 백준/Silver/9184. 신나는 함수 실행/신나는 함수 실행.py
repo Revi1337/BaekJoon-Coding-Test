70fd5640c37f84f4dce3244d6e28c89a603fb3c9
Revi1337 <david122123@gmail.com>
2025-08-02 14:30:03 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 176 ms, Memory: 122768 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -14,10 +14,17 @@ def solution(lst):
             memo[a][b][c] = w(20, 20, 20)
             return memo[a][b][c]
         if a < b < c:
-            memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+            memo[a][b][c - 1] = w(a, b, c - 1)
+            memo[a][b - 1][c - 1] = w(a, b - 1, c - 1)
+            memo[a][b - 1][c] = w(a, b - 1, c)
+            memo[a][b][c] = memo[a][b][c - 1] + memo[a][b - 1][c - 1] - memo[a][b - 1][c]
             return memo[a][b][c]
 
-        memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+        memo[a - 1][b][c] = w(a - 1, b, c)
+        memo[a - 1][b - 1][c] = w(a - 1, b - 1, c)
+        memo[a - 1][b][c - 1] = w(a - 1, b, c - 1)
+        memo[a - 1][b - 1][c - 1] = w(a - 1, b - 1, c - 1)
+        memo[a][b][c] = memo[a - 1][b][c] + memo[a - 1][b - 1][c] + memo[a - 1][b][c - 1] - memo[a - 1][b - 1][c - 1]
         return memo[a][b][c]
 
     memo = [[[0] * 101 for _ in range(101)] for _ in range(101)]
