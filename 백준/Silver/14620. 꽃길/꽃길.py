d7d44ffe6c99a37d0bf2e9353ace3b250a67d2a0
Revi1337 <david122123@gmail.com>
2025-08-09 14:00:34 +0900

    [Silver II] Title: 꽃길, Time: 240 ms, Memory: 112020 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -7,41 +7,35 @@ dcol = [0, 1, 0, -1]
 
 def solution(N, arr):
 
+    SIZE = N ** 2
     inside = lambda row, col : 0 <= row < N and 0 <= col < N
 
-    def backtrack(cnt, lst, check):
-        if cnt == 3:
-            poss.append([*lst])
-            return
-        for row in range(N):
-            for col in range(N):
-                if not check[row][col]:
-                    check[row][col] = 1
-                    lst.append([row, col])
-                    backtrack(cnt + 1, lst, check)
-                    lst.pop()
-                    check[row][col] = 0
-
-    poss = []
-    backtrack(0, [], [[0] * (N + 1) for _ in range(N + 1)])
-
     ans = 200 * (10 ** 2)
-    for lst in poss:
-        check = {(lst[0][0], lst[0][1]), (lst[1][0], lst[1][1]), (lst[2][0], lst[2][1])}
-        sm, possbile = sum(arr[row][col] for row, col in lst), True
-        for row, col in lst:
-            for d in range(4):
-                nrow, ncol = row + drow[d], col + dcol[d]
-                if not inside(nrow, ncol) or (nrow, ncol) in check:
-                    possbile = False
-                    break
-                check.add((nrow, ncol))
-                sm += arr[nrow][ncol]
-        if possbile:
-            ans = min(ans, sm)
+    for p1 in range(SIZE):
+        for p2 in range(p1 + 1, SIZE):
+            for p3 in range(p2 + 1, SIZE):
+                check = [[0] * (N + 1) for _ in range(N + 1)]
+                sm, possible = 0, True
+                for p in p1, p2, p3:
+                    row, col = p // N, p % N
+                    if check[row][col]:
+                        possible = False
+                        break
+                    check[row][col] = 1
+                    sm += arr[row][col]
+                    for d in range(4):
+                        nrow, ncol = row + drow[d], col + dcol[d]
+                        if not inside(nrow, ncol) or check[nrow][ncol]:
+                            possible = False
+                            break
+                        check[nrow][ncol] = 1
+                        sm += arr[nrow][ncol]
+                if possible:
+                    ans = min(ans, sm)
 
     return ans
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 print(solution(N, arr))
+
