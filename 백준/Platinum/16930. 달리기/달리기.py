240617b71a5241544739774f461a33be63e8d832
Revi1337 <david122123@gmail.com>
2025-09-02 20:47:51 +0900

    [Platinum III] Title: 달리기, Time: 844 ms, Memory: 251320 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.py
+++ 백준/Platinum/16930. 달리기/달리기.py
@@ -1,8 +1,9 @@
 # 2025-09-01
+# BFS
 # https://www.acmicpc.net/problem/16930
 
 import sys
-import heapq
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -11,30 +12,29 @@ dcol = [0, 1, 0, -1]
 
 def solution(N, M, K, arr, srow, scol, erow, ecol):
 
-    EMPTY, WALL, INF = '.', '#', float('inf')
+    EMPTY, WALL = '.', '#'
     inside = lambda row, col : 0 <= row < N and 0 <= col < M
 
     srow, scol, erow, ecol = srow - 1, scol - 1, erow - 1, ecol - 1
-    check = [[INF] * M for _ in range(N)]
+    check = [[-1] * M for _ in range(N)]
     check[srow][scol] = 0
-    pq = [(0, srow, scol)]
+    queue = deque([[srow, scol]])
 
-    while pq:
-        time, row, col = heapq.heappop(pq)
+    while queue:
+        row, col = queue.popleft()
         if row == erow and col == ecol:
-            return time
-        if check[row][col] < time:
-            continue
+            return check[row][col]
         for d in range(4):
             for off in range(1, K + 1):
                 nrow, ncol = row + drow[d] * off, col + dcol[d] * off
                 if not inside(nrow, ncol) or arr[nrow][ncol] == WALL:
                     break
-                if check[nrow][ncol] <= time:
+                if check[nrow][ncol] != -1 and check[nrow][ncol] <= check[row][col]:
                     break
-                if check[nrow][ncol] == INF:
-                    check[nrow][ncol] = time + 1
-                    heapq.heappush(pq, (time + 1, nrow, ncol))
+                if check[nrow][ncol] == -1:
+                    check[nrow][ncol] = check[row][col] + 1
+                    queue.append([nrow, ncol])
+
     return -1
 
 N, M, K = map(int, input().split())
