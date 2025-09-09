516b9e948967611e4aacd3c586d97d68451e2457
Revi1337 <david122123@gmail.com>
2025-09-09 17:18:11 +0900

    [Silver IV] Title: 수 찾기, Time: 196 ms, Memory: 135972 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,9 +1,25 @@
-n = int(input())
-datas = set(input().split())
-m = int(input())
-targets = list(map(int, input().split()))
-for target in targets:
-    if str(target) in datas:
-        print(1)
-    else:
-        print(0)
+import sys
+
+input = sys.stdin.readline
+
+def solution(N, arr, M, T):
+    arr.sort()
+    for num in T:
+        left, right = 0, N - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == num:
+                print(1)
+                break
+            if arr[mid] > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            print(0)
+
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+T = list(map(int, input().split()))
+solution(N, arr, M, T)
