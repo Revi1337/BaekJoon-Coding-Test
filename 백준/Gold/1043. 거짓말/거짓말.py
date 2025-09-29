f27f551f7ae83b637c868c5396e596db198fdd51
Revi1337 <david122123@gmail.com>
2025-09-29 19:14:49 +0900

    [Gold IV] Title: 거짓말, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,20 +1,44 @@
-def solution(N, M, kcnt, know, parties):
-    answer = []
-    know, parties = set(know), [set(party) for party in parties]
+# 2025-09-29
+# https://www.acmicpc.net/problem/1043
 
-    for _ in range(M):
-        cnt = 0
-        for party in parties:
-            if know & party:
-                know = know.union(party)
-            else:
-                cnt += 1
-        answer.append(cnt)
+import sys
 
-    return min(answer)
+input = sys.stdin.readline
+
+def solution(N, M, arr, P):
+
+    def find(n):
+        if n == parents[n]:
+            return n
+
+        parents[n] = find(parents[n])
+        return parents[n]
+
+    def union(n1, n2):
+        r1, r2 = find(n1), find(n2)
+        if r1 < r2:
+            parents[r2] = r1
+        else:
+            parents[r1] = r2
+
+    parents = list(range(N + 1))
+    for p in P:
+        p = p[1:]
+        for idx in range(len(p) - 1):
+            union(p[idx], p[idx + 1])
+
+    ans = 0
+    arr = set(find(h) for h in arr[1:])
+    for p in P:
+        for h in p[1:]:
+            if find(h) in arr:
+                break
+        else:
+            ans += 1
+
+    return ans
 
 N, M = map(int, input().split())
-k = list(map(int, input().split()))
-kcnt, know = k[0], k[1:]
-parties = [list(map(int, input().split()))[1:] for _ in range(M)]
-print(solution(N, M, kcnt, know, parties))
+arr = list(map(int, input().split()))
+P = [list(map(int, input().split())) for _ in range(M)]
+print(solution(N, M, arr, P))
