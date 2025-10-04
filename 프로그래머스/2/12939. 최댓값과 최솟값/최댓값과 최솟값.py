23add4c2b03dc148b36afefc979aeface6b74cad
Revi1337 <david122123@gmail.com>
2025-10-04 13:46:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,4 @@
-def solution(s):
-    s = list(map(int, s.split()))
-    mx, mn = max(s), min(s)
-    return " ".join(map(str, [mn, mx]))
+def solution(string):
+    nums = list(map(int, string.split(' ')))
+    mn, mx = min(nums), max(nums)
+    return f'{mn} {mx}'
