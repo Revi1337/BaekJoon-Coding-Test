ce246e24863f8743dc7a43d7a53334f2ac8d4ae2
Revi1337 <david122123@gmail.com>
2025-10-10 02:39:51 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 92 ms, Memory: 39336 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -1,29 +1,43 @@
-def solution(N, edges, A, B):
+import sys
 
-    graph = [[] for _ in range(N + 1)]
-    for v1, v2 in edges:
-        graph[v2].append(v1)
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
 
-    check = [0] * (N + 1)
-    for node in A, B:
-        if not check[node]:
-            check[node] = 1
-        else:
-            return node
+def solution(N, E, V1, V2):
 
-        while True:
-            if graph[node]:
-                node = graph[node][0]
-                if not check[node]:
-                    check[node] = 1
-                else:
-                    return node
-            else:
-                break
+    def make_tree(n, pn, lv):
+        lvs[n] = lv
+        for nn in tree[n]:
+            if nn != pn:
+                make_tree(nn, n, lv + 1)
+
+    tree = [[] for _ in range(N + 1)]
+    refers, parents = [[0] * (N + 1) for _ in range(2)]
+    for v1, v2 in E:
+        tree[v1].append(v2)
+        parents[v2] = v1
+        refers[v2] += 1
+
+    root, lvs = refers[1:].index(min(refers[1:])) + 1, [0] * (N + 1)
+    make_tree(root, -1, 0)
+
+    mxn = V1 if lvs[V1] >= lvs[V2] else V2
+    mnn = V2 if mxn == V1 else V1
+    diff = abs(lvs[V1] - lvs[V2])
+
+    while diff > 0:
+        mxn = parents[mxn]
+        diff -= 1
+
+    while mxn != mnn:
+        mxn, mnn = parents[mxn], parents[mnn]
+
+    return mxn
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    edges = [list(map(int, input().split())) for _ in range(N - 1)]
-    A, B = map(int, input().split())
-    print(solution(N, edges, A, B))
+    E = [list(map(int, input().split())) for _ in range(N - 1)]
+    V1, V2 = map(int, input().split())
+    print(solution(N, E, V1, V2))
+
