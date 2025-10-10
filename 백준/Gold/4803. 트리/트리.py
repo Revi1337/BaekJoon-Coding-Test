bf6935ce9fc8b3f6d4ecdd670c597934441f58b2
Revi1337 <david122123@gmail.com>
2025-10-10 14:27:52 +0900

    [Gold IV] Title: 트리, Time: 268 ms, Memory: 130172 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,38 +1,33 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
-
 def solution(N, M, E):
 
     def find(n):
-        if n == P[n]:
+        if n == parents[n]:
             return n
 
-        P[n] = find(P[n])
-        return P[n]
+        parents[n] = find(parents[n])
+        return parents[n]
 
     def union(n1, n2):
         root1, root2 = find(n1), find(n2)
-        if root2 > root1:
-            P[root2] = root1
+        if root1 > root2:
+            parents[root1] = root2
         else:
-            P[root1] = root2
+            parents[root2] = root1
 
-    P, C = list(range(N + 1)), set()
-    for n1, n2 in E:
-        r1, r2 = find(n1), find(n2)
-        if r1 != r2:
-            union(n1, n2)
+    parents, cycle = list(range(N + 1)), set()
+    for v1, v2 in E:
+        if find(v1) != find(v2):
+            union(v1, v2)
         else:
-            C.add(n1); C.add(n2)
+            cycle.add(v1)
+            cycle.add(v2)
 
-    P = [find(n) for n in range(N + 1)]
-    G = {P[cn] for cn in C}
-    rlen = len({P[n] for n in range(1, N + 1) if P[n] not in G})
+    parents = [find(n) for n in range(N + 1)]
+    cycle_roots = {parents[cn] for cn in cycle}
+    rlen = len({parents[n] for n in range(1, N + 1) if parents[n] not in cycle_roots})
+
     if not rlen:
         return 'No trees.'
-
     return 'There is one tree.' if rlen == 1 else f'A forest of {rlen} trees.'
 
 idx = 1
