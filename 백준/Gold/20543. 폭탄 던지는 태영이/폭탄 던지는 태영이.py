81e83214ecc7bc1ad32ecac2a4d476a58100eb33
Revi1337 <david122123@gmail.com>
2025-10-13 00:05:10 +0900

    [Gold I] Title: 폭탄 던지는 태영이, Time: 1320 ms, Memory: 224084 KB -BaekjoonHub

--- 백준/Gold/20543. 폭탄 던지는 태영이/폭탄 던지는 태영이.py
+++ 백준/Gold/20543. 폭탄 던지는 태영이/폭탄 던지는 태영이.py
@@ -14,21 +14,10 @@ def solution(N, M, H):
     ans = [[0] * N for _ in range(N)]
     for row in range(N):
         for col in range(N):
-            center = 0
-            if row >= half and col >= half:
-                center = -acc[row - half][col - half]
-
-            top = 0
-            if row >= M:
-                top = ans[row - M][col]
-
-            left = 0
-            if col >= M:
-                left = ans[row][col - M]
-
-            top_left_dup = 0
-            if row >= M and col >= M:
-                top_left_dup = ans[row - M][col - M]
+            center = -acc[row - half][col - half] if row >= half and col >= half else 0
+            top = ans[row - M][col] if row >= M else 0
+            left = ans[row][col - M] if col >= M else 0
+            top_left_dup = ans[row - M][col - M] if row >= M and col >= M else 0
 
             ans[row][col] = center + top + left - top_left_dup
 
