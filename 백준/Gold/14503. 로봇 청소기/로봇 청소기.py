c9c11cbd3f875a4d291ca860b5a3abe01a300cda
Revi1337 <david122123@gmail.com>
2025-10-23 15:39:42 +0900

    [Gold V] Title: 로봇 청소기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,31 +1,29 @@
-def solution(x, y, d, board):
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    board[x][y] = 2
-    answer = 1
+drow = [-1, 0, 1, 0]
+dcol = [0, 1, 0, -1]
+
+def solution(N, M, row, col, sd, arr):
+    ans = 0
     while True:
-        flag = False
-        for _ in range(4):
-            d = (d + 3) % 4
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if board[nx][ny] == 0:
-                x = nx
-                y = ny
-                board[x][y] = 2
-                answer += 1
-                flag = True
+        if not arr[row][col]:
+            ans += 1
+            arr[row][col] = 2
+
+        cleaned = True
+        for d in range(4):
+            if not arr[row + drow[d]][col + dcol[d]]:
+                cleaned = False
                 break
-        if flag == False:
-            if board[x - dx[d]][y - dy[d]] == 1:
-                return answer
-            else:
-                x = x - dx[d]
-                y = y - dy[d]
-    return answer
 
+        if cleaned:
+            row, col = row + drow[sd] * -1, col + dcol[sd] * -1
+            if arr[row][col] == 1:
+                return ans
+        else:
+            sd = (sd - 1) % 4
+            if arr[row + drow[sd]][col + dcol[sd]] == 0:
+                row, col = row + drow[sd], col + dcol[sd]
 
-n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-print(solution(r, c, d, arr))
+N, M = map(int, input().split())
+row, col, sd = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, M, row, col, sd, arr))
