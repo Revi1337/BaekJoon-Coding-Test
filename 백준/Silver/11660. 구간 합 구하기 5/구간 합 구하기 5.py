a94876ec9d4a775c1b69232e69429d14a7ec5e6e
Revi1337 <david122123@gmail.com>
2026-03-11 23:37:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 372 ms, Memory: 140420 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,20 +1,19 @@
-import sys
+# 2026-03-11
+# https://www.acmicpc.net/problem/11660
+# 2d arr psum
 
-input = sys.stdin.readline
+def solution(N, M, arr, T):
+    psum = [[0] * (N + 1) for _ in range(N + 1)]
+    for idx in range(1, N + 1):
+        for jdx in range(1, N + 1):
+            psum[idx][jdx] = psum[idx - 1][jdx] + psum[idx][jdx - 1] - psum[idx - 1][jdx - 1] + arr[idx - 1][jdx - 1]
 
-def solution(n, board, require):
-    prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
-    for row in range(1, n + 1):
-        for col in range(1, n + 1):
-            prefix_sum[row][col] = prefix_sum[row][col - 1] \
-                                   + prefix_sum[row - 1][col] \
-                                   - prefix_sum[row - 1][col - 1] \
-                                   + board[row - 1][col - 1]
+    for r1, c1, r2, c2 in T:
+        sm = psum[r2][c2] - psum[r1 - 1][c2] - psum[r2][c1 - 1] + psum[r1 - 1][c1 - 1]
+        print(sm)
 
-    for r1, c1, r2, c2 in require:
-        print(prefix_sum[r2][c2] - prefix_sum[r2][c1 - 1] - prefix_sum[r1 - 1][c2] + prefix_sum[r1 - 1][c1 - 1])
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+T = [list(map(int, input().split())) for _ in range(M)]
+solution(N, M, arr, T)
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
-require = [list(map(int, input().split())) for _ in range(m)]
-solution(n, board, require)
