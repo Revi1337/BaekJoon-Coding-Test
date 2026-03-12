d989213fb91509edc6f4e388a0fde9eab81b0681
Revi1337 <david122123@gmail.com>
2026-03-13 00:42:33 +0900

    [Silver I] Title: 신입 사원, Time: 4560 ms, Memory: 214812 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,14 +1,20 @@
-def solution(N, ranks):
-    ranks.sort()
-    ans, mn = 0, N + 1
-    for _, r2 in ranks:
-        if r2 < mn:
-            mn = r2
-            ans += 1
+# 2026-03-11
+# https://www.acmicpc.net/problem/2512
+# greedy
+
+def solution(N, arr):
+    arr.sort()
+    ans, mn = 0, 1e9
+    for _, v2 in arr:
+        if v2 > mn:
+            continue
+        mn = v2
+        ans += 1
+
     return ans
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    ranks = [list(map(int, input().split())) for _ in range(N)]
-    print(solution(N, ranks))
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    print(solution(N, arr))
