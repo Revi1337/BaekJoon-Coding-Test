846673355e478c6a9d145fcae6df78b6192f3b69
Revi1337 <david122123@gmail.com>
2026-03-13 22:08:51 +0900

    [Gold IV] Title: N-Queen, Time: 5960 ms, Memory: 123032 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -4,22 +4,22 @@
 
 def solution(N):
 
-    def backtrack(srow, scol):
+    def backtrack(srow):
         if srow == N:
             nonlocal ans
             ans += 1
             return
 
-        for ncol in range(scol, N):
+        for ncol in range(N):
             if ch1[srow] == ch2[ncol] == ch3[srow + ncol] == ch4[srow - ncol] == 0:
                 ch1[srow] = ch2[ncol] = ch3[srow + ncol] = ch4[srow - ncol] = 1
-                backtrack(srow + 1, scol)
+                backtrack(srow + 1)
                 ch1[srow] = ch2[ncol] = ch3[srow + ncol] = ch4[srow - ncol] = 0
 
     ch1, ch2 = [[0] * N for _ in range(2)]
     ch3, ch4 = [[0] * (N * 2) for _ in range(2)]
     ans = 0
-    backtrack(0, 0)
+    backtrack(0)
 
     return ans
 
