33225295f17e8019e9ff2cd3698d28670f7804eb
Revi1337 <david122123@gmail.com>
2026-03-15 14:21:15 +0900

    [Gold V] Title: 토마토, Time: 816 ms, Memory: 225956 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,43 +1,45 @@
+# 2026-03-15
+# https://www.acmicpc.net/problem/7569
+# bfs
+
 from collections import deque
 
-drow = [0, 0, -1, 0 , 1, 0]
+dhei = [-1, 1, 0, 0, 0, 0]
+drow = [0, 0, -1, 0, 1, 0]
 dcol = [0, 0, 0, 1, 0, -1]
-dheight = [1, -1, 0, 0, 0, 0]
 
-def solution(m, n, h, boxes):
+def solution(N, M, H, arr):
+
+    inside = lambda hei, row, col: 0 <= hei < H and 0 <= row < N and 0 <= col < M
+
     queue = deque()
-    for height in range(h):
-        for row in range(n):
-            for col in range(m):
-                if boxes[height][row][col] == 1:
-                    queue.append((height, row, col))
+    check = [[[0] * M for _ in range(N)] for _ in range(H)]
+    for hei in range(H):
+        for row in range(N):
+            for col in range(M):
+                if arr[hei][row][col] == 1:
+                    queue.append([hei, row, col, 0])
+                    check[hei][row][col] = 1
+                elif arr[hei][row][col] == -1:
+                    check[hei][row][col] = -1
 
+    ans = 0
     while queue:
-        height, row, col = queue.popleft()
+        hei, row, col, cnt = queue.popleft()
+        ans = cnt
         for d in range(6):
-            nheight = height + dheight[d]
-            nrow = row + drow[d]
-            ncol = col + dcol[d]
-            if (0 <= nheight < h) and (0 <= nrow < n) and (0 <= ncol < m):
-                if boxes[nheight][nrow][ncol] == 0:
-                    boxes[nheight][nrow][ncol] = boxes[height][row][col] + 1
-                    queue.append((nheight, nrow, ncol))
+            nhei, nrow, ncol = hei + dhei[d], row + drow[d], col + dcol[d]
+            if inside(nhei, nrow, ncol) and check[nhei][nrow][ncol] == 0 and arr[nhei][nrow][ncol] == 0:
+                check[nhei][nrow][ncol] = 1
+                queue.append([nhei, nrow, ncol, cnt + 1])
 
-    answer = 0
-    for height in range(h):
-        for row in range(n):
-            for col in range(m):
-                if boxes[height][row][col] == 0:
+    for hei in range(H):
+        for row in range(N):
+            for col in range(M):
+                if not check[hei][row][col]:
                     return -1
-                answer = max(answer, boxes[height][row][col])
+    return ans
 
-    return answer - 1
-
-m, n, h = map(int, input().split())
-boxes = []
-for _ in range(h):
-    box = []
-    for _ in range(n):
-        box.append(list(map(int, input().split())))
-    boxes.append(box)
-print(solution(m, n, h, boxes))
+M, N, H = map(int, input().split())
+arr = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+print(solution(N, M, H, arr))
