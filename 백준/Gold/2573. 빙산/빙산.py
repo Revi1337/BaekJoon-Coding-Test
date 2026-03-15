346d39a503250ee092884ec366b54872d4bd7f13
Revi1337 <david122123@gmail.com>
2026-03-15 16:51:56 +0900

    [Gold IV] Title: 빙산, Time: 684 ms, Memory: 214128 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,54 +1,63 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
-"""
-빙산 (https://www.acmicpc.net/problem/2573)
-2024-09-12
-"""
+# 2026-03-15
+# https://www.acmicpc.net/problem/2573
+# bfs
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(N, M, board):
-    for year in range(1, 90000):
-        tracking = []
-        for row in range(1, N - 1):
-            for col in range(1, M - 1):
-                if board[row][col] > 0:
-                    counter = 0
-                    for d in range(4):
-                        nr, nc = row + drow[d], col + dcol[d]
-                        if board[nr][nc] == 0:
-                            counter += 1
-                    tracking.append((row, col, counter))
-        for row, col, counter in tracking:
-            if board[row][col] - counter <= 0:
-                board[row][col] = 0
-            else:
-                board[row][col] -= counter
+def solution(N, M, arr):
 
-        counter = 0
-        check = set()
-        for row in range(1, N - 1):
-            for col in range(1, M - 1):
-                if board[row][col] > 0 and (row, col) not in check:
-                    counter += 1
-                    check.add((row, col))
-                    queue = deque([(row, col)])
+    inside = lambda row, col: 0 <= row < N and 0 <= col < M
+
+    ices = set()
+    for row in range(N):
+        for col in range(M):
+            if arr[row][col] > 0:
+                ices.add((row, col))
+
+    ans = 0
+    while True:
+        check = [[0] * M for _ in  range(N)]
+        cnt = 0
+        for srow in range(N):
+            for scol in range(M):
+                if not check[srow][scol] and arr[srow][scol] > 0:
+                    cnt += 1
+                    check[srow][scol] = 1
+                    queue = [[srow, scol]]
                     while queue:
-                        r, c = queue.popleft()
+                        row, col = queue.pop()
                         for d in range(4):
-                            nr, nc = r + drow[d], c + dcol[d]
-                            if board[nr][nc] > 0 and (nr, nc) not in check:
-                                check.add((nr, nc))
-                                queue.append((nr, nc))
-                    if counter >= 2:
-                        return year
-        if counter == 0:
+                            nrow, ncol = row + drow[d], col + dcol[d]
+                            if inside(nrow, ncol) and not check[nrow][ncol] and arr[nrow][ncol] > 0:
+                                check[nrow][ncol] = 1
+                                queue.append([nrow, ncol])
+
+        if cnt >= 2:
+            return ans
+        if cnt == 0:
             return 0
 
+        melt = [[0] * M for _ in range(N)]
+        for row in range(N):
+            for col in range(M):
+                if arr[row][col] > 0:
+                    water = 0
+                    for d in range(4):
+                        nrow = row + drow[d]
+                        ncol = col + dcol[d]
+
+                        if arr[nrow][ncol] == 0:
+                            water += 1
+                    melt[row][col] = water
+
+        for row in range(N):
+            for col in range(M):
+                if arr[row][col] > 0:
+                    arr[row][col] = max(0, arr[row][col] - melt[row][col])
+
+        ans += 1
+
 N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-print(solution(N, M, board))
+arr = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, M, arr))
