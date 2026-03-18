afd9247c3fcd1d0c1a1e79c722952ae422426eec
Revi1337 <david122123@gmail.com>
2026-03-19 01:49:05 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 140 ms, Memory: 114372 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,26 +1,22 @@
-import sys
+# 2026-03-18
+# https://www.acmicpc.net/problem/9205
+# bfs
+
 from collections import deque
-
-input = sys.stdin.readline
-
-"""
-맥주 마시면서 걸어가기 (https://www.acmicpc.net/problem/9205)
-2024-09-13
-"""
 
 drow = [-50, 0, 50, 0]
 dcol = [0, 50, 0, -50]
 
-def solution(N, home, marks, fes):
+def solution(N, H, STO, E):
     check = [0] * N
-    queue = deque([(home[0], home[1])])
+    queue = deque([(H[0], H[1])])
     while queue:
         row, col = queue.popleft()
-        if abs(row - fes[0]) + abs(col - fes[1]) <= 1000:
+        if abs(row - E[0]) + abs(col - E[1]) <= 1000:
             return 'happy'
         for idx in range(N):
             if not check[idx]:
-                nrow, ncol = marks[idx]
+                nrow, ncol = STO[idx]
                 if abs(row - nrow) + abs(col - ncol) <= 1000:
                     check[idx] = 1
                     queue.append((nrow, ncol))
@@ -29,7 +25,7 @@ def solution(N, home, marks, fes):
 T = int(input())
 for _ in range(T):
     N = int(input())
-    home = list(map(int, input().split()))
-    marks = [list(map(int, input().split())) for _ in range(N)]
-    fes = list(map(int, input().split()))
-    print(solution(N, home, marks, fes))
+    H = list(map(int, input().split()))
+    STO = [list(map(int, input().split())) for _ in range(N)]
+    E = list(map(int, input().split()))
+    print(solution(N, H, STO, E))
