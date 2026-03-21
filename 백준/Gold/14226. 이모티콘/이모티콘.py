efa51d8daefcc064f3163334de3fbe4376f26339
Revi1337 <david122123@gmail.com>
2026-03-22 01:10:38 +0900

    [Gold IV] Title: 이모티콘, Time: 124 ms, Memory: 118736 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,29 +1,27 @@
+# 2026-03-21
+# https://www.acmicpc.net/problem/14226
+# bfs
+
 from collections import deque
-import sys
 
-input = sys.stdin.readline
+def solution(S):
+    check = [[0] * (S + 1) for _ in range(S + 1)]
+    check[1][0] = 1
+    queue = deque([(1, 0, 0)])  # screen, clipboard, time
 
-"""
-이미 (현재 화면, 클립보드) 가 check 에 들어있다면 무시할 수 있음
-    - 왜? 후에 들어온 튜플은 당연히 시간히 더 오래 걸린것이기 때문 
-"""
-def solution(s):
-    check = dict()
-    check[(1, 0)] = 0
-    queue = deque([(1, 0)])
     while queue:
-        screen, cache = queue.popleft()
-        if screen == s:
-            return check[(screen, cache)]
-        if (screen, screen) not in check.keys():                    # 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장했을 때
-            check[(screen, screen)] = check[(screen, cache)] + 1
-            queue.append((screen, screen))
-        if (screen + cache, cache) not in check.keys():             # 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기했을 때
-            check[(screen + cache, cache)] = check[(screen, cache)] + 1
-            queue.append((screen + cache, cache))
-        if (screen - 1, cache) not in check.keys():                 # 화면에 있는 이모티콘 중 하나를 삭제했을 때
-            check[(screen - 1, cache)] = check[(screen, cache)] + 1
-            queue.append((screen - 1, cache))
+        s, c, t = queue.popleft()
+        if s == S:
+            return t
+        if not check[s][s]:
+            check[s][s] = 1
+            queue.append((s, s, t + 1))
+        if c > 0 and s + c <= S and not check[s + c][c]:
+            check[s + c][c] = 1
+            queue.append((s + c, c, t + 1))
+        if s - 1 >= 0 and not check[s - 1][c]:
+            check[s - 1][c] = 1
+            queue.append((s - 1, c, t + 1))
 
-s = int(input())
-print(solution(s))
+S = int(input())
+print(solution(S))
