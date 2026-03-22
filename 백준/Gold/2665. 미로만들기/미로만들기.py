3e7726cfbbfa8092ed3e3da3e6c4aba57ff6667c
Revi1337 <david122123@gmail.com>
2026-03-22 15:09:34 +0900

    [Gold IV] Title: 미로만들기, Time: 124 ms, Memory: 114200 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,29 +1,31 @@
-import sys
-from collections import deque
+# 2026-03-22
+# https://www.acmicpc.net/problem/2665
+# 0-1 bfs
 
-input = sys.stdin.readline
+from collections import deque
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
-def solution(n, rooms):
-    rooms[0][0] = -1
+def solution(N, arr):
+
+    inside = lambda row, col: 0 <= row < N and 0 <= col < N
+
+    arr[0][0] = -1
     queue = deque([(0, 0, 0)])
     while queue:
         row, col, cnt = queue.popleft()
-        if (row, col) == (n - 1, n - 1):
+        if row == N - 1 and col == N - 1:
             return cnt
         for d in range(4):
-            nrow = row + drow[d]
-            ncol = col + dcol[d]
-            if (0 <= nrow < n) and (0 <= ncol < n) and (rooms[nrow][ncol] != -1):
-                if rooms[nrow][ncol] == 1:
+            nrow, ncol = row + drow[d], col + dcol[d]
+            if inside(nrow, ncol) and arr[nrow][ncol] != -1:
+                if arr[nrow][ncol] == 1:
                     queue.appendleft((nrow, ncol, cnt))
-                elif not rooms[nrow][ncol]:
+                else:
                     queue.append((nrow, ncol, cnt + 1))
-                rooms[nrow][ncol] = -1
+                arr[nrow][ncol] = -1
 
-n = int(input())
-rooms = [list(map(int, input().rstrip())) for _ in range(n)]
-print(solution(n, rooms))
-
+N = int(input())
+arr = [list(map(int, input().rstrip())) for _ in range(N)]
+print(solution(N, arr))
