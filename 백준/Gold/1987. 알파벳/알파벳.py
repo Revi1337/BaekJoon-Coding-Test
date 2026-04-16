f98f0ed97fedad5b66ac8b88554ecba379c91ca5
Revi1337 <david122123@gmail.com>
2026-04-16 17:41:14 +0900

    [Gold IV] Title: 알파벳, Time: 5308 ms, Memory: 160868 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,7 +1,6 @@
 # 2026-04-16
 # https://www.acmicpc.net/problem/1987
-# 알파벳 (백트래킹) bfs 로도 가능할듯?
-
+# 알파벳 (백트래킹 + 비트마스킹) bfs 로도 가능할듯?
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
@@ -12,18 +11,18 @@ def solution(N, M, arr):
     def backtrack(srow, scol, check, cnt):
         nonlocal ans
         ans = max(ans, cnt)
+        if ans == 26:
+            return
         for d in range(4):
             nrow, ncol = srow + drow[d], scol + dcol[d]
             if inside(nrow, ncol):
-                sign = arr[nrow][ncol]
-                if sign not in check:
-                    check.add(sign)
-                    backtrack(nrow, ncol, check, cnt + 1)
-                    check.discard(sign)
+                bit = 1 << (ord(arr[nrow][ncol]) - 65)
+                if not (check & bit):
+                    backtrack(nrow, ncol, check | bit, cnt + 1)
 
     ans = 0
-    backtrack(0, 0, set(arr[0][0]), 1)
-
+    start = 1 << (ord(arr[0][0]) - 65)
+    backtrack(0, 0, start, 1)
     return ans
 
 N, M = map(int, input().split())
