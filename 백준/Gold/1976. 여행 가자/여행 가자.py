340aa667967e5c47f9fc8385c52d2dbacd69e08e
Revi1337 <david122123@gmail.com>
2026-04-18 22:18:28 +0900

    [Gold IV] Title: 여행 가자, Time: 96 ms, Memory: 110864 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,33 +1,35 @@
-import sys
-
-input = sys.stdin.readline
+# 2026-04-18
+# https://www.acmicpc.net/problem/1976
+# 여행 가자
+# Union-Find
 
 def solution(N, M, E, T):
 
     def find(n):
-        if n == parents[n]:
-            return n
+        while n != parents[n]:
+            parents[n] = parents[parents[n]]
+            n = parents[n]
 
-        parents[n] = find(parents[n])
-        return parents[n]
+        return n
 
     def union(n1, n2):
-        root1, root2 = find(n1), find(n2),
-        if root1 < root2:
-            parents[root2] = root1
+        r1, r2 = find(n1), find(n2)
+        if r1 < r2:
+            parents[r2] = r1
         else:
-            parents[root1] = root2
+            parents[r1] = r2
 
     parents = list(range(N + 1))
-    for n, ns in enumerate(E, start=1):
-        for nn in range(len(ns)):
-            if ns[nn] and find(n) != find(nn + 1):
-                union(n, nn + 1)
+    for n1 in range(N):
+        for n2, con in enumerate(E[n1]):
+            if con:
+                union(n1, n2)
 
-    for n in range(1, N + 1):
-        find(n)
-
-    return 'YES' if len(set(parents[t] for t in T)) == 1 else 'NO'
+    ans = [find(n - 1) for n in T]
+    for an in ans:
+        if an != ans[0]:
+            return 'NO'
+    return 'YES'
 
 N = int(input())
 M = int(input())
