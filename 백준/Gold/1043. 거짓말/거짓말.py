bc8fbf4f83582943c71d8fb5ac685efe0d50f67e
Revi1337 <david122123@gmail.com>
2026-04-18 23:18:10 +0900

    [Gold IV] Title: 거짓말, Time: 92 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,44 +1,25 @@
-# 2025-09-29
+# 2026-04-18
 # https://www.acmicpc.net/problem/1043
+# 거짓말
+# DataStructure(Set) or Union-Find
 
-import sys
-
-input = sys.stdin.readline
-
-def solution(N, M, arr, P):
-
-    def find(n):
-        if n == parents[n]:
-            return n
-
-        parents[n] = find(parents[n])
-        return parents[n]
-
-    def union(n1, n2):
-        r1, r2 = find(n1), find(n2)
-        if r1 < r2:
-            parents[r2] = r1
-        else:
-            parents[r1] = r2
-
-    parents = list(range(N + 1))
-    for p in P:
-        p = p[1:]
-        for idx in range(len(p) - 1):
-            union(p[idx], p[idx + 1])
+def solution(N, M, T, E):
+    truth = set(T[1:])
+    for _ in range(M):
+        for lst in E:
+            parti = set(lst[1:])
+            if truth & parti:
+                truth = truth.union(parti)
 
     ans = 0
-    arr = set(find(h) for h in arr[1:])
-    for p in P:
-        for h in p[1:]:
-            if find(h) in arr:
-                break
-        else:
+    for lst in E:
+        parti = set(lst[1:])
+        if not truth & parti:
             ans += 1
 
     return ans
 
 N, M = map(int, input().split())
-arr = list(map(int, input().split()))
-P = [list(map(int, input().split())) for _ in range(M)]
-print(solution(N, M, arr, P))
+T = list(map(int, input().split()))
+E = [list(map(int, input().split())) for _ in range(M)]
+print(solution(N, M, T, E))
