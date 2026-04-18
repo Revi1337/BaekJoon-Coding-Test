a1d9ce5ec337b05d06ebb44492126873cecd7e6c
Revi1337 <david122123@gmail.com>
2026-04-18 23:52:23 +0900

    [Gold IV] Title: 사이클 게임, Time: 876 ms, Memory: 227888 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,30 +1,30 @@
-import sys
-
-input = sys.stdin.readline
+# 2026-04-18
+# https://www.acmicpc.net/problem/20040
+# 사이클 게임
 
 def solution(N, M, E):
 
     def find(n):
-        if n == parent[n]:
-            return parent[n]
-
-        parent[n] = find(parent[n])
-        return parent[n]
+        while n != parents[n]:
+            parents[n] = parents[parents[n]]
+            n = parents[n]
+        return n
 
     def union(n1, n2):
-        root1, root2 = find(n1), find(n2)
-        if root1 < root2:
-            parent[root2] = root1
+        r1, r2 = find(n1), find(n2)
+        if r1 < r2:
+            parents[r2] = r1
         else:
-            parent[root1] = root2
+            parents[r1] = r2
 
-    parent = list(range(N + 1))
-    for idx, (v1, v2) in enumerate(E, start=1):
-        if find(v1) == find(v2):
-            return idx
-        union(v1, v2)
+    parents = list(range(N))
+    for idx, (n1, n2) in enumerate(E):
+        if find(n1) == find(n2):
+            return idx + 1
+        union(n1, n2)
 
     return 0
+
 
 N, M = map(int, input().split())
 E = [list(map(int, input().split())) for _ in range(M)]
