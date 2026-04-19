9bb258d48fe2fd2d3c4219daeaaec766c8a84f6c
Revi1337 <david122123@gmail.com>
2026-04-19 17:19:06 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 416 ms, Memory: 147604 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -1,29 +1,37 @@
-def solution(N, edges):
+# 2026-04-19
+# https://www.acmicpc.net/problem/7511
+# 소셜 네트워킹 어플리케이션
+# Union-Find
+
+def solution(N, E):
 
     def find(n):
-        if island[n] == n:
-            return n
+        while n != parents[n]:
+            parents[n] = parents[parents[n]]
+            n = parents[n]
+        return n
 
-        island[n] = find(island[n])
-        return island[n]
-
-    def union(i1, i2):
-        r1, r2 = find(i1), find(i2)
+    def union(n1, n2):
+        r1, r2 = find(n1), find(n2)
         if r1 < r2:
-            island[r2] = r1
+            parents[r2] = r1
         else:
-            island[r1] = r2
+            parents[r1] = r2
 
-    island = list(range(0, N + 1))
-    for i1, i2 in edges:
-        union(i1, i2)
+    parents = list(range(N + 1))
+    for n1, n2 in E:
+        union(n1, n2)
 
-    init = find(1)
-    for land in range(2, N + 1):
-        if init != find(land):
-            print(1, land)
-            return
+    roots = [find(n) for n in range(N + 1)]
+
+    ans = [1, None]
+    for n in range(1, N + 1):
+        if roots[n] != roots[ans[0]]:
+            ans[1] = n
+            break
+
+    print(*ans)
 
 N = int(input())
-edges = [list(map(int, input().split())) for _ in range(N - 2)]
-solution(N, edges)
+E = [list(map(int, input().split())) for _ in range(N - 2)]
+solution(N, E)
