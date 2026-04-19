c9344a340de958dae45724582f1e293445c645f4
Revi1337 <david122123@gmail.com>
2026-04-19 18:55:11 +0900

    [Gold V] Title: 링크와 스타트, Time: 292 ms, Memory: 111932 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
@@ -1,36 +1,34 @@
 # 2026-04-19
 # https://www.acmicpc.net/problem/15661
 # 링크와 스타트
-# V3. Backtracking (탐색 범위를 줄이자) + 간단히 컷엣지
+# V4. Backtracking (탐색 범위를 줄이자) + 간단히 컷엣지 + 캐싱
 
 def solution(N, arr):
 
-    def backtrack(n, left, right):
+    def backtrack(n, lsum, rsum, left, right):
         nonlocal ans
         if ans == 0:
             return
         if n == N:
             if left and right:
-                lsm = rsm = 0
-                for idx in range(len(left)):
-                    for jdx in range(idx + 1, len(left)):
-                        lsm += arr[left[idx]][left[jdx]] + arr[left[jdx]][left[idx]]
-                for idx in range(len(right)):
-                    for jdx in range(idx + 1, len(right)):
-                        rsm += arr[right[idx]][right[jdx]] + arr[right[jdx]][right[idx]]
-                ans = min(ans, abs(lsm - rsm))
+                ans = min(ans, abs(lsum - rsum))
             return
 
         left.append(n)
-        backtrack(n + 1, left, right)
+        backtrack(n + 1, lsum + sum(cache[n][m] for m in left), rsum, left, right)
         left.pop()
 
         right.append(n)
-        backtrack(n + 1, left, right)
+        backtrack(n + 1, lsum, rsum + sum(cache[n][m] for m in right), left, right)
         right.pop()
 
+    cache = [[0] * N for _ in range(N)]
+    for n1 in range(N):
+        for n2 in range(N):
+            cache[n1][n2] = arr[n1][n2] + arr[n2][n1]
+
     ans = 100 * (N ** 2)
-    backtrack(1, [0], [])
+    backtrack(1, 0, 0, [0], [])
 
     return ans
 
