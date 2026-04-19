aa208986046bc33de02c0b5ec4421195bf5eb642
Revi1337 <david122123@gmail.com>
2026-04-19 14:42:53 +0900

    [Gold IV] Title: 친구비, Time: 160 ms, Memory: 113280 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.py
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -1,40 +1,40 @@
-import sys
+# 2026-04-19
+# https://www.acmicpc.net/problem/16562
+# 친구비
 
-input = sys.stdin.readline
-
-def solution(N, M, K, C, E):
+def solution(N, M, K, A, E):
 
     def find(n):
-        if n == parents[n]:
-            return n
-
-        parents[n] = find(parents[n])
-        return parents[n]
+        while n != parents[n]:
+            parents[n] = parents[parents[n]]
+            n = parents[n]
+        return n
 
     def union(n1, n2):
-        root1, root2 = find(n1), find(n2)
-        if root1 < root2:
-            parents[root2] = root1
+        r1, r2 = find(n1), find(n2)
+        if r1 < r2:
+            parents[r2] = r1
         else:
-            parents[root1] = root2
+            parents[r1] = r2
 
-    parents = list(range(N + 1))
-    for v1, v2 in E:
-        if find(v1) != find(v2):
-            union(v1, v2)
+    parents = list(range(N))
+    for n1, n2 in E:
+        n1, n2 = n1 - 1, n2 - 1
+        union(n1, n2)
 
-    for n in range(1, N + 1):
-        find(n)
+    lookup = {}
+    for n in range(N):
+        root = find(n)
+        lookup.setdefault(root, []).append(n)
 
-    counter = dict()
-    for n, pn in enumerate(parents[1:], start=1):
-        counter[pn] = counter.get(pn, [])
-        counter[pn].append(n)
+    cost = 0
+    for root in lookup:
+        comp = lookup[root]
+        cost += min(A[n] for n in comp)
 
-    ans = sum([min(map(lambda n: C[n - 1], lst)) for key, lst in counter.items()])
-    return 'Oh no' if ans > K else ans
+    return cost if cost <= K else 'Oh no'
 
 N, M, K = map(int, input().split())
-C = list(map(int, input().split()))
+A = list(map(int, input().split()))
 E = [list(map(int, input().split())) for _ in range(M)]
-print(solution(N, M, K, C, E))
+print(solution(N, M, K, A, E))
