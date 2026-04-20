be0f2426f590171be2bdbc95b0c7340e437aac02
Revi1337 <david122123@gmail.com>
2026-04-20 22:40:50 +0900

    [Gold III] Title: 군사 이동, Time: 180 ms, Memory: 123192 KB -BaekjoonHub

--- 백준/Gold/11085. 군사 이동/군사 이동.py
+++ 백준/Gold/11085. 군사 이동/군사 이동.py
@@ -1,35 +1,43 @@
-# 2025-11-21
+# 2026-04-19
 # https://www.acmicpc.net/problem/11085
+# 군사 이동
+# V1. Binary Search + BFS
 
-import sys
+from collections import deque
 
-input = sys.stdin.readline
+def solution(P, W, start, end, E):
 
-def solution(P, W, C, V, E):
+    def possible(limit):
+        check = [0] * P
+        queue = deque([start])
+        check[start] = 1
 
-    def find(n):
-        if n == parents[n]:
-            return n
-        parents[n] = find(parents[n])
-        return parents[n]
+        while queue:
+            n = queue.popleft()
+            if n == end:
+                return True
+            for nn, w in graph[n]:
+                if not check[nn] and w >= limit:
+                    check[nn] = True
+                    queue.append(nn)
+        return False
 
-    def union(n1, n2):
-        r1, r2 = find(n1), find(n2)
-        if r1 != r2:
-            if r1 > r2:
-                parents[r1] = r2
-            else:
-                parents[r2] = r1
+    graph = [[] for _ in range(P)]
+    for a, b, w in E:
+        graph[a].append((b, w))
+        graph[b].append((a, w))
 
-    E.sort(key=lambda x: -x[2])
-    parents = list(range(P + 1))
-    for n1, n2, c in E:
-        union(n1, n2)
-        if find(C) == find(V):
-            return c
+    left, right = 1, 1000
+    while left <= right:
+        mid = (left + right) // 2
+        if possible(mid):
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left - 1
 
 P, W = map(int, input().split())
 C, V = map(int, input().split())
-E = [list(map(int, input().split())) for _ in range(W)]
+E = [tuple(map(int, input().split())) for _ in range(W)]
 print(solution(P, W, C, V, E))
-
