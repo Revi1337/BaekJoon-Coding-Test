fce7c3a5772658f89859910d6d400680de3e6724
Revi1337 <david122123@gmail.com>
2026-04-20 21:05:42 +0900

    [Gold III] Title: 중량제한, Time: 364 ms, Memory: 151976 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,36 +1,48 @@
+# 2026-04-19
+# https://www.acmicpc.net/problem/1939
+# 중량제한
+# V1. Binary Search + BFS
+
 from collections import deque
 
-def solution(N, M, bridges, start, end):
-    graph = [[] for _ in range(N + 1)]
-    for a, b, c in bridges:
-        graph[a].append((b, c))
-        graph[b].append((a, c))
+def solution(N, M, E, arr):
 
-    def bfs(weight):
-        visited = [False] * (N + 1)
-        queue = deque([start])
-        visited[start] = True
+    st, end = arr
+
+    def possible(lim):
+        check = [0] * (N + 1)
+        check[st] = 1
+        queue = deque([st])
         while queue:
-            cur = queue.popleft()
-            for nxt, limit in graph[cur]:
-                if not visited[nxt] and limit >= weight:
-                    visited[nxt] = True
-                    queue.append(nxt)
-        return visited[end]
+            n = queue.popleft()
+            if n == end:
+                return True
+            for nn, nlim in graph[n]:
+                if nlim >= lim and not check[nn]:
+                    check[nn] = 1
+                    queue.append(nn)
+        return False
+
+    graph = [[] for _ in range(N + 1)]
+    for v1, v2, lim in E:
+        graph[v1].append([v2, lim])
+        graph[v2].append([v1, lim])
+
+    for ns in graph:
+        ns.sort(key=lambda x: x[1])
 
     left, right = 1, 1_000_000_000
-    answer = 0
     while left <= right:
         mid = (left + right) // 2
-        if bfs(mid):
-            answer = mid
+        if possible(mid):
             left = mid + 1
         else:
             right = mid - 1
 
-    return answer
+    return right
 
 N, M = map(int, input().split())
-bridges = [list(map(int, input().split())) for _ in range(M)]
-start, end = map(int, input().split())
-print(solution(N, M, bridges, start, end))
+E = [list(map(int, input().split())) for _ in range(M)]
+arr = list(map(int, input().split()))
+print(solution(N, M, E, arr))
+
