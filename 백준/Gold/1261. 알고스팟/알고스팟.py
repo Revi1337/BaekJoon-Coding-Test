5efcaf765829b0f314b49640c3fb6013f41418cd
Revi1337 <david122123@gmail.com>
2026-04-21 20:22:58 +0900

    [Gold IV] Title: 알고스팟, Time: 136 ms, Memory: 114696 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,9 +1,13 @@
 # 2026-04-21
 # https://www.acmicpc.net/problem/1261
 # 알고스팟
-# V2. dijkstra
+# V1. bfs(중복 방문을 허용한)
+# -> dist 갱신되면 무조건 push
+# -> 순서가 중요하지 않음.
+# -> queue 는 그냥 작업 리스트 ㅇㅇ
+# == 언제든 재처리하는 DP relaxation
 
-import heapq
+from collections import deque
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
@@ -12,25 +16,22 @@ def solution(N, M, arr):
 
     inside = lambda row, col: 0 <= row < N and 0 <= col < M
 
-    INF = float('inf')
+    INF = N * M
     dist = [[INF] * M for _ in range(N)]
     dist[0][0] = 0
-    pq = [[0, 0, 0]]
+    queue = deque([(0, 0)])
 
-    while pq:
-        cost, row, col = heapq.heappop(pq)
-        if cost > dist[row][col]:
-            continue
-        if row == N - 1 and col == M - 1:
-            return dist[row][col]
+    while queue:
+        row, col = queue.popleft()
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
             if inside(nrow, ncol):
-                if cost + arr[nrow][ncol] < dist[nrow][ncol]:
-                    dist[nrow][ncol] = cost + arr[nrow][ncol]
-                    heapq.heappush(pq, [dist[nrow][ncol], nrow, ncol])
+                nc = arr[nrow][ncol]
+                if dist[row][col] + nc < dist[nrow][ncol]:
+                    dist[nrow][ncol] = dist[row][col] + nc
+                    queue.append((nrow, ncol))
 
-    return None # Impossible
+    return dist[N - 1][M - 1]
 
 M, N = map(int, input().split())
 arr = [list(map(int, list(input().rstrip()))) for _ in range(N)]
