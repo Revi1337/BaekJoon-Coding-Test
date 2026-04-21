3c597b22b0c8354c39a2ba229956cafd1fd3909d
Revi1337 <david122123@gmail.com>
2026-04-21 20:52:25 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 172 ms, Memory: 111844 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,13 +1,9 @@
 # 2026-04-21
 # https://www.acmicpc.net/problem/4485
 # 녹색 옷 입은 애가 젤다지?
-# V1. bfs(중복 방문을 허용한)
-# -> dist 갱신되면 무조건 push
-# -> 순서가 중요하지 않음.
-# -> queue 는 그냥 작업 리스트 ㅇㅇ
-# == 언제든 재처리하는 DP relaxation
+# V3. dijkstra
 
-from collections import deque
+import heapq
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
@@ -16,22 +12,25 @@ def solution(seq, N, arr):
 
     inside = lambda row, col: 0 <= row < N and 0 <= col < N
 
-    INF = 9 * N * N
+    INF = float('inf')
     dist = [[INF] * N for _ in range(N)]
     dist[0][0] = arr[0][0]
-    queue = deque([(0, 0)])
+    pq = [[dist[0][0], 0, 0]]
 
-    while queue:
-        row, col = queue.popleft()
+    while pq:
+        cost, row, col = heapq.heappop(pq)
+        if cost > dist[row][col]:
+            continue
+        if row == col == N - 1:
+            return f'Problem {seq}: {dist[N - 1][N - 1]}'
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
             if inside(nrow, ncol):
-                nc = arr[nrow][ncol]
-                if dist[row][col] + nc < dist[nrow][ncol]:
-                    dist[nrow][ncol] = dist[row][col] + nc
-                    queue.append((nrow, ncol))
+                if cost + arr[nrow][ncol] < dist[nrow][ncol]:
+                    dist[nrow][ncol] = cost + arr[nrow][ncol]
+                    heapq.heappush(pq, [dist[nrow][ncol], nrow, ncol])
 
-    return f'Problem {seq}: {dist[N - 1][N - 1]}'
+    return None # Impossible
 
 seq = 1
 while True:
