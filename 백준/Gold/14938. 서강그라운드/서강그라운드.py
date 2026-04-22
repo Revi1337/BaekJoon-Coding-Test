5d17366b4464e35cce21cd93f2ba1bd64e2fddca
Revi1337 <david122123@gmail.com>
2026-04-22 19:55:16 +0900

    [Gold IV] Title: 서강그라운드, Time: 148 ms, Memory: 112040 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,25 +1,31 @@
 # 2026-04-22
 # https://www.acmicpc.net/problem/14938
 # 서강그라운드
-# V1. Floyd-Warshall
+# V2. dijkstra
+
+import heapq
 
 def solution(N, M, R, T, E):
     T.insert(0, 0)
 
+    graph = [[] for _ in range(N + 1)]
+    for a, b, l in E:
+        graph[a].append([b, l])
+        graph[b].append([a, l])
+
     INF = float('inf')
     dist = [[INF] * (N + 1) for _ in range(N + 1)]
-    for n in range(1, N + 1):
-        dist[n][n] = 0
-
-    for a, b, l in E:
-        dist[a][b] = min(dist[a][b], l)
-        dist[b][a] = min(dist[b][a], l)
-
-    for mid in range(1, N + 1):
-        for st in range(1, N + 1):
-            for end in range(1, N + 1):
-                if dist[st][end] > dist[st][mid] + dist[mid][end]:
-                    dist[st][end] = dist[st][mid] + dist[mid][end]
+    for st in range(1, N + 1):
+        dist[st][st] = 0
+        pq = [[dist[st][st], st]]
+        while pq:
+            c, n = heapq.heappop(pq)
+            if c > dist[st][n]:
+                continue
+            for nn, nc in graph[n]:
+                if c + nc < dist[st][nn]:
+                    dist[st][nn] = c + nc
+                    heapq.heappush(pq, [dist[st][nn], nn])
 
     ans = 0
     for st in range(1, N + 1):
