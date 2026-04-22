29d7d37969bbc2dae780ffbc82adc3b9591cfa12
Revi1337 <david122123@gmail.com>
2026-04-22 21:28:38 +0900

    [Gold IV] Title: 미로만들기, Time: 124 ms, Memory: 114420 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,9 +1,9 @@
 # 2026-04-22
 # https://www.acmicpc.net/problem/2665
 # 미로 만들기
-# V2. dijkstra
+# V3. bfs(0-1)
 
-import heapq
+from collections import deque
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
@@ -15,24 +15,20 @@ def solution(N, arr):
     INF = N * N
     dist = [[INF] * N for _ in range(N)]
     dist[0][0] = 0
-    pq = [[dist[0][0], 0, 0]]
-    while pq:
-        cost, row, col = heapq.heappop(pq)
-        if cost > dist[row][col]:
-            continue
-        if row == col == N - 1:
-            return dist[row][col]
+    queue = deque([(0, 0)])
+    while queue:
+        row, col = queue.popleft()
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
             if inside(nrow, ncol):
                 if arr[nrow][ncol] and dist[row][col] < dist[nrow][ncol]:
                     dist[nrow][ncol] = dist[row][col]
-                    heapq.heappush(pq, [dist[nrow][ncol], nrow, ncol])
+                    queue.appendleft((nrow, ncol))
                 elif not arr[nrow][ncol] and dist[row][col] + 1 < dist[nrow][ncol]:
                     dist[nrow][ncol] = dist[row][col] + 1
-                    heapq.heappush(pq, [dist[nrow][ncol], nrow, ncol])
+                    queue.append((nrow, ncol))
 
-    return None # Impossible
+    return dist[N - 1][N - 1]
 
 N = int(input())
 arr = [list(map(int, input().rstrip())) for _ in range(N)]
