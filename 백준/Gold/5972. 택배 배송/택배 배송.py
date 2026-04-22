9dd8af9aab5fcb4194f2441dac85a33549c745c7
Revi1337 <david122123@gmail.com>
2026-04-22 20:54:03 +0900

    [Gold V] Title: 택배 배송, Time: 284 ms, Memory: 133464 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,28 +1,32 @@
+# 2026-04-22
+# https://www.acmicpc.net/problem/5972
+# 택배 배송
+
 import heapq
 
-INF = float('inf')
-
-def solution(N, M, edges):
+def solution(N, M, E):
     graph = [[] for _ in range(N + 1)]
-    for v1, v2, cost in edges:
-        graph[v1].append([v2, cost])
-        graph[v2].append([v1, cost])
+    for a, b, c in E:
+        graph[a].append([b, c])
+        graph[b].append([a, c])
 
-    costs = [INF] * (N + 1)
-    costs[1] = 0
-    pq = [[costs[1], 1]]
+    INF = float('inf')
+    dist = [INF] * (N + 1)
+    dist[1] = 0
+    pq = [[dist[1], 1]]
 
     while pq:
-        cost, ver = heapq.heappop(pq)
-        if cost > costs[ver]:
+        c, n = heapq.heappop(pq)
+        if c > dist[n]:
             continue
-        for nver, ncost in graph[ver]:
-            if cost + ncost < costs[nver]:
-                costs[nver] = cost + ncost
-                heapq.heappush(pq, [costs[nver], nver])
+        if n == N:
+            return dist[n]
+        for nn, nc in graph[n]:
+            if c + nc < dist[nn]:
+                dist[nn] = c + nc
+                heapq.heappush(pq, [dist[nn], nn])
 
-    return costs[N]
 
 N, M = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(M)]
-print(solution(N, M, edges))
+E = [list(map(int, input().split())) for _ in range(M)]
+print(solution(N, M, E))
