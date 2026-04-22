af436aec8f25cad62575189a19ee44780c500d4d
Revi1337 <david122123@gmail.com>
2026-04-22 21:16:32 +0900

    [Gold IV] Title: 미로만들기, Time: 128 ms, Memory: 114420 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,6 +1,7 @@
-# 2026-03-22
+# 2026-04-22
 # https://www.acmicpc.net/problem/2665
-# 0-1 bfs
+# 미로 만들기 0-1 bfs
+# V1. bfs(중복을 허용한)
 
 from collections import deque
 
@@ -11,20 +12,23 @@ def solution(N, arr):
 
     inside = lambda row, col: 0 <= row < N and 0 <= col < N
 
-    arr[0][0] = -1
-    queue = deque([(0, 0, 0)])
+    INF = N * N
+    dist = [[INF] * N for _ in range(N)]
+    dist[0][0] = 0
+    queue = deque([(0, 0)])
     while queue:
-        row, col, cnt = queue.popleft()
-        if row == N - 1 and col == N - 1:
-            return cnt
+        row, col = queue.popleft()
         for d in range(4):
             nrow, ncol = row + drow[d], col + dcol[d]
-            if inside(nrow, ncol) and arr[nrow][ncol] != -1:
-                if arr[nrow][ncol] == 1:
-                    queue.appendleft((nrow, ncol, cnt))
-                else:
-                    queue.append((nrow, ncol, cnt + 1))
-                arr[nrow][ncol] = -1
+            if inside(nrow, ncol):
+                if arr[nrow][ncol] and dist[row][col] < dist[nrow][ncol]:
+                    dist[nrow][ncol] = dist[row][col]
+                    queue.append((nrow, ncol))
+                elif not arr[nrow][ncol] and dist[row][col] + 1 < dist[nrow][ncol]:
+                    dist[nrow][ncol] = dist[row][col] + 1
+                    queue.append((nrow, ncol))
+
+    return dist[N - 1][N - 1]
 
 N = int(input())
 arr = [list(map(int, input().rstrip())) for _ in range(N)]
