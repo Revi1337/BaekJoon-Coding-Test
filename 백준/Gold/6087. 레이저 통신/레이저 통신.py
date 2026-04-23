fff8c46ad5680c00bd0db11cb22ab79c381eb0a9
Revi1337 <david122123@gmail.com>
2026-04-23 19:48:55 +0900

    [Gold III] Title: 레이저 통신, Time: 220 ms, Memory: 115504 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.py
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.py
@@ -1,48 +1,52 @@
-import sys
-from collections import deque
+# 2026-04-23
+# https://www.acmicpc.net/problem/6087
+# 레이저 통신
+# V1. dijkstra
 
-input = sys.stdin.readline
+import heapq
 
-# 상 우 하 좌
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
 
 def solution(W, H, arr):
 
-    WALL, EMPTY, TAR = '*', '.', 'C'
-    inside = lambda row, col : 0 <= row < H and 0 <= col < W
+    inside = lambda r, c: 0 <= r < H and 0 <= c < W
 
-    tars = []
+    spos = []
     for row in range(H):
         for col in range(W):
-            if arr[row][col] == TAR:
-                tars.append([row, col])
+            if arr[row][col] == 'C':
+                spos.append((row, col))
+                arr[row][col] = '.'
 
-    INF = 1e9
-    check = [[[INF] * W for _ in range(H)] for _ in range(4)]
+    (srow, scol), (erow, ecol) = spos
+    INF = float('inf')
+    dist = [[[INF] * 4 for _ in range(W)] for _ in range(H)]
 
-    queue = deque()
-    row, col = tars[0]
+    pq = []
     for d in range(4):
-        check[d][row][col] = 0
-        queue.append([row, col, d])
+        dist[srow][scol][d] = 0
+        heapq.heappush(pq, (0, d, srow, scol))
 
-    while queue:
-        row, col, d = queue.popleft()
-        if [row, col] == tars[1]:
-            return check[d][row][col]
+    while pq:
+        cost, d, row, col = heapq.heappop(pq)
+        if cost > dist[row][col][d]:
+            continue
+        if (row, col) == (erow, ecol):
+            return cost
 
         nrow, ncol = row + drow[d], col + dcol[d]
-        if not inside(nrow, ncol) or arr[nrow][ncol] == WALL:
+        if not inside(nrow, ncol) or arr[nrow][ncol] == '*':
             continue
 
-        if check[d][nrow][ncol] > check[d][row][col]:
-            check[d][nrow][ncol] = check[d][row][col]
-            queue.appendleft([nrow, ncol, d])
-        for nd in (d + 1) % 4, (d + 3) % 4:
-            if check[nd][nrow][ncol] > check[d][row][col] + 1:
-                check[nd][nrow][ncol] = check[d][row][col] + 1
-                queue.append([nrow, ncol, nd])
+        if cost < dist[nrow][ncol][d]: # 거울 설치 x
+            dist[nrow][ncol][d] = cost
+            heapq.heappush(pq, (cost, d, nrow, ncol))
+
+        for nd in [(d + 1) % 4, (d + 3) % 4]: # 거울 설치 o
+            if cost + 1 < dist[nrow][ncol][nd]:
+                dist[nrow][ncol][nd] = cost + 1
+                heapq.heappush(pq, (cost + 1, nd, nrow, ncol))
 
 W, H = map(int, input().split())
 arr = [list(input().rstrip()) for _ in range(H)]
