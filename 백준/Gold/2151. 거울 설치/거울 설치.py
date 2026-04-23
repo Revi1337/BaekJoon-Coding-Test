b8fc6ccd5b5c428efbe3a351fdec2237e2cd883f
Revi1337 <david122123@gmail.com>
2026-04-23 20:43:53 +0900

    [Gold III] Title: 거울 설치, Time: 132 ms, Memory: 114720 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -1,9 +1,9 @@
 # 2026-04-23
 # https://www.acmicpc.net/problem/2151
 # 거울 설치
-# V1. dijkstra
+# V2. bfs(0-1)
 
-import heapq
+from collections import deque
 
 drow = [-1, 0, 1, 0]
 dcol = [0, 1, 0, -1]
@@ -23,13 +23,13 @@ def solution(N, arr):
     INF = float('inf')
     dist = [[[INF] * 4 for _ in range(N)] for _ in range(N)]
 
-    pq = []
+    queue = deque()
     for d in range(4):
         dist[srow][scol][d] = 0
-        heapq.heappush(pq, (0, d, srow, scol))
+        queue.append((0, d, srow, scol))
 
-    while pq:
-        cost, d, row, col = heapq.heappop(pq)
+    while queue:
+        cost, d, row, col = queue.popleft()
         if cost > dist[row][col][d]:
             continue
         if (row, col) == (erow, ecol):
@@ -41,15 +41,15 @@ def solution(N, arr):
 
         if arr[nrow][ncol] == '.':
             dist[nrow][ncol][d] = cost
-            heapq.heappush(pq, (cost, d, nrow, ncol))
+            queue.appendleft((cost, d, nrow, ncol))
         elif arr[nrow][ncol] == '!':
-            if cost < dist[nrow][ncol][d]: # 거울 설치 x
+            if cost < dist[nrow][ncol][d]:  # 거울 설치 x
                 dist[nrow][ncol][d] = cost
-                heapq.heappush(pq, (cost, d, nrow, ncol))
-            for nd in [(d + 1) % 4, (d + 3) % 4]: # 거울 설치 o
+                queue.appendleft((cost, d, nrow, ncol))
+            for nd in [(d + 1) % 4, (d + 3) % 4]:  # 거울 설치 o
                 if cost + 1 < dist[nrow][ncol][nd]:
                     dist[nrow][ncol][nd] = cost + 1
-                    heapq.heappush(pq, (cost + 1, nd, nrow, ncol))
+                    queue.append((cost + 1, nd, nrow, ncol))
 
 N = int(input())
 arr = [list(input().rstrip()) for _ in range(N)]
