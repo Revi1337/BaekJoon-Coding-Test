1fc1b387ae2f3e841d1c8ec1948786be2be6d782
Revi1337 <david122123@gmail.com>
2026-04-23 17:58:33 +0900

    [Gold III] Title: 택배, Time: 388 ms, Memory: 116680 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,31 +1,40 @@
 # 2026-04-23
 # https://www.acmicpc.net/problem/1719
 # 택배
-# V1. floyd-warshall
+# V2. dijkstra
+
+import heapq
 
 def solution(N, M, E):
     INF = 1000 * (N + 1)
-    dist = [[INF] * (N + 1) for _ in range(N + 1)]
+
+    graph = [[] for _ in range(N + 1)]
     trace = [[0] * (N + 1) for _ in range(N + 1)]
-
-    for v in range(1, N + 1):
-        dist[v][v] = 0
-
+    dist = [[INF] * (N + 1) for _ in range(N + 1)]
     for v1, v2, c in E:
-        dist[v1][v2] = dist[v2][v1] = c
+        graph[v1].append([v2, c])
+        graph[v2].append([v1, c])
         trace[v1][v2] = v2
         trace[v2][v1] = v1
 
-    for mid in range(1, N + 1):
-        for st in range(1, N + 1):
-            for end in range(1, N + 1):
-                if dist[st][mid] + dist[mid][end] < dist[st][end]:
-                    dist[st][end] = dist[st][mid] + dist[mid][end]
-                    trace[st][end] = trace[st][mid]
+    for st in range(1, N + 1):
+        dist[st][st] = 0
+        pq = [[dist[st][st], st]]
+        while pq:
+            c, n = heapq.heappop(pq)
+            if c > dist[st][n]:
+                continue
+            for nn, nc in graph[n]:
+                if c + nc < dist[st][nn]:
+                    dist[st][nn] = c + nc
+                    heapq.heappush(pq, [dist[st][nn], nn])
+                    if n == st:
+                        trace[st][nn] = nn
+                    else:
+                        trace[st][nn] = trace[st][n]
 
     for line in trace[1:]:
         print(*[st if st else '-' for st in line[1:]])
-
 
 N, M = map(int, input().split())
 E = [list(map(int, input().split())) for _ in range(M)]
