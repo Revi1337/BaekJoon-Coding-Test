10bbcbac75848907c406f3e12866e93e61bcbb46
Revi1337 <david122123@gmail.com>
2026-04-23 21:32:22 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 304 ms, Memory: 142932 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,39 +1,42 @@
-import sys
+# 2026-04-23
+# https://www.acmicpc.net/problem/11779
+# 최소비용 구하기 2
+# dijkstra
+
 import heapq
 
-input = sys.stdin.readline
+def solution(N, M, E, st, end):
+    INF = float('inf')
 
-def solution(n, m, edges,prolog, epilog):
-    node_cnt = n
-    graph = [[] for _ in range(node_cnt + 1)]
-    for v1, v2, cost in edges:
-        graph[v1].append([v2, cost])
+    graph = [[] for _ in range(N + 1)]
+    for v1, v2, c in E:
+        graph[v1].append([v2, c])
 
-    def dijkstra(prolog):
-        distance = [float('inf') for _ in range(node_cnt + 1)]
-        tracking = [float('inf') for _ in range(node_cnt + 1)]
-        distance[prolog] = 0
-        tracking[prolog] = [prolog]
-        queue = []
-        heapq.heappush(queue, [distance[prolog], prolog])
-        while queue:
-            cost, node = heapq.heappop(queue)
-            if distance[node] < cost:
-                continue
-            for next_node, next_cost in graph[node]:
-                if cost + next_cost < distance[next_node]:
-                    distance[next_node] = cost + next_cost
-                    tracking[next_node] = tracking[node] + [next_node]
-                    heapq.heappush(queue, [cost + next_cost, next_node])
-        return distance, tracking
+    dist = [INF] * (N + 1)
+    dist[st] = 0
+    pq = [[dist[st], st]]
 
-    costs, paths = dijkstra(prolog)
-    print(costs[epilog])
-    print(len(paths[epilog]))
-    print(" ".join(map(str, paths[epilog])))
+    trace = [[] for _ in range(N + 1)]
+    for n in range(1, N + 1):
+        trace[n].append(n)
 
-n = int(input())
-m = int(input())
-edges = [list(map(int, input().split())) for _ in range(m)]
-prolog, epilog = map(int, input().split())
-solution(n, m, edges, prolog, epilog)
+    while pq:
+        c, n = heapq.heappop(pq)
+        if c > dist[n]:
+            continue
+        if n == end:
+            print(dist[end])
+            print(len(trace[end]))
+            print(*trace[end])
+            return
+        for nn, nc in graph[n]:
+            if c + nc < dist[nn]:
+                dist[nn] = c + nc
+                heapq.heappush(pq, [dist[nn], nn])
+                trace[nn] = trace[n] + [nn]
+
+N = int(input())
+M = int(input())
+E = [list(map(int, input().split())) for _ in range(M)]
+st, end = map(int, input().split())
+solution(N, M, E, st, end)
