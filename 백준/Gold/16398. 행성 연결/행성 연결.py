92f9a4d266f34dcb64159553b2a064f8a73fb33f
Revi1337 <david122123@gmail.com>
2026-04-25 00:07:29 +0900

    [Gold IV] Title: 행성 연결, Time: 2036 ms, Memory: 197532 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -1,17 +1,23 @@
+# 2026-04-24
+# https://www.acmicpc.net/problem/16398
+# 행성 연결
+# mst
+# V1. kruskal
+
 import heapq
 
 def solution(N, dist):
     conn, pq, ans = [0] * N, [[0, 0]], 0
 
     while pq:
-        cost, n = heapq.heappop(pq)
-        if conn[n]:
+        cc, cn = heapq.heappop(pq)
+        if conn[cn]:
             continue
-        conn[n] = 1
-        ans += cost
-        for nn, nc in enumerate(dist[n]):
-            if dist[n][nn] != 0 and not conn[nn]:
-                heapq.heappush(pq, (nc, nn))
+        conn[cn] = 1
+        ans += cc
+        for nn, nc in enumerate(dist[cn]):
+            if dist[cn][nn] != 0 and not conn[nn]:
+                heapq.heappush(pq, [nc, nn])
 
     return ans
 
