380fe47faf4859368914adc1867e64acd3ed5589
Revi1337 <david122123@gmail.com>
2026-04-24 17:58:37 +0900

    [Gold III] Title: 웜홀, Time: 224 ms, Memory: 112980 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,25 +1,33 @@
-def solution(N, M, K, roads, holes):
-    edges = []
-    for v, nv, c in roads:
-        edges.append([v, nv, c])
-        edges.append([nv, v, c])
-    for v, nv, c in holes:
-        edges.append([v, nv, -c])
+# 2026-04-24
+# https://www.acmicpc.net/problem/1865
+# 웜홀
+# bellman-ford
 
-    dist = [1e9] * (N + 1)
-    dist[0] = 0
-    for _ in range(N):
-        for v, nv, cost in edges:
-            if dist[nv] > dist[v] + cost:
-                dist[nv] = dist[v] + cost
-                if _ == N - 1:
+def solution(N, M, W, E, WW):
+    EE = []
+    for v1, v2, c in E:
+        EE.append((v1, v2, c))
+        EE.append((v2, v1, c))
+    for v1, v2, c in WW:
+        EE.append((v1, v2, -c))
+
+    for v in range(1, N + 1):
+        EE.append((0, v, 0))
+
+    dist = [0] * (N + 1)
+    st = 0
+    dist[st] = 0
+    for v in range(N):
+        for v1, v2, c in EE:
+            if dist[v1] + c < dist[v2]:
+                dist[v2] = dist[v1] + c
+                if v == N - 1:
                     return 'YES'
-
     return 'NO'
 
 T = int(input())
 for _ in range(T):
-    N, M, K = map(int, input().split())
-    roads = [list(map(int, input().split())) for _ in range(M)]
-    holes = [list(map(int, input().split())) for _ in range(K)]
-    print(solution(N, M, K, roads, holes))
+    N, M, W = map(int, input().split())
+    E = [list(map(int, input().split())) for _ in range(M)]
+    WW = [list(map(int, input().split())) for _ in range(W)]
+    print(solution(N, M, W, E, WW))
