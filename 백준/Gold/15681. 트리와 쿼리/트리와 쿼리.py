739ddef864d176ccce4edd32a5716a2280ee9f51
Revi1337 <david122123@gmail.com>
2026-04-26 01:15:02 +0900

    [Gold V] Title: 트리와 쿼리, Time: 460 ms, Memory: 89784 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,37 +1,38 @@
+# 2026-04-25
+# https://www.acmicpc.net/problem/15681
+# 트리와 쿼리
+# tree
+# dfs(post order + tree dp)
+# 아 시이이발 설마.  input = sys.stdin.readline 문제냐? 
+
 import sys
+
+input = sys.stdin.readline
 
 sys.setrecursionlimit(10 ** 6)
 
-def solution(N, R, Q, E, ques):
+def solution(N, R, Q, E, U):
 
-    def make_tree(n, p):
-        for cn in tree[n]:
-            if cn != p:
-                P[cn] = n
-                C[n].append(cn)
-                make_tree(cn, n)
+    def make_tree(n, pn):
+        cnt = 1
+        for nn in tree[n]:
+            if nn != pn:
+                cnt += make_tree(nn, n)
 
-    def count_child(n):
-        tot = 0
-        for cn in C[n]:
-            tot += count_child(cn)
-
-        DP[n] += tot
-        return DP[n]
+        memo[n] += cnt
+        return memo[n]
 
     tree = [[] for _ in range(N + 1)]
     for v1, v2 in E:
         tree[v1].append(v2)
         tree[v2].append(v1)
 
-    P, C = [0] * (N + 1), [[] for _ in range(N + 1)]
+    memo = [0] * (N + 1)
     make_tree(R, -1)
 
-    DP = [1] * (N + 1)
-    count_child(R)
-    print(*[DP[n] for n in ques], sep = '\n')
+    print(*[memo[u] for u in U], sep = '\n')
 
-N, R, Q = map(int,sys.stdin.readline().rstrip().split())
-E = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N - 1)]
-ques = [int(sys.stdin.readline()) for _ in range(Q)]
-solution(N, R, Q, E, ques)
+N, R, Q = map(int, input().split())
+E = [list(map(int, input().split())) for _ in range(N - 1)]
+U = [int(input()) for _ in range(Q)]
+solution(N, R, Q, E, U)
