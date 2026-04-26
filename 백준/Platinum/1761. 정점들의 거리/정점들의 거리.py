adee5a3ddc8d537cba57c49360652b753bf9304e
Revi1337 <david122123@gmail.com>
2026-04-27 01:17:43 +0900

    [Platinum V] Title: 정점들의 거리, Time: 304 ms, Memory: 231480 KB -BaekjoonHub

--- 백준/Platinum/1761. 정점들의 거리/정점들의 거리.py
+++ 백준/Platinum/1761. 정점들의 거리/정점들의 거리.py
@@ -1,58 +1,67 @@
+# 2026-04-26
+# https://www.acmicpc.net/problem/1761
+# 정점들의 거리
+# tree
+
 import sys
 import math
 
-sys.setrecursionlimit(10 ** 5)
 input = sys.stdin.readline
+sys.setrecursionlimit((10 ** 4) * 5)
 
 LOG = math.ceil(math.log2(40_000))
 
 def solution(N, E, M, Q):
 
-    def make_tree_and_find_root():
-        ref = [0] * (N + 1)
-        tree = [[] for _ in range(N + 1)]
-        for v1, v2, cost in E:
-            tree[v2].append([v1, cost])
-            tree[v1].append([v2, cost])
-            ref[v1], ref[v2] = ref[v1] + 1, ref[v2] + 1
+    def find_root():
+        freq = [0] * (N + 1)
+        for v1, v2, c in E:
+            freq[v1] += 1
+            freq[v2] += 1
 
-        return tree, ref.index(min(ref[1:]))
+        return freq.index(min(freq[1:]))
 
-    def tree_trav(n, pn, d):
+    def make_tree(n, pn, d, sm):
         parents[n][0] = pn
-        depths[n] = d
+        depth[n] = d
+        dist[n] = sm
         for nn, nc in tree[n]:
             if nn != pn:
-                dist[nn] = dist[n] + nc
-                tree_trav(nn, n, d + 1)
+                make_tree(nn, n, d + 1, sm + nc)
 
-    def set_parents():
+    def binary_lift():
         for k in range(1, LOG):
             for n in range(1, N + 1):
                 parents[n][k] = parents[parents[n][k - 1]][k - 1]
 
-    def lca_with_dist(n1, n2):
+    def lca(n1, n2):
         cn1, cn2 = n1, n2
-        if depths[n1] > depths[n2]:
-            n1, n2, cn1, cn2 = n2, n1, n2, n1
+        if depth[n1] > depth[n2]:
+            n1, n2, cn1, cn2 = n2, n1, cn2, cn1
         for k in range(LOG - 1, -1, -1):
-            if depths[n2] - depths[n1] >= (1 << k):
+            if depth[n2] - depth[n1] >= (1 << k):
                 n2 = parents[n2][k]
         if n1 == n2:
             return dist[cn2] - dist[cn1]
         for k in range(LOG - 1, -1, -1):
             if parents[n1][k] != parents[n2][k]:
                 n1, n2 = parents[n1][k], parents[n2][k]
-        return (dist[cn1] - dist[parents[n1][0]]) + (dist[cn2] - dist[parents[n1][0]])
+        return (dist[cn2] - dist[parents[n1][0]]) + (dist[cn1] - dist[parents[n1][0]])
 
+    tree = [[] for _ in range(N + 1)]
+    for v1, v2, c in E:
+        tree[v1].append([v2, c])
+        tree[v2].append([v1, c])
+
+    depth, dist = [[0] * (N + 1) for _ in range(2)]
     parents = [[0] * LOG for _ in range(N + 1)]
-    depths, dist = [[0] * (N + 1) for _ in range(2)]
-    tree, root = make_tree_and_find_root()
-    tree_trav(root, root, 0)
-    set_parents()
+    root = find_root()
+
+    make_tree(root, root, 0, 0)
+    binary_lift()
 
     for ns in Q:
-        print(lca_with_dist(*ns))
+        print(lca(*ns))
 
 N = int(input())
 E = [list(map(int, input().split())) for _ in range(N - 1)]
