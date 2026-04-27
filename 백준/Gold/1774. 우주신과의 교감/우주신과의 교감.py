867e4bb85d180589a92478c0e9dfa9fa436797bc
Revi1337 <david122123@gmail.com>
2026-04-27 14:38:52 +0900

    [Gold III] Title: 우주신과의 교감, Time: 1620 ms, Memory: 181092 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -1,5 +1,8 @@
-# 2025-10-02
+# 2026-04-27
 # https://www.acmicpc.net/problem/1774
+# 우주신과의 교감
+# MST
+# kruskal
 
 import sys
 from math import sqrt
@@ -9,23 +12,22 @@ input = sys.stdin.readline
 def solution(N, M, E, C):
 
     def find(n):
-        if n == parents[n]:
-            return n
-
-        parents[n] = find(parents[n])
-        return parents[n]
+        while n != parents[n]:
+            parents[n] = parents[parents[n]]
+            n = parents[n]
+        return n
 
     def union(n1, n2):
-        root1, root2 = find(n1), find(n2)
-        if root2 > root1:
-            parents[root2] = root1
+        r1, r2 = find(n1), find(n2)
+        if r1 < r2:
+            parents[r2] = r1
         else:
-            parents[root1] = root2
+            parents[r1] = r2
 
     E.insert(0, [0, 0])
     edges = []
     for e1 in range(1, N + 1):
-        for e2 in range(N):
+        for e2 in range(1, N + 1):
             if e1 != e2:
                 row1, col1 = E[e1]
                 row2, col2 = E[e2]
@@ -45,6 +47,7 @@ def solution(N, M, E, C):
             cnt += 1
 
     return "{:.2f}".format(ans)
+
 
 N, M = map(int, input().split())
 E = [list(map(int, input().split())) for _ in range(N)]
